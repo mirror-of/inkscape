bbff4ca2e7d3432b8d37f66130a811ec1ad0bf4c
bulia byak <buliabyak@gmail.com>
2003-11-13 14:17:26 +0000

    alt, not space, calls menu
    (bzr r186)

--- src/event-context.c
+++ src/event-context.c
@@ -265,7 +265,8 @@ sp_event_context_private_root_handler (SPEventContext *event_context, GdkEvent *
 				ret = TRUE;
 			}
 			break;
-		case GDK_space: // Space - root menu
+		case GDK_Alt_L: //was GDK_space: // Space - root menu
+		case GDK_Alt_R: 
 			sp_event_root_menu_popup (desktop, NULL, event);
 			ret= TRUE;
 			break;
