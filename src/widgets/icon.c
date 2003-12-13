a8834ce1db323c27538b354e952427d5a8f7fa73
MenTaLguY <mental@rydia.net>
2003-12-13 05:42:11 +0000

    More background tweaks.
    (bzr r515)

--- src/widgets/icon.c
+++ src/widgets/icon.c
@@ -301,8 +301,8 @@ sp_icon_paint (SPIcon *icon, GdkRectangle *area)
 				/* fixme: We support only plain-color themes */
 				/* fixme: But who needs other ones anyways? (Lauris) */
 
-				/* buttons with relief = none don't paint their background */
-				if (GTK_IS_BUTTON (widget->parent) && gtk_button_get_relief (GTK_BUTTON (widget->parent)) == GTK_RELIEF_NONE) {
+				/* buttons with relief = none don't paint their background when in normal state */
+				if (GTK_IS_BUTTON (widget->parent) && gtk_button_get_relief (GTK_BUTTON (widget->parent)) == GTK_RELIEF_NONE && widget->state == GTK_STATE_NORMAL) {
 					style = widget->style;
 				} else {
 					gtk_widget_ensure_style (widget->parent);
