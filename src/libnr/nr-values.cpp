223c08d21a612c68f8f505e7f549f59a6a194e1a
Bob Jamison <ishmalius@gmail.com>
2004-01-02 06:46:30 +0000

    Remove export and WIN32 switches
    (bzr r774)

--- src/libnr/nr-values.cpp
+++ src/libnr/nr-values.cpp
@@ -41,3 +41,18 @@ inline NR::Point abs(NR::Point const &b) {
 	return ret;
 }
 
+
+/*
+The following predefined objects are for reference
+and comparison.
+*/
+NRMatrix NR_MATRIX_IDENTITY =
+       {{1.0, 0.0, 0.0, 1.0, 0.0, 0.0}};
+NRRect   NR_RECT_EMPTY =
+       {NR_HUGE, NR_HUGE, -NR_HUGE, -NR_HUGE};
+NRRectL  NR_RECT_L_EMPTY =
+       {NR_HUGE_L, NR_HUGE_L, -NR_HUGE_L, -NR_HUGE_L};
+NRRectL  NR_RECT_S_EMPTY =
+       {NR_HUGE_S, NR_HUGE_S, -NR_HUGE_S, -NR_HUGE_S};
+
+
--- src/libnr/nr-values.h
+++ src/libnr/nr-values.h
@@ -14,27 +14,18 @@
 
 #define NR_EPSILON 1e-18
 
-#define NR_HUGE 1e18
+#define NR_HUGE   1e18
 #define NR_HUGE_L (0x7fffffff)
 #define NR_HUGE_S (0x7fff)
 
-#ifdef _WIN32
-#define NR_EXPORT static
-#else
-#define NR_EXPORT
-#endif
+/*
+The following predefined objects are for reference
+and comparison.  They are defined in nr-values.cpp
+*/
+extern NRMatrix NR_MATRIX_IDENTITY;
+extern NRRect   NR_RECT_EMPTY;
+extern NRRectL  NR_RECT_L_EMPTY;
+extern NRRectL  NR_RECT_S_EMPTY;
 
-#if defined (__NR_VALUES_C__) || defined (_WIN32)
-NR_EXPORT NRMatrix NR_MATRIX_IDENTITY = {{1.0, 0.0, 0.0, 1.0, 0.0, 0.0}};
-NR_EXPORT NRRect NR_RECT_EMPTY = {NR_HUGE, NR_HUGE, -NR_HUGE, -NR_HUGE};
-NR_EXPORT NRRectL NR_RECT_L_EMPTY = {NR_HUGE_L, NR_HUGE_L, -NR_HUGE_L, -NR_HUGE_L};
-NR_EXPORT NRRectL NR_RECT_S_EMPTY = {NR_HUGE_S, NR_HUGE_S, -NR_HUGE_S, -NR_HUGE_S};
-#else
-extern NRMatrix NR_MATRIX_IDENTITY;
-extern NRMatrix NR_MATRIX_IDENTITY;
-extern NRRect NR_RECT_EMPTY;
-extern NRRectL NR_RECT_L_EMPTY;
-extern NRRectL NR_RECT_S_EMPTY;
-#endif
 
 #endif
