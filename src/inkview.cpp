0106e564311b35c3fa1100d99abfca6c569adb22
sk1p <sk1p@users.sourceforge.net>
2004-01-02 17:52:56 +0000

    re-enabled SVG Viewer
    (bzr r785)

--- src/inkview.cpp
+++ src/inkview.cpp
@@ -257,6 +257,6 @@ void inkscape_ref (void) {}
 void inkscape_unref (void) {}
 void inkscape_add_document (SPDocument *document) {}
 void inkscape_remove_document (SPDocument *document) {}
-SPRepr *inkscape_get_repr (Inkscape::Application *inkscape, const unsigned char *key) {return NULL;}
+SPRepr *inkscape_get_repr (Inkscape::Application *inkscape, const gchar *key) {return NULL;}
 #include "widgets/menu.h"
 void sp_menu_append (SPMenu *menu, const gchar *name, const gchar *tip, const void *data) {}
