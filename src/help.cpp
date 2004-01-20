e9e316f6e9a7bd37cf55a09c293689bf51d36bc8
Jon Phillips <jon@fabricatorz.com>
2004-01-20 21:09:13 +0000

    I cleaned up and commented the help code. I'm starting work on the help system.
    (bzr r1105)

--- src/help.cpp
+++ src/help.cpp
@@ -27,8 +27,8 @@ static GtkWidget *w = NULL;
 static gint
 sp_help_about_delete (GtkWidget *widget, GdkEvent *event, gpointer data)
 {
-	w = NULL;
-	return FALSE;
+    w = NULL;
+    return FALSE;
 }
 
 #define WINDOW_MIN 20
@@ -37,37 +37,56 @@ sp_help_about_delete (GtkWidget *widget, GdkEvent *event, gpointer data)
 void
 sp_help_about (void)
 {
-	SPDocument *doc;
-	SPObject *version;
-	GtkWidget *v;
-	gint width, height;
+    SPDocument *doc;
+    SPObject *version;
+    GtkWidget *v;
+    gint width, height;
 
-	if (!w) {
-		doc = sp_document_new (INKSCAPE_PIXMAPDIR "/about.svg", FALSE, TRUE);
-		g_return_if_fail (doc != NULL);
-		version = sp_document_lookup_id (doc, "version");
-		if (version && SP_IS_TEXT (version)) {
-			sp_text_set_repr_text_multiline (SP_TEXT (version), INKSCAPE_VERSION);
-		}
-		sp_document_ensure_up_to_date (doc);
+    if (!w) {
+        doc = sp_document_new (INKSCAPE_PIXMAPDIR "/about.svg", FALSE, TRUE);
+        g_return_if_fail (doc != NULL);
+        version = sp_document_lookup_id (doc, "version");
+        
+        if (version && SP_IS_TEXT (version)) {
+            sp_text_set_repr_text_multiline ( SP_TEXT (version), 
+                                              INKSCAPE_VERSION);
+        }
+		
+        sp_document_ensure_up_to_date (doc);
 
-		w = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-		gtk_window_set_title (GTK_WINDOW (w), _("About Inkscape"));
 
-		width  = static_cast< gint > (CLAMP( sp_document_width(doc), WINDOW_MIN, WINDOW_MAX ));
-		height = static_cast< gint > (CLAMP( sp_document_height(doc), WINDOW_MIN, WINDOW_MAX ));
-		gtk_window_set_default_size (GTK_WINDOW (w), width, height );
-		gtk_window_set_position(GTK_WINDOW(w), GTK_WIN_POS_CENTER);
+        w = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
-		gtk_window_set_policy (GTK_WINDOW (w), TRUE, TRUE, FALSE);
-		gtk_signal_connect (GTK_OBJECT (w), "delete_event", GTK_SIGNAL_FUNC (sp_help_about_delete), NULL);
+        gtk_window_set_title (GTK_WINDOW (w), _("About Inkscape"));
 
-		v = sp_svg_view_widget_new (doc);
-		sp_svg_view_widget_set_resize (SP_SVG_VIEW_WIDGET (v), FALSE, sp_document_width (doc), sp_document_height (doc));
-		sp_document_unref (doc);
-		gtk_widget_show (v);
-		gtk_container_add (GTK_CONTAINER (w), v);
-	}
+        width  = static_cast< gint > ( CLAMP( sp_document_width(doc), 
+                                              WINDOW_MIN, WINDOW_MAX ));
+        
+        height = static_cast< gint > ( CLAMP( sp_document_height(doc), 
+                                              WINDOW_MIN, WINDOW_MAX ));
 
-	gtk_window_present (GTK_WINDOW (w));
-}
+        gtk_window_set_default_size ( GTK_WINDOW (w), width, height );
+
+        gtk_window_set_position( GTK_WINDOW(w), GTK_WIN_POS_CENTER);
+
+        gtk_window_set_policy ( GTK_WINDOW (w), TRUE, TRUE, FALSE);
+
+        gtk_signal_connect ( GTK_OBJECT (w), "delete_event", 
+                             GTK_SIGNAL_FUNC (sp_help_about_delete), NULL);
+
+        v = sp_svg_view_widget_new (doc);
+
+        sp_svg_view_widget_set_resize ( SP_SVG_VIEW_WIDGET (v), FALSE, 
+                                        sp_document_width (doc), 
+                                        sp_document_height (doc));
+
+        sp_document_unref (doc);
+        gtk_widget_show (v);
+        gtk_container_add (GTK_CONTAINER (w), v);
+
+    } // close if (!w)
+
+    gtk_window_present (GTK_WINDOW (w));
+
+} // close sp_help_about()
+
