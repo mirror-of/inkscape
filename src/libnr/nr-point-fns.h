e52fea912125fe60d75ee456a624f937a772ff56
Nathan Hurst <njh@njhurst.com>
2004-01-23 02:15:07 +0000

    added a lerp operator
    (bzr r1128)

--- src/libnr/nr-point-fns.h
+++ src/libnr/nr-point-fns.h
@@ -10,8 +10,15 @@ extern double L2(Point const &p);
 extern double LInfty(Point const &p);
 extern double atan2(Point const p);
 
+/** Return NR::rotate(pi/2)*p.  This is much more efficient. */
 inline Point rot90(Point const &p) {
 	return Point(-p[Y], p[X]);
+}
+
+/** Given two points and a parameter t \in [0, 1], return a point
+ * proportionally from a to b by t. */
+inline Point Lerp(double t, Point const a, Point const b) {
+	return (1-t)*a + t*b;
 }
 
 } /* namespace NR */
