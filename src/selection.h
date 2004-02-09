cc6fcd08806560bc3aa2e585d6504711eec6d7d6
Peter Moulder <peter.moulder@monash.edu>
2004-02-09 00:59:05 +0000

    cleanups
    (bzr r1375)

--- src/selection.h
+++ src/selection.h
@@ -39,32 +39,32 @@ struct _SPSelectionClass {
 
 /* Constructor */
 
-SPSelection * sp_selection_new (SPDesktop * desktop);
+SPSelection *sp_selection_new(SPDesktop *desktop);
 
 /* This are private methods & will be removed from this file */
 
-void sp_selection_changed (SPSelection * selection);
+void sp_selection_changed(SPSelection *selection);
 void sp_current_selection_changed ();
-void sp_selection_update_statusbar (SPSelection * selection);
+void sp_selection_update_statusbar(SPSelection *selection);
 
-gboolean sp_selection_item_selected (SPSelection * selection, SPItem *item);
-gboolean sp_selection_repr_selected (SPSelection * selection, SPRepr *repr);
+gboolean sp_selection_item_selected(SPSelection *selection, SPItem *item);
+gboolean sp_selection_repr_selected(SPSelection *selection, SPRepr *repr);
 #define sp_selection_is_empty(s) (s->items == NULL)
 
-void sp_selection_add_item (SPSelection * selection, SPItem * item);
-void sp_selection_add_repr (SPSelection * selection, SPRepr * repr);
-void sp_selection_set_item (SPSelection * selection, SPItem * item);
-void sp_selection_set_repr (SPSelection * selection, SPRepr * repr);
-void sp_selection_remove_item (SPSelection * selection, SPItem * item);
-void sp_selection_remove_repr (SPSelection * selection, SPRepr * repr);
-void sp_selection_set_item_list (SPSelection * selection, const GSList * list);
-void sp_selection_set_repr_list (SPSelection * selection, const GSList * list);
-#define sp_selection_set_empty(s) sp_selection_empty (s)
-void sp_selection_empty (SPSelection * selection);
-const GSList *sp_selection_item_list (SPSelection * selection);
-const GSList *sp_selection_repr_list (SPSelection * selection);
-SPItem *sp_selection_item (SPSelection * selection);
-SPRepr *sp_selection_repr (SPSelection * selection);
+void sp_selection_add_item(SPSelection *selection, SPItem *item);
+void sp_selection_add_repr(SPSelection *selection, SPRepr *repr);
+void sp_selection_set_item(SPSelection *selection, SPItem *item);
+void sp_selection_set_repr(SPSelection *selection, SPRepr *repr);
+void sp_selection_remove_item(SPSelection *selection, SPItem *item);
+void sp_selection_remove_repr(SPSelection *selection, SPRepr *repr);
+void sp_selection_set_item_list(SPSelection *selection, GSList const *list);
+void sp_selection_set_repr_list(SPSelection *selection, GSList const *list);
+#define sp_selection_set_empty(s) sp_selection_empty(s)
+void sp_selection_empty(SPSelection *selection);
+const GSList *sp_selection_item_list (SPSelection *selection);
+const GSList *sp_selection_repr_list (SPSelection *selection);
+SPItem *sp_selection_item (SPSelection *selection);
+SPRepr *sp_selection_repr (SPSelection *selection);
 
 NRRect *sp_selection_bbox (SPSelection *selection, NRRect *bbox);
 NR::Rect sp_selection_bbox (SPSelection *selection);
@@ -72,8 +72,7 @@ NRRect *sp_selection_bbox_document (SPSelection *selection, NRRect *bbox);
 NR::Rect sp_selection_bbox_document (SPSelection *selection);
 
 /* Returns number of points used */
-int sp_selection_snappoints (SPSelection *selection, NR::Point *points, int size);
+int sp_selection_snappoints(SPSelection *selection, NR::Point points[], int size);
 
 
-
-#endif
+#endif /* !__SP_SELECTION_H__ */
