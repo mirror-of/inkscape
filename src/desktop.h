ee2be2f0def0d5caf5d5d5643b977294945420f9
Peter Moulder <peter.moulder@monash.edu>
2004-02-11 01:37:33 +0000

    (sp_desktop_set_coordinate_status): Add NR::Point version.
    (bzr r1432)

--- src/desktop.h
+++ src/desktop.h
@@ -110,6 +110,14 @@ void sp_desktop_pop_event_context (SPDesktop *desktop, unsigned int key);
 #define SP_COORDINATES_UNDERLINE_Y (1 << NR::Y)
 
 void sp_desktop_set_coordinate_status (SPDesktop *desktop, gdouble x, gdouble y, guint underline);
+
+inline void sp_desktop_set_coordinate_status(SPDesktop *desktop, NR::Point const &p, guint underline)
+{
+	using NR::X;
+	using NR::Y;
+	sp_desktop_set_coordinate_status(desktop, p[X], p[Y], underline);
+}
+
 SPItem *sp_desktop_item_at_point (SPDesktop const *desktop, NR::Point const p, gboolean into_groups);
 SPItem *sp_desktop_group_at_point (SPDesktop const *desktop, NR::Point const p);
 
