c243cbfd5b2863e9a73ecc29524b5b7a58943b14
Peter Moulder <peter.moulder@monash.edu>
2004-02-13 12:10:28 +0000

    Update modeline.
    (bzr r1490)

--- src/libnr/nr-point-fns-test.cpp
+++ src/libnr/nr-point-fns-test.cpp
@@ -58,9 +58,8 @@ int main(int argc, char *argv[])
   Local Variables:
   mode:c++
   c-file-style:"stroustrup"
-  c-file-offsets:((innamespace . 0)(substatement-open . 0))
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
   indent-tabs-mode:nil
-  c-brace-offset:0
   fill-column:99
   End:
   vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
