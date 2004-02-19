06412184499459ab8f9d2a0a53d2207ff203507d
Nathan Hurst <njh@njhurst.com>
2004-02-19 05:41:03 +0000

    code tidy
    (bzr r1555)

--- src/widgets/dash-selector.cpp
+++ src/widgets/dash-selector.cpp
@@ -100,12 +100,6 @@ sp_dash_selector_get_type (void)
 static void
 sp_dash_selector_class_init (SPDashSelectorClass *klass)
 {
-	GtkObjectClass *object_class;
-	GtkWidgetClass *widget_class;
-
-	object_class = (GtkObjectClass *) klass;
-	widget_class = (GtkWidgetClass *) klass;
-
 	parent_class = (GtkHBoxClass*)gtk_type_class (GTK_TYPE_HBOX);
 
 	signals[CHANGED] = gtk_signal_new ("changed",
@@ -119,24 +113,21 @@ sp_dash_selector_class_init (SPDashSelectorClass *klass)
 static void
 sp_dash_selector_init (SPDashSelector *dsel)
 {
-	GtkWidget *m, *mi, *sb;
-	int i;
-
 	dsel->dash = gtk_option_menu_new ();
 	gtk_widget_show (dsel->dash);
 	gtk_box_pack_start (GTK_BOX (dsel), dsel->dash, FALSE, FALSE, 0);
 
-	m = gtk_menu_new ();
+	GtkWidget *m = gtk_menu_new ();
 	gtk_widget_show (m);
-	for (i = 0; dashes[i]; i++) {
-		mi = sp_dash_selector_menu_item_new (dsel, dashes[i]);
+	for (int i = 0; dashes[i]; i++) {
+		GtkWidget *mi = sp_dash_selector_menu_item_new (dsel, dashes[i]);
 		gtk_widget_show (mi);
 		gtk_menu_append (GTK_MENU (m), mi);
 	}
 	gtk_option_menu_set_menu (GTK_OPTION_MENU (dsel->dash), m);
 
 	dsel->offset = gtk_adjustment_new (0.0, 0.0, 10.0, 0.1, 1.0, 1.0);
-	sb = gtk_spin_button_new (GTK_ADJUSTMENT (dsel->offset), 0.1, 2);
+	GtkWidget *sb = gtk_spin_button_new (GTK_ADJUSTMENT (dsel->offset), 0.1, 2);
 	sp_dialog_defocus_on_enter (sb); 
 	gtk_widget_show (sb);
 	gtk_box_pack_start (GTK_BOX (dsel), sb, FALSE, FALSE, 0);
@@ -148,34 +139,27 @@ sp_dash_selector_init (SPDashSelector *dsel)
 GtkWidget *
 sp_dash_selector_new (SPRepr *drepr)
 {
-	GtkWidget *dsel;
-
 	if (!dashes) {
-		SPRepr *dr;
-		int ndashes;
-
-		ndashes = 0;
+		int ndashes = 0;
 		if (drepr) {
-			for (dr = drepr->children; dr; dr = dr->next) {
-				if (!strcmp (sp_repr_name (dr), "dash")) ndashes += 1;
+			for (SPRepr *dr = drepr->children; dr; dr = dr->next) {
+				if (!strcmp (sp_repr_name (dr), "dash"))
+					ndashes += 1;
 			}
 		}
 
 		if (ndashes > 0) {
-			SPStyle *style;
-			int pos;
-			pos = 0;
-			style = sp_style_new ();
+			int pos = 0;
+			SPStyle *style = sp_style_new ();
 			dashes = g_new (double *, ndashes + 1);
-			for (dr = drepr->children; dr; dr = dr->next) {
+			for (SPRepr *dr = drepr->children; dr; dr = dr->next) {
 				if (!strcmp (sp_repr_name (dr), "dash")) {
 					sp_style_read_from_repr (style, dr);
 					if (style->stroke_dash.n_dash > 0) {
-						double *d;
-						int i;
 						dashes[pos] = g_new (double, style->stroke_dash.n_dash + 1);
-						d = dashes[pos];
-						for (i = 0; i < style->stroke_dash.n_dash; i++) {
+						double *d = dashes[pos];
+						int i = 0;
+						for (; i < style->stroke_dash.n_dash; i++) {
 							d[i] = style->stroke_dash.dash[i];
 						}
 						d[i] = -1;
@@ -192,7 +176,7 @@ sp_dash_selector_new (SPRepr *drepr)
 		}
 	}
 
-	dsel = (GtkWidget*)gtk_type_new (SP_TYPE_DASH_SELECTOR);
+	GtkWidget *dsel = (GtkWidget*)gtk_type_new (SP_TYPE_DASH_SELECTOR);
 
 	return dsel;
 }
@@ -200,27 +184,23 @@ sp_dash_selector_new (SPRepr *drepr)
 void
 sp_dash_selector_set_dash (SPDashSelector *dsel, int ndash, double *dash, double offset)
 {
-	int pos;
-
-	pos = 0;
-
+	int pos = 0;
 	if (ndash > 0) {
-		double delta;
-		int i;
-		delta = 0.0;
-		for (i = 0; i < ndash; i++) delta += dash[i];
-		delta = delta / 1000.0;
+		double delta = 0.0;
+		for (int i = 0; i < ndash; i++)
+			delta += dash[i];
+		delta /= 1000.0;
 
-		for (i = 0; dashes[i]; i++) {
-			double *pattern;
-			int np;
-			pattern = dashes[i];
-			np = 0;
-			while (pattern[np] >= 0.0) np += 1;
+		for (int i = 0; dashes[i]; i++) {
+			double *pattern = dashes[i];
+			int np = 0;
+			while (pattern[np] >= 0.0)
+				np += 1;
 			if (np == ndash) {
 				int j;
 				for (j = 0; j < ndash; j++) {
-					if (!NR_DF_TEST_CLOSE (dash[j], pattern[j], delta)) break;
+					if (!NR_DF_TEST_CLOSE (dash[j], pattern[j], delta))
+						break;
 				}
 				if (j == ndash) {
 					pos = i;
@@ -238,60 +218,65 @@ sp_dash_selector_set_dash (SPDashSelector *dsel, int ndash, double *dash, double
 void
 sp_dash_selector_get_dash (SPDashSelector *dsel, int *ndash, double **dash, double *offset)
 {
-	double *pattern;
-	int nd;
+	double *pattern = (double*)gtk_object_get_data (GTK_OBJECT (dsel), "pattern");
 
-	pattern = (double*)gtk_object_get_data (GTK_OBJECT (dsel), "pattern");
-
-	nd = 0;
-	while (pattern[nd] >= 0.0) nd += 1;
+	int nd = 0;
+	while (pattern[nd] >= 0.0)
+		nd += 1;
 
 	if (nd > 0) {
-		if (ndash) *ndash = nd;
+		if (ndash)
+			*ndash = nd;
 		if (dash) {
 			*dash = g_new (double, nd);
 			memcpy (*dash, pattern, nd * sizeof (double));
 		}
-		if (offset) *offset = GTK_ADJUSTMENT (dsel->offset)->value;
+		if (offset)
+			*offset = GTK_ADJUSTMENT (dsel->offset)->value;
 	} else {
-		if (ndash) *ndash = 0;
-		if (dash) *dash = NULL;
-		if (offset) *offset = 0.0;
+		if (ndash)
+			*ndash = 0;
+		if (dash)
+			*dash = NULL;
+		if (offset)
+			*offset = 0.0;
 	}
 }
 
 static GtkWidget *
 sp_dash_selector_menu_item_new (SPDashSelector *dsel, double *pattern)
 {
-	GtkWidget *mi, *px;
-	GdkPixmap *pixmap;
-	GdkGC *gc;
-	gint8 idash[32];
-	int ndash;
+	GtkWidget *mi = gtk_menu_item_new ();
 
-	mi = gtk_menu_item_new ();
-
-	pixmap = gdk_pixmap_new (GTK_WIDGET (dsel)->window, 64, 16, gdk_visual_get_best_depth ());
-	gc = gdk_gc_new (pixmap);
+	GdkPixmap *pixmap = gdk_pixmap_new (GTK_WIDGET (dsel)->window, 64, 16, gdk_visual_get_best_depth ());
+	GdkGC *gc = gdk_gc_new (pixmap);
 
 	gdk_rgb_gc_set_foreground (gc, 0xffffffff);
 	gdk_draw_rectangle (pixmap, gc, TRUE, 0, 0, 64, 16);
 	gdk_rgb_gc_set_foreground (gc, 0x00000000);
-	for (ndash = 0; pattern[ndash] >= 0.0; ndash ++) idash[ndash] = (gint8) (DASH_PREVIEW_WIDTH * pattern[ndash] + 0.5);
+	
+	int ndash = 0;
+	gint8 idash[32];
+	for (; pattern[ndash] >= 0.0; ndash ++)
+		idash[ndash] = (gint8) (DASH_PREVIEW_WIDTH * pattern[ndash] + 0.5);
 	if (ndash > 0) {
-		gdk_gc_set_line_attributes (gc, DASH_PREVIEW_WIDTH, GDK_LINE_ON_OFF_DASH, GDK_CAP_BUTT, GDK_JOIN_MITER);
+		gdk_gc_set_line_attributes (gc, DASH_PREVIEW_WIDTH, 
+									GDK_LINE_ON_OFF_DASH, GDK_CAP_BUTT, 
+									GDK_JOIN_MITER);
 		gdk_gc_set_dashes (gc, 0, idash, ndash);
 	} else {
-		gdk_gc_set_line_attributes (gc, DASH_PREVIEW_WIDTH, GDK_LINE_SOLID, GDK_CAP_BUTT, GDK_JOIN_MITER);
+		gdk_gc_set_line_attributes (gc, DASH_PREVIEW_WIDTH, 
+									GDK_LINE_SOLID, GDK_CAP_BUTT, 
+									GDK_JOIN_MITER);
 	}
 	gdk_draw_line (pixmap, gc, 4, 8, 60, 8);
 
 	gdk_gc_unref (gc);
 
-	px = gtk_pixmap_new (pixmap, NULL);
-#if 1
+	GtkWidget *px = gtk_pixmap_new (pixmap, NULL);
+	
 	gdk_pixmap_unref (pixmap);
-#endif
+	
 	gtk_widget_show (px);
 	gtk_container_add (GTK_CONTAINER (mi), px);
 
@@ -305,9 +290,7 @@ sp_dash_selector_menu_item_new (SPDashSelector *dsel, double *pattern)
 static void
 sp_dash_selector_dash_activate (GtkObject *object, SPDashSelector *dsel)
 {
-	double *pattern;
-
-	pattern = (double*)gtk_object_get_data (object, "pattern");
+	double *pattern = (double*)gtk_object_get_data (object, "pattern");
 	gtk_object_set_data (GTK_OBJECT (dsel), "pattern", pattern);
 
 	gtk_signal_emit (GTK_OBJECT (dsel), signals[CHANGED]);
