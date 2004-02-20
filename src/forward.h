59488cf5fa2a327a7475ebab4b050ec0c65ad355
Peter Moulder <peter.moulder@monash.edu>
2004-02-20 08:40:21 +0000

    Add modeline
    (bzr r1587)

--- src/forward.h
+++ src/forward.h
@@ -175,4 +175,16 @@ typedef struct _SPEvent SPEvent;
 
 typedef struct _SPPrintContext SPPrintContext;
 
-#endif
+
+#endif /* !__FORWARD_H__ */
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+  vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
+*/
