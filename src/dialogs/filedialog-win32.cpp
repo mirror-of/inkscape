85c3fa595df5aa0f51860f78f4fccd70ba659b94
Bob Jamison <ishmalius@gmail.com>
2004-02-20 13:54:02 +0000

    Added Jon's patch for opening files with unicode names.
    (bzr r1610)

--- src/dialogs/filedialog-win32.cpp
+++ src/dialogs/filedialog-win32.cpp
@@ -1,10 +1,12 @@
 
 #include "filedialog.h"
-#include "module.h"
+#include "modules/win32.h"
 
 #include <windows.h>
 
 #include <glib.h>
+
+#define UNSAFE_SCRATCH_BUFFER_SIZE 4096
 
 namespace Inkscape
 {
@@ -19,8 +21,9 @@ namespace Dialogs
 #################################*/
 
 struct FileOpenNativeData_def {
-    OPENFILENAME ofn;
-    gchar fnbuf[4096];
+    char *dir;
+    char *filter;
+    char *title;
 };
 
 FileOpenDialog::FileOpenDialog(
@@ -40,31 +43,11 @@ FileOpenDialog::FileOpenDialog(
                  "SVG files\0*.svg\0"
                  "All files\0*\0";
 
-    nativeData->fnbuf[0] = '\0';
-
-    OPENFILENAME ofn = {
-	    sizeof (OPENFILENAME),
-	    NULL,                       // hwndOwner
-	    NULL,                       // hInstance
-	    (const CHAR *)filter,       // lpstrFilter
-	    NULL,                       // lpstrCustomFilter
-	    0,                          // nMaxCustFilter 
-	    1,                          // nFilterIndex
-	    nativeData->fnbuf,          // lpstrFile
-	    sizeof (nativeData->fnbuf), // nMaxFile
-	    NULL,                       // lpstrFileTitle
-	    0,                          // nMaxFileTitle
-	    (const CHAR *)dir,          // lpstrInitialDir
-	    (const CHAR *)title,        // lpstrTitle
-	    OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY, // Flags
-	    0,                          // nFileOffset
-	    0,                          // nFileExtension
-	    NULL,                       // lpstrDefExt
-	    0,                          // lCustData
-	    NULL,                       // lpfnHook
-	    NULL                        // lpTemplateName
-        };
-    nativeData->ofn = ofn;
+    if ( !dir )
+        dir = "";
+    nativeData->dir    = g_strdup(dir);
+    nativeData->filter = g_strdup(filter);
+    nativeData->title  = g_strdup(title);
 
 }
 
@@ -73,9 +56,12 @@ FileOpenDialog::FileOpenDialog(
 FileOpenDialog::~FileOpenDialog() {
 
     //do any cleanup here
-    if ( nativeData )
+    if ( nativeData ) {
+        g_free(nativeData->dir);
+        g_free(nativeData->filter);
+        g_free(nativeData->title);
         g_free( nativeData );
-
+    }
 }
 
 
@@ -86,15 +72,91 @@ FileOpenDialog::show() {
     if ( !nativeData ) {
         //error
         return NULL;
-        }
+    }
 
-    int retval  = GetOpenFileName (&(nativeData->ofn));
+    gchar *result = NULL;
+    gint  retval  = FALSE;
+
+    //Jon's UNICODE patch
+    if ( sp_win32_is_os_wide() ) {
+        gunichar2 fnbufW[UNSAFE_SCRATCH_BUFFER_SIZE * sizeof(gunichar2)] = {0};
+        gunichar2* dirW    = 
+            g_utf8_to_utf16( nativeData->dir,    -1, NULL, NULL, NULL );
+        gunichar2* filterW = 
+            g_utf8_to_utf16( nativeData->filter, -1, NULL, NULL, NULL );
+        gunichar2* titleW  = 
+            g_utf8_to_utf16( nativeData->title,  -1, NULL, NULL, NULL );
+        OPENFILENAMEW ofn = {
+            sizeof (OPENFILENAMEW),
+            NULL,                   // hwndOwner
+            NULL,                   // hInstance
+            (const WCHAR *)filterW, // lpstrFilter
+            NULL,                   // lpstrCustomFilter
+            0,                      // nMaxCustFilter
+            1,                      // nFilterIndex
+            (WCHAR *)fnbufW,        // lpstrFile
+            sizeof (fnbufW) / sizeof(WCHAR), // nMaxFile
+            NULL,                   // lpstrFileTitle
+            0,                      // nMaxFileTitle
+            (const WCHAR *)dirW,    // lpstrInitialDir
+            (const WCHAR *)titleW,  // lpstrTitle
+            OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY, // Flags
+            0,                      // nFileOffset
+            0,                      // nFileExtension
+            NULL,                   // lpstrDefExt
+            0,                      // lCustData
+            NULL,                   // lpfnHook
+            NULL                    // lpTemplateName
+        };
+
+        retval = GetOpenFileNameW (&ofn);
+        if (retval)
+            result = g_utf16_to_utf8( fnbufW, -1, NULL, NULL, NULL );
+
+        g_free( dirW );
+        g_free( filterW );
+        g_free( titleW );
+
+    } else {
+        gchar *dir    = nativeData->dir;
+        gchar *filter = nativeData->filter;
+        gchar *title  = nativeData->title;
+        gchar fnbuf[UNSAFE_SCRATCH_BUFFER_SIZE] = {0};
+
+        OPENFILENAMEA ofn = {
+            sizeof (OPENFILENAMEA),
+            NULL,                  // hwndOwner
+            NULL,                  // hInstance
+            (const CHAR *)filter,  // lpstrFilter
+            NULL,                  // lpstrCustomFilter
+            0,                     // nMaxCustFilter
+            1,                     // nFilterIndex
+            fnbuf,                 // lpstrFile
+            sizeof (fnbuf),        // nMaxFile
+            NULL,                  // lpstrFileTitle
+            0,                     // nMaxFileTitle
+            (const CHAR *)dir,     // lpstrInitialDir
+            (const CHAR *)title,   // lpstrTitle
+            OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY, // Flags
+            0,                     // nFileOffset
+            0,                     // nFileExtension
+            NULL,                  // lpstrDefExt
+            0,                     // lCustData
+            NULL,                  // lpfnHook
+            NULL                   // lpTemplateName
+        };
+
+        retval = GetOpenFileNameA (&ofn);
+        if ( retval )
+            result = g_strdup(fnbuf);
+    }
 
     if ( !retval ) {
         //int errcode = CommDlgExtendedError();
         return NULL;
-        }
-    return g_strdup (nativeData->fnbuf);
+    }
+
+    return result;
 
 }
 
@@ -132,26 +194,26 @@ FileSaveDialog::FileSaveDialog(
     nativeData->fnbuf[0] = '\0';
 
     OPENFILENAME ofn = {
-	    sizeof (OPENFILENAME),
-	    NULL,                       // hwndOwner
-	    NULL,                       // hInstance
-	    "SVG with extension namespaces\0*\0Standard SVG\0*\0", // lpstrFilter
-	    NULL,                       // lpstrCustomFilter
-	    0,                          // nMaxCustFilter 
-	    1,                          // nFilterIndex
-	    nativeData->fnbuf,          // lpstrFile
-	    sizeof (nativeData->fnbuf), // nMaxFile
-	    NULL,                       // lpstrFileTitle
-	    0,                          // nMaxFileTitle
-	    (const CHAR *)dir,          // lpstrInitialDir
-	    "Save document to file",    // lpstrTitle
-	    OFN_HIDEREADONLY,           // Flags
-	    0,                          // nFileOffset
-	    0,                          // nFileExtension
-	    NULL,                       // lpstrDefExt
-	    0,                          // lCustData
-	    NULL,                       // lpfnHook
-	    NULL                        // lpTemplateName
+        sizeof (OPENFILENAME),
+        NULL,                       // hwndOwner
+        NULL,                       // hInstance
+        "SVG with extension namespaces\0*\0Standard SVG\0*\0", // lpstrFilter
+        NULL,                       // lpstrCustomFilter
+        0,                          // nMaxCustFilter 
+        1,                          // nFilterIndex
+        nativeData->fnbuf,          // lpstrFile
+        sizeof (nativeData->fnbuf), // nMaxFile
+        NULL,                       // lpstrFileTitle
+        0,                          // nMaxFileTitle
+        (const CHAR *)dir,          // lpstrInitialDir
+        "Save document to file",    // lpstrTitle
+        OFN_HIDEREADONLY,           // Flags
+        0,                          // nFileOffset
+        0,                          // nFileExtension
+        NULL,                       // lpstrDefExt
+        0,                          // lCustData
+        NULL,                       // lpfnHook
+        NULL                        // lpTemplateName
         };
     nativeData->ofn = ofn;
 
