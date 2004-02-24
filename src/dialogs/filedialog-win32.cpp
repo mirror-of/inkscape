c8e2d21c17504f778b45493de4c5332ed990f60b
Bob Jamison <ishmalius@gmail.com>
2004-02-24 23:14:10 +0000

    Fixed a unicode bug on XP.  utf8->utf16 on the filters does not work because the embedded \0s in the strings
    (bzr r1670)

--- src/dialogs/filedialog-win32.cpp
+++ src/dialogs/filedialog-win32.cpp
@@ -21,9 +21,9 @@ namespace Dialogs
 #################################*/
 
 struct FileOpenNativeData_def {
-    char *dir;
-    char *filter;
-    char *title;
+    char           *dir;
+    FileDialogType fileTypes;
+    char           *title;
 };
 
 FileOpenDialog::FileOpenDialog(
@@ -35,19 +35,12 @@ FileOpenDialog::FileOpenDialog(
         // do we want exceptions?
         return;
         }
-    gchar *filter = "";
-    if ( fileTypes == SVG_TYPES )
-        filter = "SVG files\0*.svg;*.svgz\0All files\0*\0";
-    else if ( fileTypes == IMPORT_TYPES )
-        filter = "Image files\0*.svg;*.png;*.jpg;*.jpeg;*.bmp;*.gif;*.tiff;*.xpm\0"
-                 "SVG files\0*.svg\0"
-                 "All files\0*\0";
 
     if ( !dir )
         dir = "";
-    nativeData->dir    = g_strdup(dir);
-    nativeData->filter = g_strdup(filter);
-    nativeData->title  = g_strdup(title);
+    nativeData->dir       = g_strdup(dir);
+    nativeData->fileTypes = fileTypes;
+    nativeData->title     = g_strdup(title);
 
 }
 
@@ -58,7 +51,6 @@ FileOpenDialog::~FileOpenDialog() {
     //do any cleanup here
     if ( nativeData ) {
         g_free(nativeData->dir);
-        g_free(nativeData->filter);
         g_free(nativeData->title);
         g_free( nativeData );
     }
@@ -77,14 +69,20 @@ FileOpenDialog::show() {
     gchar *result = NULL;
     gint  retval  = FALSE;
 
+
     //Jon's UNICODE patch
     if ( sp_win32_is_os_wide() ) {
         gunichar2 fnbufW[UNSAFE_SCRATCH_BUFFER_SIZE * sizeof(gunichar2)] = {0};
-        gunichar2* dirW    = 
+        gunichar2* dirW    =
             g_utf8_to_utf16( nativeData->dir,    -1, NULL, NULL, NULL );
-        gunichar2* filterW = 
-            g_utf8_to_utf16( nativeData->filter, -1, NULL, NULL, NULL );
-        gunichar2* titleW  = 
+        gunichar2 *filterW = (gunichar2 *) L"";
+		if ( nativeData->fileTypes == SVG_TYPES )
+		   filterW = (gunichar2 *) L"SVG files\0*.svg;*.svgz\0All files\0*\0";
+		else if ( nativeData->fileTypes == IMPORT_TYPES )
+		   filterW = (gunichar2 *) L"Image files\0*.svg;*.png;*.jpg;*.jpeg;*.bmp;*.gif;*.tiff;*.xpm\0"
+		            L"SVG files\0*.svg\0"
+		            L"All files\0*\0";
+        gunichar2* titleW  =
             g_utf8_to_utf16( nativeData->title,  -1, NULL, NULL, NULL );
         OPENFILENAMEW ofn = {
             sizeof (OPENFILENAMEW),
@@ -114,14 +112,20 @@ FileOpenDialog::show() {
             result = g_utf16_to_utf8( fnbufW, -1, NULL, NULL, NULL );
 
         g_free( dirW );
-        g_free( filterW );
         g_free( titleW );
 
     } else {
         gchar *dir    = nativeData->dir;
-        gchar *filter = nativeData->filter;
         gchar *title  = nativeData->title;
         gchar fnbuf[UNSAFE_SCRATCH_BUFFER_SIZE] = {0};
+
+        gchar *filter = "";
+		if ( nativeData->fileTypes == SVG_TYPES )
+		   filter = "SVG files\0*.svg;*.svgz\0All files\0*\0";
+		else if ( nativeData->fileTypes == IMPORT_TYPES )
+		   filter = "Image files\0*.svg;*.png;*.jpg;*.jpeg;*.bmp;*.gif;*.tiff;*.xpm\0"
+		            "SVG files\0*.svg\0"
+		            "All files\0*\0";
 
         OPENFILENAMEA ofn = {
             sizeof (OPENFILENAMEA),
@@ -199,7 +203,7 @@ FileSaveDialog::FileSaveDialog(
         NULL,                       // hInstance
         "SVG with extension namespaces\0*\0Standard SVG\0*\0", // lpstrFilter
         NULL,                       // lpstrCustomFilter
-        0,                          // nMaxCustFilter 
+        0,                          // nMaxCustFilter
         1,                          // nFilterIndex
         nativeData->fnbuf,          // lpstrFile
         sizeof (nativeData->fnbuf), // nMaxFile
