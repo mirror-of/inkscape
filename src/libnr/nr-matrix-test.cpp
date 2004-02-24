7e6abccd6085ce16eaf96536dfa836e13611ea92
Peter Moulder <peter.moulder@monash.edu>
2004-02-24 07:20:12 +0000

    Split off nr-rotate-test.cpp,nr-scale-test.cpp,nr-translate-test.cpp from nr-matrix-test.cpp. Add tests for recent operator* additions.
    (bzr r1660)

--- src/libnr/nr-matrix-test.cpp
+++ src/libnr/nr-matrix-test.cpp
@@ -1,12 +1,15 @@
-#include "../utest/utest.h"
+#include <utest/utest.h>
 #include <libnr/nr-matrix.h>
 #include <libnr/nr-matrix-fns.h>
+#include <libnr/nr-matrix-ops.h>
+#include <libnr/nr-rotate.h>
+#include <libnr/nr-rotate-ops.h>
+#include <libnr/nr-scale-ops.h>
+#include <libnr/nr-translate.h>
+#include <libnr/nr-translate-ops.h>
 using NR::Matrix;
 using NR::X;
 using NR::Y;
-
-#include <libnr/nr-point-fns.cpp> // bad!
-#include <libnr/nr-matrix-fns.cpp> // bad!
 
 inline bool point_equalp(NR::Point const &a, NR::Point const &b)
 {
@@ -14,156 +17,13 @@ inline bool point_equalp(NR::Point const &a, NR::Point const &b)
 		 NR_DF_TEST_CLOSE(a[Y], b[Y], 1e-5) );
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int rc = EXIT_SUCCESS;
 
 	Matrix const m_id(NR::identity());
-
-	utest_start("NR::scale");
-
-	NR::scale const sa(1.5, 2.0);
-	UTEST_TEST("x,y constructor and operator[] const") {
-		UTEST_ASSERT(sa[X] == 1.5);
-		UTEST_ASSERT(sa[Y] == 2.0);
-		UTEST_ASSERT(sa[0u] == 1.5);
-		UTEST_ASSERT(sa[1u] == 2.0);
-	}
-
-	NR::Point const b(-2.0, 3.0);
-	NR::scale const sb(b);
-
-	UTEST_TEST("copy constructor, operator==, operator!=") {
-		NR::scale const sa_copy(sa);
-		UTEST_ASSERT( sa == sa_copy );
-		UTEST_ASSERT(!( sa != sa_copy ));
-		UTEST_ASSERT( sa != sb );
-	}
-
-	UTEST_TEST("operator=") {
-		NR::scale sa_eq(sb);
-		sa_eq = sa;
-		UTEST_ASSERT( sa == sa_eq );
-	}
-
-	UTEST_TEST("point constructor") {
-		UTEST_ASSERT(sb[X] == b[X]);
-		UTEST_ASSERT(sb[Y] == b[Y]);
-	}
-
-	UTEST_TEST("operator*(Point, scale)") {
-		NR::Point const ab( b * sa );
-		UTEST_ASSERT( ab == NR::Point(-3.0, 6.0) );
-	}
-
-	UTEST_TEST("operator*(scale, scale)") {
-		NR::scale const sab( sa * sb );
-		UTEST_ASSERT( sab == NR::scale(-3.0, 6.0) );
-	}
-
-	UTEST_TEST("operator/(scale, scale)") {
-		NR::scale const sa_b( sa / sb );
-		UTEST_ASSERT( sa_b == NR::scale(-0.75, 2./3.) );
-	}
-
-	if (!utest_end()) {
-		rc = EXIT_FAILURE;
-	}
-
-
-	utest_start("rotate");
-
-	NR::rotate const r_id(0.0);
-	NR::rotate const rot234(.234);
-	UTEST_TEST("constructors, comparisons") {
-		UTEST_ASSERT( r_id == r_id );
-		UTEST_ASSERT( rot234 == rot234 );
-		UTEST_ASSERT( rot234 != r_id );
-		UTEST_ASSERT( r_id == NR::rotate(NR::Point(1.0, 0.0)) );
-		UTEST_ASSERT( Matrix(r_id) == m_id );
-		UTEST_ASSERT( Matrix(r_id).test_identity() );
-
-		UTEST_ASSERT( rot234 == NR::rotate(NR::Point(cos(.234), sin(.234))) );
-	}
-
-	UTEST_TEST("operator=") {
-		NR::rotate rot234_eq(r_id);
-		rot234_eq = rot234;
-		UTEST_ASSERT( rot234 == rot234_eq );
-		UTEST_ASSERT( rot234_eq != r_id );
-	}
-
-	UTEST_TEST("inverse") {
-		UTEST_ASSERT( r_id.inverse() == r_id );
-		UTEST_ASSERT( rot234.inverse() == NR::rotate(-.234) );
-	}
-
-	NR::rotate const rot180(NR::Point(-1.0, 0.0));
-	UTEST_TEST("operator*(Point, rotate)") {
-		UTEST_ASSERT( b * r_id == b );
-		UTEST_ASSERT( b * rot180 == -b );
-		UTEST_ASSERT( b * rot234 == b * Matrix(rot234) );
-	}
-
-	UTEST_TEST("operator*(rotate, rotate)") {
-		UTEST_ASSERT( r_id * r_id == r_id );
-		UTEST_ASSERT( rot180 * rot180 == r_id );
-		UTEST_ASSERT( rot234 * r_id == rot234 );
-		UTEST_ASSERT( r_id * rot234 == rot234 );
-		UTEST_ASSERT(matrix_equalp(rot234 * rot234.inverse(), r_id, 1e-14));
-		UTEST_ASSERT(matrix_equalp(rot234.inverse() * rot234, r_id, 1e-14));
-		UTEST_ASSERT(point_equalp(( NR::rotate(0.25) * NR::rotate(.5) ).vec, NR::rotate(.75).vec));
-	}
-
-	UTEST_TEST("operator/(rotate, rotate)") {
-		UTEST_ASSERT( rot234 / r_id == rot234 );
-		UTEST_ASSERT( rot234 / rot180 == rot234 * rot180 );
-		UTEST_ASSERT(matrix_equalp(rot234 / rot234, r_id, 1e-14));
-		UTEST_ASSERT(matrix_equalp(r_id / rot234, rot234.inverse(), 1e-14));
-	}
-
-	if (!utest_end()) {
-		rc = EXIT_FAILURE;
-	}
-
-
-	utest_start("translate");
-
-	NR::translate const tb(b);
-	NR::translate const tc(-3.0, -2.0);
-	UTEST_TEST("constructors, operator[]") {
-		UTEST_ASSERT( tc[X] == -3.0 && tc[Y] == -2.0 );
-		UTEST_ASSERT( tb[0] == b[X] && tb[1] == b[Y] );
-	}
-
-	UTEST_TEST("operator=") {
-		NR::translate tb_eq(tc);
-		tb_eq = tb;
-		UTEST_ASSERT( tb == tb_eq );
-		UTEST_ASSERT( tb_eq != tc );
-	}
-
-	NR::translate const tbc( tb * tc );
-	UTEST_TEST("operator*(translate, translate)") {
-		UTEST_ASSERT( tbc.offset == NR::Point(-5.0, 1.0) );
-		UTEST_ASSERT( tbc.offset == ( tc * tb ).offset );
-		UTEST_ASSERT( Matrix(tbc) == Matrix(tb) * Matrix(tc) );
-	}
-
-	UTEST_TEST("operator*(Point, translate)") {
-		UTEST_ASSERT( tbc.offset == b * tc );
-		UTEST_ASSERT( b * tc == b * Matrix(tc) );
-	}
-
-	NR::translate const t_id(0.0, 0.0);
-	UTEST_TEST("identity") {
-		UTEST_ASSERT( b * t_id == b );
-		UTEST_ASSERT( Matrix(t_id) == m_id );
-	}
-
-	if (!utest_end()) {
-		rc = EXIT_FAILURE;
-	}
-
+	NR::rotate const r_id(NR::Point(1, 0));
+	NR::translate const t_id(0, 0);
 
 	utest_start("Matrix");
 
@@ -190,9 +50,9 @@ int main(int argc, char *argv[]) {
 		UTEST_ASSERT( p * ms == NR::Point(10.0, 21.0) );
 	}
 
+	NR::rotate const r86(NR::Point(.8, .6));
+	NR::Matrix const mr86(r86);
 	UTEST_TEST("rotate constructor") {
-		NR::rotate const r86(NR::Point(.8, .6));
-		NR::Matrix const mr86(r86);
 		NR::Point const p0(1.0, 0.0);
 		NR::Point const p90(0.0, 1.0);
 		UTEST_ASSERT( p0 * r86 == NR::Point(.8, .6) );
@@ -207,6 +67,7 @@ int main(int argc, char *argv[]) {
 	NR::translate const t23(2.0, 3.0);
 	UTEST_TEST("translate constructor") {
 		NR::Matrix const mt23(t23);
+		NR::Point const b(-2.0, 3.0);
 		UTEST_ASSERT( b * t23 == b * mt23 );
 	}
 
@@ -214,8 +75,9 @@ int main(int argc, char *argv[]) {
 	UTEST_TEST("test_identity") {
 		UTEST_ASSERT(m_id.test_identity());
 		UTEST_ASSERT(Matrix(t_id).test_identity());
-		UTEST_ASSERT(!(Matrix(tb).test_identity()));
+		UTEST_ASSERT(!(Matrix(NR::translate(-2, 3)).test_identity()));
 		UTEST_ASSERT(Matrix(r_id).test_identity());
+		NR::rotate const rot180(NR::Point(-1, 0));
 		UTEST_ASSERT(!(Matrix(rot180).test_identity()));
 		UTEST_ASSERT(Matrix(s_id).test_identity());
 		UTEST_ASSERT(!(Matrix(NR::scale(1.0, 0.0)).test_identity()));
@@ -239,12 +101,50 @@ int main(int argc, char *argv[]) {
 		NR::Matrix const invaff = aff.inverse();
 		UTEST_ASSERT( invaff[1] == -1.0 );
 		
-		NR::Matrix ef = elliptic_quadratic_form(invaff);
-		for(int i = 0; i < 3; i++)
-			g_print("%f %f\n", ef[i*2], ef[i*2+1]);
-		
-		
+		NR::Matrix const ef(elliptic_quadratic_form(invaff));
+		NR::Matrix const exp_ef(2, -1,
+					-1, 1,
+					0, 0);
+		UTEST_ASSERT( ef == exp_ef );
 	}
+
+	UTEST_TEST("Matrix * rotate") {
+		NR::Matrix const ma(2.0, -1.0,
+				    4.0, 4.0,
+				    -0.5, 2.0);
+		NR::Matrix const a_r86( ma * r86 );
+		NR::Matrix const ma1( a_r86 * r86.inverse() );
+		UTEST_ASSERT(matrix_equalp(ma1, ma, 1e-12));
+		NR::Matrix const exp_a_r86( 2*.8 + -1*-.6,  2*.6 + -1*.8,
+					    4*.8 + 4*-.6,   4*.6 + 4*.8,
+					    -.5*.8 + 2*-.6, -.5*.6 + 2*.8 );
+		UTEST_ASSERT(matrix_equalp(a_r86, exp_a_r86, 1e-12));
+	}
+
+	UTEST_TEST("translate*scale, scale*translate") {
+		NR::translate const t2n4(2, -4);
+		NR::scale const sn2_8(-2, 8);
+		NR::Matrix const exp_ts(-2, 0,
+					0,  8,
+					-4, -32);
+		NR::Matrix const exp_st(-2, 0,
+					0,  8,
+					2, -4);
+		UTEST_ASSERT( exp_ts == t2n4 * sn2_8 );
+		UTEST_ASSERT( exp_st == sn2_8 * t2n4 );
+	}
+
+	UTEST_TEST("Matrix * scale") {
+		NR::Matrix const ma(2.0, -1.0,
+				    4.0, 4.0,
+				    -0.5, 2.0);
+		NR::scale const sn2_8(-2, 8);
+		NR::Matrix const exp_as(-4, -8,
+					-8, 32,
+					1,  16);
+		UTEST_ASSERT( ma * sn2_8 == exp_as );
+	}
+
 	if (!utest_end()) {
 		rc = EXIT_FAILURE;
 	}
