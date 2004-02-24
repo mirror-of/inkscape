03d9420dbb59ac668213cc5a5269d9faa6474672
Peter Moulder <peter.moulder@monash.edu>
2004-02-24 10:31:50 +0000

    cleanup: whitespace & local var renames only
    (bzr r1666)

--- src/libnr/nr-matrix-ops.h
+++ src/libnr/nr-matrix-ops.h
@@ -12,97 +12,100 @@ namespace NR {
 
 inline bool operator==(Matrix const &a, Matrix const &b)
 {
-	for(unsigned i = 0; i < 6; ++i) {
-		if (a[i] != b[i]) {
-			return false;
-		}
-	}
-	return true;
+    for(unsigned i = 0; i < 6; ++i) {
+        if ( a[i] != b[i] ) {
+            return false;
+        }
+    }
+    return true;
 }
 
 inline bool operator!=(Matrix const &a, Matrix const &b)
 {
-	return !(a == b);
+    return !( a == b );
 }
 
 inline Point operator*(Point const &v, Matrix const &m)
 {
-	NR::Point const m_xform_col_0(m[0], m[2]);
-	NR::Point const m_xform_col_1(m[1], m[3]);
-	NR::Point const m_xlate(m[4], m[5]);
-	return ( Point(dot(v, m_xform_col_0),
-		       dot(v, m_xform_col_1))
-		 + m_xlate );
-/*
-	return Point(v[X] * m[0]  +  v[Y] * m[2]  +  m[4],
-		     v[X] * m[1]  +  v[Y] * m[3]  +  m[5]);
-*/
+#if 1  /* Which code makes it easier to see what's happening? */
+    NR::Point const xform_col0(m[0],
+                               m[2]);
+    NR::Point const xform_col1(m[1],
+                               m[3]);
+    NR::Point const xlate(m[4], m[5]);
+    return ( Point(dot(v, xform_col0),
+                   dot(v, xform_col1))
+             + xlate );
+#else
+    return Point(v[X] * m[0]  +  v[Y] * m[2]  +  m[4],
+                 v[X] * m[1]  +  v[Y] * m[3]  +  m[5]);
+#endif
 }
 
 inline Point &Point::operator*=(Matrix const &m)
 {
-	*this = *this * m;
-	return *this;
+    *this = *this * m;
+    return *this;
 }
 
 Matrix operator*(Matrix const &a, Matrix const &b);
 
 inline Matrix operator*(Matrix const &m, rotate const &r)
 {
-	return m * Matrix(r);
+    return m * Matrix(r);
 }
 
 inline Matrix operator*(Matrix const &m, translate const &t)
 {
-	Matrix ret(m);
-	ret[4] += t[X];
-	ret[5] += t[Y];
-	assert_close( ret, m * Matrix(t) );
-	return ret;
+    Matrix ret(m);
+    ret[4] += t[X];
+    ret[5] += t[Y];
+    assert_close( ret, m * Matrix(t) );
+    return ret;
 }
 
 inline Matrix operator*(translate const &t, Matrix const &m)
 {
-	Matrix ret(m);
-	ret[4] += m[0] * t[X] + m[2] * t[Y];
-	ret[5] += m[1] * t[X] + m[3] * t[Y];
-	assert_close( ret, Matrix(t) * m );
-	return ret;
+    Matrix ret(m);
+    ret[4] += m[0] * t[X] + m[2] * t[Y];
+    ret[5] += m[1] * t[X] + m[3] * t[Y];
+    assert_close( ret, Matrix(t) * m );
+    return ret;
 }
 
 inline Matrix operator*(translate const &t, scale const &s)
 {
-	Matrix ret(s);
-	ret[4] = t[X] * s[X];
-	ret[5] = t[Y] * s[Y];
-	assert_close( ret, Matrix(t) * Matrix(s) );
-	return ret;
+    Matrix ret(s);
+    ret[4] = t[X] * s[X];
+    ret[5] = t[Y] * s[Y];
+    assert_close( ret, Matrix(t) * Matrix(s) );
+    return ret;
 }
 
 inline Matrix operator*(scale const &s, translate const &t)
 {
-	return Matrix(s) * t;
+    return Matrix(s) * t;
 }
 
 inline Matrix operator*(Matrix const &m, scale const &s)
 {
-	Matrix ret(m);
-	ret[0] *= s[X]; ret[1] *= s[Y];
-	ret[2] *= s[X]; ret[3] *= s[Y];
-	ret[4] *= s[X]; ret[5] *= s[Y];
-	assert_close( ret, m * Matrix(s) );
-	return ret;
+    Matrix ret(m);
+    ret[0] *= s[X]; ret[1] *= s[Y];
+    ret[2] *= s[X]; ret[3] *= s[Y];
+    ret[4] *= s[X]; ret[5] *= s[Y];
+    assert_close( ret, m * Matrix(s) );
+    return ret;
 }
 
 inline Matrix operator*(scale const &s, Matrix const &m)
 {
-	Matrix ret(m);
-	ret[0] *= s[X];
-	ret[1] *= s[X];
-	ret[2] *= s[Y];
-	ret[3] *= s[Y];
-	assert_close( ret, Matrix(s) * m );
-	return ret;
+    Matrix ret(m);
+    ret[0] *= s[X];
+    ret[1] *= s[X];
+    ret[2] *= s[Y];
+    ret[3] *= s[Y];
+    assert_close( ret, Matrix(s) * m );
+    return ret;
 }
 
 } /* namespace NR */
