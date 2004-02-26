d7300b928f56a1ef501f3113d87c1b9cd7dc6b3f
Peter Moulder <peter.moulder@monash.edu>
2004-02-26 09:49:05 +0000

    Move vim modeline to last line.
    (bzr r1689)

--- src/marker-status.h
+++ src/marker-status.h
@@ -13,6 +13,5 @@ void marker_status(char const *format, ...) __attribute__((format(__printf__, 1,
   indent-tabs-mode:nil
   fill-column:99
   End:
-  vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
 */
-
+// vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
