b3501e478c1308f7c09be4ac9ef4fe736c35591a
bulia byak <buliabyak@gmail.com>
2004-03-20 05:23:48 +0000

    fix for 915440 (from sodi)
    (bzr r1978)

--- src/libnrtype/nr-type-w32.cpp
+++ src/libnrtype/nr-type-w32.cpp
@@ -179,7 +179,7 @@ nr_typeface_w32_setup (NRTypeFace *tface, NRTypeFaceDef *def)
 
     tfw32->fonts = NULL;
     tfw32->logfont = (LOGFONT *)g_hash_table_lookup (namedict, def->name);
-    tfw32->logfont->lfHeight = 1000;
+    tfw32->logfont->lfHeight = -1000;
     tfw32->logfont->lfWidth = 0;
     tfw32->hfont = CreateFontIndirect (tfw32->logfont);
 
