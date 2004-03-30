96c1dfcba4f8b80b04c5f330752f8afce9b5b816
bulia byak <buliabyak@gmail.com>
2004-03-30 18:03:38 +0000

    label edits
    (bzr r2065)

--- src/extension/internal/ps.cpp
+++ src/extension/internal/ps.cpp
@@ -131,16 +131,16 @@ PrintPS::setup (Inkscape::Extension::Print * mod)
 	mod->get_param("bitmap", &p2bm);
 	rb = gtk_radio_button_new_with_label (NULL, _("Print using PostScript operators"));
 	gtk_tooltips_set_tip ((GtkTooltips *) tt, rb,
-						  _("Use PostScript vector operators. The resulting image will be (usually) smaller "
-						    "and can be arbitrarily scaled, but alpha transparency, "
-							"gradients, markers, and patterns will be lost"), NULL);
+						  _("Use PostScript vector operators. The resulting image is usually smaller "
+						    "in file size and can be arbitrarily scaled, but alpha transparency, "
+							"gradients, markers, and patterns will be lost."), NULL);
 	if (!p2bm) gtk_toggle_button_set_active ((GtkToggleButton *) rb, TRUE);
 	gtk_box_pack_start (GTK_BOX (vb), rb, FALSE, FALSE, 0);
 	rb = gtk_radio_button_new_with_label (gtk_radio_button_get_group ((GtkRadioButton *) rb), _("Print as bitmap"));
 	gtk_tooltips_set_tip ((GtkTooltips *) tt, rb,
-						  _("Print everything as bitmap. The resulting image will be (usually) larger "
-						    "and cannot be arbitrarily scaled without quality loss, but all objects "
-							"will be rendered exactly as displayed"), NULL);
+						  _("Print everything as bitmap. The resulting image is usually larger "
+						    "in file size and cannot be arbitrarily scaled without quality loss, "
+							"but all objects will be rendered exactly as displayed."), NULL);
 	if (p2bm) gtk_toggle_button_set_active ((GtkToggleButton *) rb, TRUE);
 	gtk_box_pack_start (GTK_BOX (vb), rb, FALSE, FALSE, 0);
 	/* Resolution */
