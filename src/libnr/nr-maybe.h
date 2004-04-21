ec09faf3661f696e265472f851648a4a04b9d326
Bob Jamison <ishmalius@gmail.com>
2004-04-21 14:48:10 +0000

    tweak to make compilable on 3.4
    (bzr r2395)

--- src/libnr/nr-maybe.h
+++ src/libnr/nr-maybe.h
@@ -18,13 +18,14 @@
 
 #include <stdexcept>
 #include <typeinfo>
+#include <string>
 
 namespace NR {
 
 /** An exception class for run-time type errors */
 template <typename T>
 class IsNot : public std::domain_error {
-    IsNot() : domain_error(string("Is not ") + typeid(T).name()) {}
+    IsNot() : domain_error(std::string("Is not ") + typeid(T).name()) {}
 };
 
 /** A type with only one value, which (in principle) is only equal to itself.
