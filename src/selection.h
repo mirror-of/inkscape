2852521dd5350cace9aae5c2b610170d10515278
MenTaLguY <mental@rydia.net>
2004-04-21 17:23:51 +0000

    marked old selection API deprecated
    (bzr r2396)

--- src/selection.h
+++ src/selection.h
@@ -64,8 +64,11 @@ public:
 	NRRect *boundsInDocument(NRRect *dest) const;
 	NR::Rect boundsInDocument() const;
 
+	/* Returns number of points used */
 	int getSnapPoints(NR::Point points[], int max_points) const;
 
+	/* TODO: these two should not be part of the public interface */
+	/* (in fact, updateStatusbar does not belong here at all) */
 	void updateStatusbar();
 	void invokeChanged();
 
@@ -106,95 +109,141 @@ struct SPSelectionClass {
 	GObjectClass parent_class;
 };
 
-/* Constructor */
 
-inline SPSelection *sp_selection_new(SPDesktop *desktop) {
+/* these wrappers are all obsolete and should be replaced with their
+ * contents at their call sites */
+
+inline __attribute__ ((deprecated))
+SPSelection *sp_selection_new(SPDesktop *desktop)
+{
 	return new SPSelection(desktop);
 }
 
-/* This are private methods & will be removed from this file */
-
-inline void sp_selection_changed(SPSelection *selection) {
+inline __attribute__ ((deprecated))
+void sp_selection_changed(SPSelection *selection)
+{
 	selection->invokeChanged();
 }
-inline void sp_selection_update_statusbar(SPSelection *selection) {
+inline __attribute__ ((deprecated))
+void sp_selection_update_statusbar(SPSelection *selection)
+{
 	selection->updateStatusbar();
 }
 
-inline gboolean sp_selection_item_selected(SPSelection const *selection,
-		                           SPItem *item)
+inline __attribute__ ((deprecated))
+gboolean sp_selection_item_selected(SPSelection const *selection,
+		                    SPItem *item)
 {
 	return selection->includesItem(item);
 }
-inline gboolean sp_selection_repr_selected(SPSelection const *selection,
-                                           SPRepr *repr)
+inline __attribute__ ((deprecated))
+gboolean sp_selection_repr_selected(SPSelection const *selection,
+                                    SPRepr *repr)
 {
 	return selection->includesRepr(repr);
 }
-inline gboolean sp_selection_is_empty(SPSelection const *selection) {
+
+inline __attribute__ ((deprecated))
+gboolean sp_selection_is_empty(SPSelection const *selection)
+{
 	return selection->isEmpty();
 }
 
-inline void sp_selection_add_item(SPSelection *selection, SPItem *item) {
+inline __attribute__ ((deprecated))
+void sp_selection_add_item(SPSelection *selection, SPItem *item)
+{
 	selection->addItem(item);
 }
-inline void sp_selection_add_repr(SPSelection *selection, SPRepr *repr) {
+inline __attribute__ ((deprecated))
+void sp_selection_add_repr(SPSelection *selection, SPRepr *repr)
+{
 	selection->addRepr(repr);
 }
-inline void sp_selection_set_item(SPSelection *selection, SPItem *item) {
+inline __attribute__ ((deprecated))
+void sp_selection_set_item(SPSelection *selection, SPItem *item)
+{
 	selection->setItem(item);
 }
-inline void sp_selection_set_repr(SPSelection *selection, SPRepr *repr) {
+inline __attribute__ ((deprecated))
+void sp_selection_set_repr(SPSelection *selection, SPRepr *repr)
+{
 	selection->setRepr(repr);
 }
-inline void sp_selection_remove_item(SPSelection *selection, SPItem *item) {
+inline __attribute__ ((deprecated))
+void sp_selection_remove_item(SPSelection *selection, SPItem *item)
+{
 	selection->removeItem(item);
 }
-inline void sp_selection_remove_repr(SPSelection *selection, SPRepr *repr) {
+inline __attribute__ ((deprecated))
+void sp_selection_remove_repr(SPSelection *selection, SPRepr *repr)
+{
 	selection->removeRepr(repr);
 }
-inline void sp_selection_set_item_list(SPSelection *selection, GSList const *list) {
+inline __attribute__ ((deprecated))
+void sp_selection_set_item_list(SPSelection *selection, GSList const *list)
+{
 	selection->setItemList(list);
 }
-inline void sp_selection_set_repr_list(SPSelection *selection, GSList const *list) {
+inline __attribute__ ((deprecated))
+void sp_selection_set_repr_list(SPSelection *selection, GSList const *list)
+{
 	selection->setReprList(list);
 }
-inline void sp_selection_empty(SPSelection *selection) {
+inline __attribute__ ((deprecated))
+void sp_selection_empty(SPSelection *selection)
+{
 	selection->clear();
 }
-inline GSList const *sp_selection_item_list (SPSelection *selection) {
+inline __attribute__ ((deprecated))
+GSList const *sp_selection_item_list (SPSelection *selection)
+{
 	g_return_val_if_fail(selection != NULL, NULL);
 	return selection->itemList();
 }
-inline GSList const *sp_selection_repr_list (SPSelection *selection) {
+inline __attribute__ ((deprecated))
+GSList const *sp_selection_repr_list (SPSelection *selection)
+{
 	g_return_val_if_fail(selection != NULL, NULL);
 	return selection->reprList();
 }
-inline SPItem *sp_selection_item (SPSelection *selection) {
+inline __attribute__ ((deprecated))
+SPItem *sp_selection_item (SPSelection *selection)
+{
 	g_return_val_if_fail(selection != NULL, NULL);
 	return selection->singleItem();
 }
-inline SPRepr *sp_selection_repr (SPSelection *selection) {
+inline __attribute__ ((deprecated))
+SPRepr *sp_selection_repr (SPSelection *selection)
+{
 	g_return_val_if_fail(selection != NULL, NULL);
 	return selection->singleRepr();
 }
 
-inline NRRect *sp_selection_bbox (SPSelection *selection, NRRect *bbox) {
+inline __attribute__ ((deprecated))
+NRRect *sp_selection_bbox (SPSelection *selection, NRRect *bbox)
+{
 	return selection->bounds(bbox);
 }
-inline NR::Rect sp_selection_bbox (SPSelection *selection) {
+inline __attribute__ ((deprecated))
+NR::Rect sp_selection_bbox (SPSelection *selection)
+{
 	return selection->bounds();
 }
-inline NRRect *sp_selection_bbox_document (SPSelection *selection, NRRect *bbox) {
+inline __attribute__ ((deprecated))
+NRRect *sp_selection_bbox_document (SPSelection *selection,
+                                           NRRect *bbox)
+{
 	return selection->boundsInDocument(bbox);
 }
-inline NR::Rect sp_selection_bbox_document (SPSelection *selection) {
+inline __attribute__ ((deprecated))
+NR::Rect sp_selection_bbox_document (SPSelection *selection)
+{
 	return selection->boundsInDocument();
 }
 
-/* Returns number of points used */
-inline int sp_selection_snappoints(SPSelection *selection,
-                                   NR::Point points[], int size)
+inline __attribute__ ((deprecated))
+int sp_selection_snappoints(SPSelection *selection,
+                            NR::Point points[], int size)
 {
 	return selection->getSnapPoints(points, size);
 }
