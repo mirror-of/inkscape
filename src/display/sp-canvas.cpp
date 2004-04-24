5b893b8ad85fabd9af8e547c5e6670e54ed77729
Peter Moulder <peter.moulder@monash.edu>
2004-04-24 04:40:21 +0000

    Specify directory of sp-canvas.h.
    (bzr r2447)

--- src/display/sp-canvas.cpp
+++ src/display/sp-canvas.cpp
@@ -25,8 +25,7 @@
 #include <gtk/gtksignal.h>
 
 #include <helper/sp-marshal.h>
-
-#include <sp-canvas.h>
+#include <display/sp-canvas.h>
 #include <libnr/nr-matrix-fns.h>
 #include <libnr/nr-matrix-ops.h>
 #include <libnr/nr-convex-hull.h>
