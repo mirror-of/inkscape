49f4e1d968a13545e632670ad976e3ff7c8c79af
Peter Moulder <peter.moulder@monash.edu>
2004-05-16 09:52:22 +0000

    Cleanup: greater conformance to CodingStyle.
    (bzr r2743)

--- src/sp-defs.h
+++ src/sp-defs.h
@@ -1,5 +1,5 @@
-#ifndef __SP_DEFS_H__
-#define __SP_DEFS_H__
+#ifndef SEEN_SP_DEFS_H
+#define SEEN_SP_DEFS_H
 
 /*
  * SVG <defs> implementation
@@ -14,20 +14,32 @@
 
 #include "sp-object.h"
 
-#define SP_TYPE_DEFS            (sp_defs_get_type ())
-#define SP_DEFS(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), SP_TYPE_DEFS, SPDefs))
-#define SP_DEFS_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), SP_TYPE_DEFS, SPDefsClass))
-#define SP_IS_DEFS(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), SP_TYPE_DEFS))
-#define SP_IS_DEFS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), SP_TYPE_DEFS))
+#define SP_TYPE_DEFS            (sp_defs_get_type())
+#define SP_DEFS(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), SP_TYPE_DEFS, SPDefs))
+#define SP_DEFS_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), SP_TYPE_DEFS, SPDefsClass))
+#define SP_IS_DEFS(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), SP_TYPE_DEFS))
+#define SP_IS_DEFS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), SP_TYPE_DEFS))
 
 struct SPDefs {
-	SPObject object;
+    SPObject object;
 };
 
 struct SPDefsClass {
-	SPObjectClass parent_class;
+    SPObjectClass parent_class;
 };
 
-GType sp_defs_get_type (void);
+GType sp_defs_get_type(void);
 
-#endif
+
+#endif /* !SEEN_SP_DEFS_H */
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
