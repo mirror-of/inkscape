3f2d9a77bfe8ff15a8d41afcd14e8fa8fbaa5507
Peter Moulder <peter.moulder@monash.edu>
2004-05-23 12:22:41 +0000

    #include <gtk/gtktooltips.h>
    (bzr r2823)

--- src/widgets/sp-color-scales.h
+++ src/widgets/sp-color-scales.h
@@ -1,23 +1,23 @@
-#ifndef __SP_COLOR_SCALES_H__
-#define __SP_COLOR_SCALES_H__
+#ifndef SEEN_SP_COLOR_SCALES_H
+#define SEEN_SP_COLOR_SCALES_H
 
+#include <glib/gtypes.h>
+#include <gtk/gtktooltips.h>
 #include <gtk/gtkvbox.h>
-#include "../color.h"
-#include "sp-color-slider.h"
-#include "sp-color-selector.h"
 
-#include <glib.h>
-
+#include <color.h>
+#include <widgets/sp-color-slider.h>
+#include <widgets/sp-color-selector.h>
 
 
 struct SPColorScales;
 struct SPColorScalesClass;
 
 typedef enum {
-	SP_COLOR_SCALES_MODE_NONE = 0,
-	SP_COLOR_SCALES_MODE_RGB = 1,
-	SP_COLOR_SCALES_MODE_HSV = 2,
-	SP_COLOR_SCALES_MODE_CMYK = 3
+    SP_COLOR_SCALES_MODE_NONE = 0,
+    SP_COLOR_SCALES_MODE_RGB = 1,
+    SP_COLOR_SCALES_MODE_HSV = 2,
+    SP_COLOR_SCALES_MODE_CMYK = 3
 } SPColorScalesMode;
 
 
@@ -25,12 +25,12 @@ typedef enum {
 class ColorScales: public ColorSelector
 {
 public:
-    ColorScales( SPColorSelector* csel );
+    ColorScales(SPColorSelector *csel);
     virtual ~ColorScales();
 
     virtual void init();
 
-    virtual void setSubmode( guint submode );
+    virtual void setSubmode(guint submode);
     virtual guint getSubmode() const;
 
     void setMode(SPColorScalesMode mode);
@@ -38,58 +38,69 @@ public:
 
 
 protected:
-    virtual void _colorChanged( const SPColor& color, gfloat alpha );
+    virtual void _colorChanged(SPColor const &color, gfloat alpha);
 
-    static void _adjustmentAnyChanged ( GtkAdjustment *adjustment, SPColorScales *cs );
-    static void _sliderAnyGrabbed( SPColorSlider *slider, SPColorScales *cs );
-    static void _sliderAnyReleased( SPColorSlider *slider, SPColorScales *cs );
-    static void _sliderAnyChanged( SPColorSlider *slider, SPColorScales *cs );
-    static void _adjustmentChanged( SPColorScales *cs, guint channel );
+    static void _adjustmentAnyChanged(GtkAdjustment *adjustment, SPColorScales *cs);
+    static void _sliderAnyGrabbed(SPColorSlider *slider, SPColorScales *cs);
+    static void _sliderAnyReleased(SPColorSlider *slider, SPColorScales *cs);
+    static void _sliderAnyChanged(SPColorSlider *slider, SPColorScales *cs);
+    static void _adjustmentChanged(SPColorScales *cs, guint channel);
 
-    void _getRgbaFloatv( gfloat *rgba );
-    void _getCmykaFloatv( gfloat *cmyka );
+    void _getRgbaFloatv(gfloat *rgba);
+    void _getCmykaFloatv(gfloat *cmyka);
     guint32 _getRgba32();
-    void _updateSliders( guint channels );
-    void _recalcColor( gboolean changing );
+    void _updateSliders(guint channels);
+    void _recalcColor(gboolean changing);
 
     SPColorScalesMode _mode;
     gboolean _updating : 1;
     gboolean _dragging : 1;
-    GtkAdjustment* _a[5]; /* Channel adjustments */
-    GtkWidget* _s[5]; /* Channel sliders */
-    GtkWidget* _b[5]; /* Spinbuttons */
-    GtkWidget* _l[5]; /* Labels */
+    GtkAdjustment *_a[5]; /* Channel adjustments */
+    GtkWidget *_s[5]; /* Channel sliders */
+    GtkWidget *_b[5]; /* Spinbuttons */
+    GtkWidget *_l[5]; /* Labels */
 
-    GtkTooltips* _tt; /* tooltip container */
+    GtkTooltips *_tt; /* tooltip container */
 
 private:
     // By default, disallow copy constructor and assignment operator
-    ColorScales( const ColorScales& obj );
-    ColorScales& operator=( const ColorScales& obj );
+    ColorScales(ColorScales const &obj);
+    ColorScales &operator=(ColorScales const &obj );
 };
 
 
 
-#define SP_TYPE_COLOR_SCALES (sp_color_scales_get_type ())
-#define SP_COLOR_SCALES(o) (GTK_CHECK_CAST ((o), SP_TYPE_COLOR_SCALES, SPColorScales))
-#define SP_COLOR_SCALES_CLASS(k) (GTK_CHECK_CLASS_CAST ((k), SP_TYPE_COLOR_SCALES, SPColorScalesClass))
-#define SP_IS_COLOR_SCALES(o) (GTK_CHECK_TYPE ((o), SP_TYPE_COLOR_SCALES))
-#define SP_IS_COLOR_SCALES_CLASS(k) (GTK_CHECK_CLASS_TYPE ((k), SP_TYPE_COLOR_SCALES))
+#define SP_TYPE_COLOR_SCALES (sp_color_scales_get_type())
+#define SP_COLOR_SCALES(o) (GTK_CHECK_CAST((o), SP_TYPE_COLOR_SCALES, SPColorScales))
+#define SP_COLOR_SCALES_CLASS(k) (GTK_CHECK_CLASS_CAST((k), SP_TYPE_COLOR_SCALES, SPColorScalesClass))
+#define SP_IS_COLOR_SCALES(o) (GTK_CHECK_TYPE((o), SP_TYPE_COLOR_SCALES))
+#define SP_IS_COLOR_SCALES_CLASS(k) (GTK_CHECK_CLASS_TYPE((k), SP_TYPE_COLOR_SCALES))
 
 struct SPColorScales {
-	SPColorSelector parent;
+    SPColorSelector parent;
 };
 
 struct SPColorScalesClass {
-	SPColorSelectorClass parent_class;
+    SPColorSelectorClass parent_class;
 };
 
-GType sp_color_scales_get_type (void);
+GType sp_color_scales_get_type();
 
-GtkWidget *sp_color_scales_new (void);
+GtkWidget *sp_color_scales_new();
 
-gfloat get1( const GtkAdjustment *a );
-void set255( GtkAdjustment *a, gfloat v );
+gfloat get1(GtkAdjustment const *a);
+void set255(GtkAdjustment *a, gfloat v);
 
 
-#endif
+#endif /* !SEEN_SP_COLOR_SCALES_H */
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
