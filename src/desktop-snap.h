3bdbb34583c107699b49407e4513c33756b04a92
Peter Moulder <peter.moulder@monash.edu>
2004-06-17 12:48:35 +0000

    Add some missing #include's.  Replace SPDesktop and NR::scale declarations with #include forward.h, libnr/nr-forward.h. whitespace changes according to CodingStyle.
    (bzr r3051)

--- src/desktop-snap.h
+++ src/desktop-snap.h
@@ -1,5 +1,5 @@
-#ifndef __SP_DESKTOP_SNAP_H__
-#define __SP_DESKTOP_SNAP_H__
+#ifndef SEEN_DESKTOP_SNAP_H
+#define SEEN_DESKTOP_SNAP_H
 
 /*
  * Snap distance calculation
@@ -16,67 +16,92 @@
 
 #include <vector>
 
-struct SPDesktop;
-namespace NR { class scale; }
+#include <forward.h>
+#include <libnr/nr-coord.h>
+#include <libnr/nr-dim2.h>
+#include <libnr/nr-forward.h>
+
 
 /* FIXME: these classes are work-in-progress */
+/* Consider moving Snapper etc. to separate files if this reduces the number of translation units
+ * that #include these class definitions.  Doing so reduces the amount of work of the compiler, and
+ * reduces the number of recompilations necessary.
+ *
+ * Also, I'd suggest use of the standard modeline stuff (see end of this file) to make it easier to
+ * conform to Inkscape's indentation standards.
+ *
+ * -- pjrm
+ */
 
 ///< Parent for classes that can snap points to something
 class Snapper
 {
- public:
-  Snapper(NR::Coord const d);
+public:
+    Snapper(NR::Coord const d);
 
-  void setDistance(NR::Coord const d);
-  NR::Coord getDistance() const;
+    void setDistance(NR::Coord const d);
 
- private:
-  NR::Coord _distance;
+    NR::Coord getDistance() const;
+
+private:
+    NR::Coord _distance;
 };
 
 ///< Snap to grid
 class GridSnapper : public Snapper
 {
- public:
-  GridSnapper(NR::Coord const d);
+public:
+    GridSnapper(NR::Coord const d);
 };
 
 ///< Snap to guides
 class GuideSnapper : public Snapper
 {
- public:
-  GuideSnapper(NR::Coord const d);
+public:
+    GuideSnapper(NR::Coord const d);
 };
 
 
 /* Single point methods */
-double sp_desktop_free_snap (SPDesktop const *desktop, NR::Point &req);
-double sp_desktop_vector_snap (SPDesktop const *desktop, NR::Point &req, NR::Point const &d);
+double sp_desktop_free_snap(SPDesktop const *desktop, NR::Point &req);
+double sp_desktop_vector_snap(SPDesktop const *desktop, NR::Point &req, NR::Point const &d);
 
 gdouble sp_desktop_dim_snap(SPDesktop const *dt, NR::Point& req, NR::Dim2 const dim);
 
 /* List of points methods */
 
 NR::Coord sp_desktop_vector_snap_list(SPDesktop const *desktop, const std::vector<NR::Point> &p,
-				      NR::Point const &norm, NR::scale const &s);
+                                      NR::Point const &norm, NR::scale const &s);
 
 NR::Coord sp_desktop_dim_snap_list(SPDesktop const *desktop, const std::vector<NR::Point> &p,
-				   double const dx, unsigned const dim);
+                                   double const dx, unsigned const dim);
 
 NR::Coord sp_desktop_dim_snap_list_scale(SPDesktop const *desktop, const std::vector<NR::Point> &p,
-					 NR::Point const &norm, double const sx, NR::Dim2 const dim);
+                                         NR::Point const &norm, double const sx, NR::Dim2 const dim);
 
 NR::Coord sp_desktop_dim_snap_list_skew(SPDesktop const *desktop, const std::vector<NR::Point> &p,
-					NR::Point const &norm, double const sx, NR::Dim2 const dim);
+                                        NR::Point const &norm, double const sx, NR::Dim2 const dim);
 
 // These little functions are only here to provide an edge between NR::Point-land and NR::Point-land.
 
 NR::Coord sp_desktop_dim_snap_list(SPDesktop const *desktop, const std::vector<NR::Point> &p,
-				   double const dx, NR::Dim2 const dim);
+                                   double const dx, NR::Dim2 const dim);
 
 
 /* Single point methods */
-double sp_desktop_free_snap (SPDesktop const *desktop, NR::Point *req);
-double sp_desktop_vector_snap (SPDesktop const *desktop, NR::Point *req, NR::Coord dx, NR::Coord dy);
+double sp_desktop_free_snap(SPDesktop const *desktop, NR::Point *req);
+double sp_desktop_vector_snap(SPDesktop const *desktop, NR::Point *req, NR::Coord dx, NR::Coord dy);
 
-#endif
+
+#endif /* !SEEN_DESKTOP_SNAP_H */
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
