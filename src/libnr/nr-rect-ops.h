97119f7b4b61099b6961d9b9692e71106dbb7064
Peter Moulder <peter.moulder@monash.edu>
2004-06-19 07:45:51 +0000

    Mark as inline.  Add FIXME comment.
    (bzr r3106)

--- src/libnr/nr-rect-ops.h
+++ src/libnr/nr-rect-ops.h
@@ -1,8 +1,6 @@
 #ifndef SEEN_NR_RECT_OPS_H
 #define SEEN_NR_RECT_OPS_H
 
-/* ex:set et ts=4 sw=4: */
-
 /*
  * Rect operators
  *
@@ -11,28 +9,44 @@
  * This code is licensed under the GNU GPL; see COPYING for more information.
  */
 
-#include <libnr/nr-matrix.h>
+#include <libnr/nr-forward.h>
 #include <libnr/nr-rect.h>
 #include <libnr/nr-convex-hull.h>
 
 namespace NR {
 
-	Rect expand(Rect const &r, double by) {
-		const NR::Point p(by, by);
-		return Rect(r.min()+p, r.max()-p);
-	}
-	Rect expand(Rect const &r, NR::Point by) {
-		return Rect(r.min()+by, r.max()-by);
-	}
+inline Rect expand(Rect const &r, double by) {
+    NR::Point const p(by, by);
+    return Rect(r.min() + p, r.max() - p);
+}
 
-ConvexHull operator*(Rect const &r, Matrix const &m) {
+inline Rect expand(Rect const &r, NR::Point by) {
+    return Rect(r.min() + by, r.max() - by);
+}
+
+#if 0
+inline ConvexHull operator*(Rect const &r, Matrix const &m) {
+    /* FIXME: no mention of m.  Should probably be made non-inline. */
     ConvexHull points(r.corner(0));
     for ( unsigned i = 1 ; i < 4 ; i++ ) {
         points.add(r.corner(i));
     }
     return points;
 }
+#endif
 
 } /* namespace NR */
 
-#endif
+
+#endif /* !SEEN_NR_RECT_OPS_H */
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
