32e0103bff74098e2db1d7a48b78868cf215af43
nemies <nemies@users.sourceforge.net>
2004-07-23 03:08:16 +0000

    tempfile cleanups from pjrm (patch #990009)
    (bzr r3524)

--- share/extensions/sk2svg.sh
+++ share/extensions/sk2svg.sh
@@ -3,10 +3,10 @@
 rc=0
 
 TMPDIR="${TMPDIR-/tmp}"
-TEMPFILENAME=`mktemp 2>/dev/null || echo "$TMPDIR/tmpskfile.svg"`
-skconvert "$1" ${TEMPFILENAME}.svg > /dev/null 2>&1 || rc=1
-
-cat < "${TEMPFILENAME}.svg" || rc=1
-rm -f "${TEMPFILENAME}.svg"
-rm -f "${TEMPFILENAME}"
+UNIQTMPDIR=`mktemp -d 2>/dev/null || (mkdir "$TMPDIR/sk2svg.$$" && echo "$TMPDIR/sk2svg.$$") || echo "$TMPDIR"`
+TMPSVG="$UNIQTMPDIR/sk2svg$$.svg"
+skconvert "$1" "$TMPSVG" > /dev/null 2>&1 || rc=1
+cat < "$TMPSVG" || RC=1
+rm -f "$TMPSVG"
+[ "$UNIQTMPDIR" = "$TMPDIR" ] || rmdir "$UNIQTMPDIR"
 exit $rc
