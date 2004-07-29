424f615481c51834c9e05524e65fb93ab237b266
Peter Moulder <peter.moulder@monash.edu>
2004-07-29 05:45:00 +0000

    Move SPDesktopWidget stuff to new file sp-desktop-widget.h
    (bzr r3578)

--- src/desktop.h
+++ src/desktop.h
@@ -15,23 +15,13 @@
  *
  */
 
-class SPDesktopWidget;
-class SPDesktopWidgetClass;
-
-#define SP_TYPE_DESKTOP_WIDGET (sp_desktop_widget_get_type ())
-#define SP_DESKTOP_WIDGET(o) (GTK_CHECK_CAST ((o), SP_TYPE_DESKTOP_WIDGET, SPDesktopWidget))
-#define SP_DESKTOP_WIDGET_CLASS(k) (GTK_CHECK_CLASS_CAST ((k), SP_TYPE_DESKTOP_WIDGET, SPDesktopWidgetClass))
-#define SP_IS_DESKTOP_WIDGET(o) (GTK_CHECK_TYPE ((o), SP_TYPE_DESKTOP_WIDGET))
-#define SP_IS_DESKTOP_WIDGET_CLASS(k) (GTK_CHECK_CLASS_TYPE ((k), SP_TYPE_DESKTOP_WIDGET))
-
-#include <gtk/gtkoptionmenu.h>
-#include <gtk/gtkmenu.h>
 #include <sigc++/sigc++.h>
 #include <libnr/nr-rect.h>
 #include <libnr/nr-matrix.h>
 #include <libnr/nr-matrix-fns.h>
 #include "helper/helper-forward.h"
 #include "forward.h"
+#include "sp-desktop-widget.h"
 #include "style.h"
 #include "view.h"
 #include "verbs.h"
@@ -230,70 +220,10 @@ void sp_desktop_next_zoom (SPDesktop *dt);
 
 const SPUnit *sp_desktop_get_default_unit (SPDesktop *dt);
 
-/* SPDesktopWidget */
-
-struct SPDesktopWidget {
-	SPViewWidget viewwidget;
-
-	unsigned int update : 1;
-
-	SPDesktop *desktop;
-
-	// the root vbox of the window layout
-	GtkWidget *vbox; 
-
-	GtkWidget *menubar, *statusbar;
-
-	GtkWidget *hscrollbar, *vscrollbar, *vscrollbar_box;
-
-	GtkWidget *tool_toolbox, *aux_toolbox, *commands_toolbox;
-
-	/* Rulers */
-	GtkWidget *hruler, *vruler;
-	double dt2r;
-	NR::Point ruler_origin;
-
-	GtkWidget *sticky_zoom;
-	GtkWidget *coord_status;
-        GtkWidget *select_status;
-        GtkWidget *zoom_status;
-	gulong zoom_update;
-
-	GtkWidget *layer_selector;
-
-        gint coord_status_id, select_status_id;
-
-	SPCanvas *canvas;
-
-	GtkAdjustment *hadj, *vadj;
-
-	void setMessage(Inkscape::MessageType type, gchar const *message);
-	static void _update_layer_display(SPObject *layer, SPDesktopWidget *widget);
-	static void _activate_layer_menu(GtkOptionMenu *selector, SPDesktopWidget *widget);
-	static void _deactivate_layer_menu(GtkOptionMenu *selector, SPDesktopWidget *widget);
-	unsigned _buildLayerMenuItems(GtkMenu *menu, SPObject *layer);
-	void _buildLayerStatusMenuItem(GtkMenu *menu, SPObject *layer);
-};
-
-struct SPDesktopWidgetClass {
-	SPViewWidgetClass parent_class;
-};
-
-GtkType sp_desktop_widget_get_type (void);
-
-/* Constructor */
-
-SPViewWidget *sp_desktop_widget_new (SPNamedView *namedview);
-
-gint sp_desktop_widget_set_focus (GtkWidget *widget, GdkEvent *event, SPDesktopWidget  *dtw);
-
-void sp_desktop_widget_show_decorations (SPDesktopWidget *dtw, gboolean show);
 
 #ifdef HAVE_GTK_WINDOW_FULLSCREEN
 void fullscreen(SPDesktop *dt);
 #endif /* HAVE_GTK_WINDOW_FULLSCREEN */
-
-void sp_desktop_widget_layout (SPDesktopWidget *dtw);
 
 void sp_desktop_apply_css_recursive (SPObject *o, SPCSSAttr *css, bool skip_lines);
 void sp_desktop_set_color (SPDesktop *desktop, const ColorRGBA &color, bool is_relative, bool fill);
