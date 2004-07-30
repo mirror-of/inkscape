a244e4859c037b742406f66b1ec0c11ccabcbd63
Peter Moulder <peter.moulder@monash.edu>
2004-07-30 03:33:20 +0000

    Move most stuff to display/display-forward.h.  Leave temporary #include of that header until all includers are updated.
    (bzr r3590)

--- src/helper/helper-forward.h
+++ src/helper/helper-forward.h
@@ -12,38 +12,11 @@
  * Released under GNU GPL, read the file 'COPYING' for more information
  */
 
-#include <glib-object.h>
-
-struct SPCanvas;
-struct SPCanvasClass;
-struct SPCanvasItem;
-struct SPCanvasItemClass;
-struct SPCanvasGroup;
-struct SPCanvasGroupClass;
+#include <display/display-forward.h>
 
 struct SPUnit;
 struct SPUnitSelector;
 struct SPUnitSelectorClass;
-
-
-#define SP_TYPE_CANVAS_ITEM (sp_canvas_item_get_type ())
-#define SP_CANVAS_ITEM(obj) (GTK_CHECK_CAST ((obj), SP_TYPE_CANVAS_ITEM, SPCanvasItem))
-#define SP_IS_CANVAS_ITEM(obj) (GTK_CHECK_TYPE ((obj), SP_TYPE_CANVAS_ITEM))
-#define SP_CANVAS_ITEM_GET_CLASS(o) (GTK_CHECK_GET_CLASS ((o), SP_TYPE_CANVAS_ITEM, SPCanvasItemClass))
-
-GType sp_canvas_item_get_type (void);
-
-#define SP_TYPE_CANVAS_GROUP (sp_canvas_group_get_type ())
-#define SP_CANVAS_GROUP(obj) (GTK_CHECK_CAST ((obj), SP_TYPE_CANVAS_GROUP, SPCanvasGroup))
-#define SP_IS_CANVAS_GROUP(obj) (GTK_CHECK_TYPE ((obj), SP_TYPE_CANVAS_GROUP))
-
-GType sp_canvas_group_get_type (void);
-
-#define SP_TYPE_CANVAS (sp_canvas_get_type ())
-#define SP_CANVAS(obj) (GTK_CHECK_CAST ((obj), SP_TYPE_CANVAS, SPCanvas))
-#define SP_IS_CANVAS(obj) (GTK_CHECK_TYPE ((obj), SP_TYPE_CANVAS))
-
-GType sp_canvas_get_type (void);
 
 #endif
 
