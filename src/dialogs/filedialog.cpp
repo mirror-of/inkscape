ab1278ac894f999bc7b11d1db91ed1d6b5eba16f
Bob Jamison <ishmalius@gmail.com>
2004-08-02 16:25:40 +0000

    cleanup, add draft 'no preview' thing, add bulia's dir callbacks
    (bzr r3635)

--- src/dialogs/filedialog.cpp
+++ src/dialogs/filedialog.cpp
@@ -153,7 +153,7 @@ bool SVGPreview::set(const char *fName, int dialogType)
         bool retval = setFileName(fName);
         return retval;
         }
-    else if ((dialogType == IMPORT_TYPES || dialogType == EXPORT_TYPES) &&
+    else if (/*(dialogType == IMPORT_TYPES || dialogType == EXPORT_TYPES) &&*/
                  (
                   g_str_has_suffix(fName, ".bmp" ) ||
                   g_str_has_suffix(fName, ".gif" ) ||
@@ -241,9 +241,95 @@ bool SVGPreview::set(const char *fName, int dialogType)
         g_free(xmlBuffer);
         return retval;
         }
+    else
+        {
+        //Arbitrary size of svg doc -- rather 'portrait' shaped
+        gint previewWidth  = 300;
+        gint previewHeight = 600;
 
-    return false;
+        //Our template.  Modify to taste
+        gchar *xformat =
+          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+          "<svg\n"
+          "xmlns=\"http://www.w3.org/2000/svg\"\n"
+          "xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n"
+          "width=\"%d\" height=\"%d\">\n"
+          "<g transform=\"translate(-170,24.27184)\">\n"
+          "<path\n"
+          "style=\"font-size:12;fill:#ffffff;fill-rule:evenodd;stroke:#000000;stroke-width:0.936193pt;\"\n"
+          "d=\"M 397.64309 320.25301 L 280.39197 282.517 L 250.74227 124.83447 L 345.08225 "
+          "29.146783 L 393.59996 46.667064 L 483.89679 135.61619 L 397.64309 320.25301 z \"\n"
+          "id=\"whiteSpace\" />\n"
+          "<path\n"
+          "style=\"font-size:12;fill-rule:evenodd;stroke-width:1pt;fill:#000000;fill-opacity:1;\"\n"
+          "d=\"M 476.95792 339.17168 C 495.78197 342.93607 499.54842 356.11361 495.78197 359.87802 "
+          "C 492.01856 363.6434 482.6065 367.40781 475.07663 361.76014 C 467.54478 "
+          "356.11361 467.54478 342.93607 476.95792 339.17168 z \"\n"
+          "id=\"droplet01\" />\n"
+          "<path\n"
+          "style=\"font-size:12;fill-rule:evenodd;stroke-width:1pt;fill:#000000;fill-opacity:1;\"\n"
+          "d=\"M 286.46194 340.42914 C 284.6277 340.91835 269.30405 327.71337 257.16909 333.8338 "
+          "C 245.03722 339.95336 236.89276 353.65666 248.22676 359.27982 C 259.56184 364.90298 "
+          "267.66433 358.41867 277.60113 351.44119 C 287.53903 344.46477 "
+          "287.18046 343.1206 286.46194 340.42914 z \"\n"
+          "id=\"droplet02\" />\n"
+          "<path\n"
+          "style=\"font-size:12;fill-rule:evenodd;stroke-width:1pt;fill:#000000;fill-opacity:1;\"\n"
+          "d=\"M 510.35756 306.92856 C 520.59494 304.36879 544.24333 306.92856 540.47688 321.98634 "
+          "C 536.71354 337.04806 504.71297 331.39827 484.00371 323.87156 C 482.12141 "
+          "308.81083 505.53237 308.13423 510.35756 306.92856 z \"\n"
+          "id=\"droplet03\" />\n"
+          "<path\n"
+          "style=\"font-size:12;fill-rule:evenodd;stroke-width:1pt;fill:#000000;fill-opacity:1;\"\n"
+          "d=\"M 359.2403 21.362537 C 347.92693 21.362537 336.6347 25.683095 327.96556 34.35223 "
+          "L 173.87387 188.41466 C 165.37697 196.9114 161.1116 207.95813 160.94269 219.04577 "
+          "L 160.88418 219.04577 C 160.88418 219.08524 160.94076 219.12322 160.94269 219.16279 "
+          "C 160.94033 219.34888 160.88418 219.53256 160.88418 219.71865 L 161.14748 219.71865 "
+          "C 164.0966 230.93917 240.29699 245.24198 248.79866 253.74346 C 261.63771 266.58263 "
+          "199.5652 276.01151 212.4041 288.85074 C 225.24316 301.68979 289.99433 313.6933 302.8346 "
+          "326.53254 C 315.67368 339.37161 276.5961 353.04289 289.43532 365.88196 C 302.27439 "
+          "378.72118 345.40201 362.67257 337.5908 396.16198 C 354.92909 413.50026 391.10302 "
+          "405.2208 415.32417 387.88252 C 428.16323 375.04345 390.6948 376.17577 403.53397 "
+          "363.33668 C 416.37304 350.49745 448.78128 350.4282 476.08902 319.71589 C 465.09739 "
+          "302.62116 429.10801 295.34136 441.94719 282.50217 C 454.78625 269.66311 479.74708 "
+          "276.18423 533.60644 251.72479 C 559.89837 239.78398 557.72636 230.71459 557.62567 "
+          "219.71865 C 557.62356 219.48727 557.62567 219.27892 557.62567 219.04577 L 557.56716 "
+          "219.04577 C 557.3983 207.95812 553.10345 196.9114 544.60673 188.41466 L 390.54428 "
+          "34.35223 C 381.87515 25.683095 370.55366 21.362537 359.2403 21.362537 z M 357.92378 "
+          "41.402939 C 362.95327 41.533963 367.01541 45.368018 374.98006 50.530832 L 447.76915 "
+          "104.50827 C 448.56596 105.02498 449.32484 105.564 450.02187 106.11735 C 450.7189 106.67062 "
+          "451.3556 107.25745 451.95277 107.84347 C 452.54997 108.42842 453.09281 109.01553 453.59111 "
+          "109.62808 C 454.08837 110.24052 454.53956 110.86661 454.93688 111.50048 C 455.33532 112.13538 "
+          "455.69164 112.78029 455.9901 113.43137 C 456.28877 114.08363 456.52291 114.75639 456.7215 "
+          "115.42078 C 456.92126 116.08419 457.08982 116.73973 457.18961 117.41019 C 457.28949 "
+          "118.08184 457.33588 118.75535 457.33588 119.42886 L 414.21245 98.598549 L 409.9118 "
+          "131.16055 L 386.18512 120.04324 L 349.55654 144.50131 L 335.54288 96.1703 L 317.4919 "
+          "138.4453 L 267.08369 143.47735 L 267.63956 121.03795 C 267.63956 115.64823 296.69685 "
+          "77.915899 314.39075 68.932902 L 346.77721 45.674327 C 351.55594 42.576634 354.90608 "
+          "41.324327 357.92378 41.402939 z M 290.92738 261.61333 C 313.87149 267.56365 339.40299 "
+          "275.37038 359.88393 275.50997 L 360.76161 284.72563 C 343.2235 282.91785 306.11346 "
+          "274.45012 297.36372 269.98057 L 290.92738 261.61333 z \"\n"
+          "id=\"mountainDroplet\" />\n"
+          "</g>\n"
+          "<text xml:space=\"preserve\"\n"
+          "style=\"font-size:28.000000;font-style:normal;font-variant:normal;font-weight:bold;"
+          "font-stretch:normal;fill:#fff900;fill-opacity:1.0000000;stroke:none;stroke-width:1.0000000pt;"
+          "stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1.0000000;"
+          "font-family:Bitstream Vera Sans;text-anchor:middle;writing-mode:lr;\"\n"
+          "x=\"190\" y=\"240\">%s</text>\n"
+          "</svg>\n\n";
 
+        //Fill in the template
+        gchar *xmlBuffer = g_strdup_printf(xformat, 
+               previewWidth, previewHeight, _("Preview Not Available"));
+
+        //g_message("%s\n", xmlBuffer);
+
+        //now show it!
+        setFromMem(xmlBuffer);
+        g_free(xmlBuffer);
+        return false;
+        }
 }
 
 
@@ -437,8 +523,10 @@ void FileOpenDialogImpl::updatePreviewCallback()
     if (!fName)
         return;
 
-    bool retval = svgPreview.set(fName, dialogType);
-    set_preview_widget_active(retval);
+    svgPreview.set(fName, dialogType);
+    //leave the preview always on for now
+    //bool retval = svgPreview.set(fName, dialogType);
+    //set_preview_widget_active(retval);
 
 }
 
@@ -451,16 +539,27 @@ void FileOpenDialogImpl::updatePreviewCallback()
  */
 void FileOpenDialogImpl::fileNameEntryChangedCallback()
 {
-    gchar *fName = (gchar *)fileNameEntry.get_text().c_str(); 
-    //g_message("User hit return.  Text is '%s'\n", fName);
+    Glib::ustring fName = fileNameEntry.get_text();
+    //g_message("User hit return.  Text is '%s'\n", fName.c_str());
 
-    //simulate an 'OK'
-    response(GTK_RESPONSE_OK);
+   if (!Glib::path_is_absolute(fName))
+        {
+        //try appending to the current path
+        fName = get_current_folder() + "/" + fName;
+        }
 
-    if (g_file_test(fName, G_FILE_TEST_IS_REGULAR))
+    //g_message("path:'%s'\n", fName.c_str());
+
+    if (Glib::file_test(fName, Glib::FILE_TEST_IS_DIR))
+        {
+        set_current_folder(fName);
+        }
+    else if (Glib::file_test(fName, Glib::FILE_TEST_IS_REGULAR))
        {
        //dialog with either (1) select a regular file or (2) cd to dir
-       set_filename(fileNameEntry.get_text());
+       //simulate an 'OK'
+       set_filename(fName);
+       response(GTK_RESPONSE_OK);
        }
 
 }
@@ -514,7 +613,7 @@ FileOpenDialogImpl::FileOpenDialogImpl(const char *dir,
 
     //###### Add a preview widget
     set_preview_widget(svgPreview);
-    set_preview_widget_active(false);
+    set_preview_widget_active(true);
 
     //Catch selection-changed events, so we can adjust the text widget
     signal_update_preview().connect( 
@@ -750,7 +849,7 @@ FileSaveDialogImpl::FileSaveDialogImpl(const char *dir,
 
     //###### Add a preview widget
     set_preview_widget(svgPreview);
-    set_preview_widget_active(false);
+    set_preview_widget_active(true);
 
     //Catch selection-changed events, so we can adjust the text widget
     signal_update_preview().connect( 
