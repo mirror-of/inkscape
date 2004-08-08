9d4d96fccf55912971181dbdfaf079b79dfc96d1
Peter Moulder <peter.moulder@monash.edu>
2004-08-08 09:35:17 +0000

    Handle in O(1) time the common case of equal tails. doc.
    (bzr r3690)

--- src/algorithms/longest-common-suffix.h
+++ src/algorithms/longest-common-suffix.h
@@ -9,8 +9,8 @@
  * Released under GNU GPL, read the file 'COPYING' for more information
  */
 
-#ifndef SEEN_INKSCAPE_ALGORITMS_LONGEST_COMMON_SUFFIX_H
-#define SEEN_INKSCAPE_ALGORITMS_LONGEST_COMMON_SUFFIX_H
+#ifndef SEEN_INKSCAPE_ALGORITHMS_LONGEST_COMMON_SUFFIX_H
+#define SEEN_INKSCAPE_ALGORITHMS_LONGEST_COMMON_SUFFIX_H
 
 #include <list>
 
@@ -20,6 +20,18 @@ namespace Traits { template <typename T> struct List; }
 
 namespace Algorithms {
 
+/**
+ * Note that this implementation and the rest of this comment assume
+ * that `List==List' runs in O(1) time.
+ *
+ * Time costs:
+ *
+ * The case of sharing a common successor is handled in O(1) time.
+ *
+ * If \a a is the longest common suffix, then runs in O(len(rest of b)) time.
+ *
+ * Otherwise, runs in O(len(a) + len(b)) time.
+ */
 template <typename List>
 List longest_common_suffix(List a, List b) {
     typedef Traits::List<List> ListT;
@@ -28,15 +40,23 @@ List longest_common_suffix(List a, List b) {
         return ListT::null();
     }
 
-    /* build parallel lists of suffixes, ordered by increasing length */
+    /* Handle in O(1) time the common case of equal tails. */
+    {
+        List const tail = ListT::rest(a);
+        if ( tail == ListT::rest(b) ) {
+            return tail;
+        }
+    }
+
+    /* Build parallel lists of suffixes, ordered by increasing length. */
 
     List lists[2] = { a, b };
     std::list<List> suffixes[2];
 
     for ( int i=0 ; i < 2 ; i++ ) {
-        for ( List iter=lists[i] ;
+        for ( List iter = lists[i] ;
               !ListT::is_null(iter) ;
-              iter=ListT::rest(iter) )
+              iter = ListT::rest(iter) )
         {
             // TODO: == is ambiguous; in scheme terms, I mean roughly:
             // (eqv? iter (vector-ref lists (- 1 i)))
@@ -51,7 +71,7 @@ List longest_common_suffix(List a, List b) {
     /* iterate through the lists of suffix lists in parallel, stopping at
        the first pair of suffix lists that have different heads */
 
-    List longest_common=ListT::null();
+    List longest_common = ListT::null();
 
     typename std::list<List>::iterator iters[2] = {
         suffixes[0].begin(),
@@ -77,14 +97,15 @@ List longest_common_suffix(List a, List b) {
 
 }
 
-#endif
+#endif /* !SEEN_INKSCAPE_ALGORITHMS_LONGEST_COMMON_SUFFIX_H */
+
 /*
   Local Variables:
   mode:c++
   c-file-style:"stroustrup"
-  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
   indent-tabs-mode:nil
   fill-column:99
   End:
 */
-// vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
