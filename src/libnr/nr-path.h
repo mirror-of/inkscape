5f36080efaf95b9cf093e26e60d22727ec530f4e
Peter Moulder <peter.moulder@monash.edu>
2004-08-20 04:47:20 +0000

    Move NArtBpath to new file n-art-bpath.h.  Add a temporary #include of the new file until .cpp files updated.
    (bzr r3814)

--- src/libnr/nr-path.h
+++ src/libnr/nr-path.h
@@ -29,43 +29,7 @@
 
 #include <libnr/nr-forward.h>
 #include <libnr/nr-coord.h>
-#include <libnr/nr-point.h>
-
-typedef enum {
-  NR_MOVETO,
-  NR_MOVETO_OPEN,
-  NR_CURVETO,
-  NR_LINETO,
-  NR_END
-} NRPathcode;
-
-class NArtBpath {
- public:
-  /*< public >*/
-  NRPathcode code;
-  double x1, y1;
-  double x2, y2;
-  double x3, y3;
-
-    NR::Point c(unsigned const i) const {
-        switch (i) {
-            case 1: return NR::Point(x1, y1);
-            case 2: return NR::Point(x2, y2);
-            case 3: return NR::Point(x3, y3);
-            default: abort();
-        }
-    }
-
-    void setC(unsigned const i, NR::Point const &p) {
-        using NR::X; using NR::Y;
-        switch (i) {
-            case 1: x1 = p[X]; y1 = p[Y]; break;
-            case 2: x2 = p[X]; y2 = p[Y]; break;
-            case 3: x3 = p[X]; y3 = p[Y]; break;
-            default: abort();
-        }
-    }
-};
+#include <libnr/n-art-bpath.h>
 
 NArtBpath* nr_artpath_affine(NArtBpath *s, NR::Matrix const &transform);
 
