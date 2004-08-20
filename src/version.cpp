ce01f798e4d58ae4dfca1ff2b28973a1e0b3ab39
Carl Hetherington <cth@carlh.net>
2004-08-20 10:28:36 +0000

    Coding style cleanups
    (bzr r3835)

--- src/version.cpp
+++ src/version.cpp
@@ -16,42 +16,51 @@
 #include <stdio.h>
 #include "version.h"
 
-gboolean
-sp_version_from_string(const gchar *string, SPVersion *version)
+gboolean sp_version_from_string(const gchar *string, SPVersion *version)
 {
-	if (!string) return FALSE;
+    if (!string) {
+        return FALSE;
+    }
 
-	version->major = 0;
-	version->minor = 0;
+    version->major = 0;
+    version->minor = 0;
 
-	return sscanf((const char *)string, "%u.%u",
-	              &version->major, &version->minor) ||
-	       sscanf((const char *)string, "%u", &version->major);
+    return sscanf((const char *)string, "%u.%u",
+                  &version->major, &version->minor) ||
+        sscanf((const char *)string, "%u", &version->major);
 }
 
-gchar *
-sp_version_to_string(SPVersion version)
+gchar *sp_version_to_string(SPVersion version)
 {
-	return g_strdup_printf("%u.%u", version.major, version.minor);
+    return g_strdup_printf("%u.%u", version.major, version.minor);
 }
 
-gboolean
-sp_version_inside_range(SPVersion version,
-                        unsigned major_min, unsigned minor_min,
-                        unsigned major_max, unsigned minor_max)
+gboolean sp_version_inside_range(SPVersion version,
+                                 unsigned major_min, unsigned minor_min,
+                                 unsigned major_max, unsigned minor_max)
 {
-	if ( version.major < major_min || version.major > major_max ) {
-		return FALSE;
-	} else if ( version.major == major_min &&
-	            version.minor <= minor_min )
-	{
-		return FALSE;
-	} else if ( version.major == major_max &&
-	            version.minor >= minor_max )
-	{
-		return FALSE;
-	} else {
-		return TRUE;
-	}
+    if ( version.major < major_min || version.major > major_max ) {
+        return FALSE;
+    } else if ( version.major == major_min &&
+                version.minor <= minor_min )
+    {
+        return FALSE;
+    } else if ( version.major == major_max &&
+                version.minor >= minor_max )
+    {
+        return FALSE;
+    } else {
+        return TRUE;
+    }
 }
 
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
