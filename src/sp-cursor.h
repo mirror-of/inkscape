8fe483edba9cd74460d61d4504fe9030e229f6af
Carl Hetherington <cth@carlh.net>
2004-08-25 08:49:44 +0000

    Coding style cleanup.
    (bzr r3891)

--- src/sp-cursor.h
+++ src/sp-cursor.h
@@ -4,7 +4,7 @@
 #include <gdk/gdk.h>
 
 void sp_cursor_bitmap_and_mask_from_xpm (GdkBitmap **bitmap, GdkBitmap **mask, gchar **xpm);
-GdkCursor * sp_cursor_new_from_xpm (gchar ** xpm, gint hot_x, gint hot_y);
+GdkCursor *sp_cursor_new_from_xpm (gchar **xpm, gint hot_x, gint hot_y);
 
 #endif
 
