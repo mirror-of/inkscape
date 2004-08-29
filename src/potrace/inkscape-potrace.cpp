205a2630c29355d34bf53e5c05f54ac9130636c8
Bob Jamison <ishmalius@gmail.com>
2004-08-29 09:31:46 +0000

    first working version
    (bzr r3931)

--- src/potrace/inkscape-potrace.cpp
+++ src/potrace/inkscape-potrace.cpp
@@ -54,6 +54,9 @@ writePaths(path_t *plist, Inkscape::SVGOStringStream& data)
     path_t *node;
     for (node=plist; node ; node=node->sibling)
         {
+        //g_message("node->fm:%d\n", node->fm);
+        if (!node->fm)
+            continue;
         dpoint_t *pt = node->fcurve[node->fm -1].c;
         data << "M " << pt[2].x << " " << pt[2].y << " ";
         for (int i=0 ; i<node->fm ; i++)
@@ -66,9 +69,9 @@ writePaths(path_t *plist, Inkscape::SVGOStringStream& data)
                     data << "L " << pt[2].x << " " << pt[2].y << " " ;
                 break;
                 case CURVETO:
-                    data << "C " << pt[1].x << " " << pt[1].y << " "
-                                 << pt[2].x << " " << pt[2].y << " "
-                                 << pt[3].x << " " << pt[3].y << " ";
+                    data << "C " << pt[0].x << " " << pt[0].y << " "
+                                 << pt[1].x << " " << pt[1].y << " "
+                                 << pt[2].x << " " << pt[2].y << " ";
 
                 break;
                 default:
@@ -104,8 +107,8 @@ getPathDataFromPixbuf(GdkPixbuf * pixbuf)
     int rowstride   = gdk_pixbuf_get_rowstride(pixbuf);
     int n_channels  = gdk_pixbuf_get_n_channels(pixbuf);
 
-    g_message("w:%d h:%d rowstride:%d channels:%d\n",
-           width, height, rowstride, n_channels);
+    //g_message("w:%d h:%d rowstride:%d channels:%d\n",
+    //       width, height, rowstride, n_channels);
 
     bitmap_t *bm = bm_new(width, height);
     bm_clear(bm, 0);
@@ -113,14 +116,13 @@ getPathDataFromPixbuf(GdkPixbuf * pixbuf)
     //##Read the data out of the Pixbuf
     int x,y;
     int row=0;
-    int by = height-1;
-    for (y=0 ; y<height ; y++, by--)
+    for (y=0 ; y<height ; y++)
         {
         guchar *p = pixdata + row;
         for (x=0 ; x<width ; x++)
             {
             double sum = (double)p[0]+(double)p[1]+(double)p[2];
-            BM_UPUT(bm, x, by, sum > cutoff ? 0 : 1);
+            BM_UPUT(bm, x, y, sum > cutoff ? 0 : 1);
             p += n_channels;
             }
         row += rowstride;
@@ -145,6 +147,12 @@ getPathDataFromPixbuf(GdkPixbuf * pixbuf)
     //## Free the Potrace bitmap
     bm_free(bm);
 
+    ret = process_path(plist);
+    if (ret)
+        {
+        g_warning("Potrace::convertImageToPath: trouble processing trace\n");
+        return false;
+        }
 
 
     Inkscape::SVGOStringStream data;
@@ -159,7 +167,7 @@ getPathDataFromPixbuf(GdkPixbuf * pixbuf)
 
     //# get the svg <path> 'd' attribute
     char *d = strdup(data.str().c_str());
-    g_message("### GOT '%s' \n", d);
+    //g_message("### GOT '%s' \n", d);
 
 
     return d;
@@ -215,17 +223,14 @@ Potrace::convertImageToPath()
     char *d = getPathDataFromPixbuf(pixbuf);
 
     SPRepr   *pathRepr  = sp_repr_new("path");
-    SPObject *reprobj   = doc->getObjectByRepr(pathRepr);
-    SPPath   *path      = SP_PATH(reprobj);
+    sp_repr_set_attr(pathRepr, "d", d);
+    //SPObject *reprobj   = doc->getObjectByRepr(pathRepr);
 
     //#Add to tree
     SPRepr *par         = sp_repr_parent(SP_OBJECT(img)->repr);
     sp_repr_add_child(par, pathRepr, SP_OBJECT(img)->repr);
 
     free(d);
-
-    //##Write our new info to the repr side
-    //reprobj->updateRepr();
 
     //## inform the document, so we can undo
     sp_document_done(doc);
