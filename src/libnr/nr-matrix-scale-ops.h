208477fffa7ac000a6cdcd281884fa98f7658096
Peter Moulder <peter.moulder@monash.edu>
2004-09-17 12:37:17 +0000

    new files nr-matrix-scale-ops.h nr-matrix-translate-ops.h
    (bzr r4119)

---
+++ src/libnr/nr-matrix-scale-ops.h
@@ -0,0 +1,3 @@
+/** \file Declarations (and definition if inline) of operator blah (NR::Matrix, NR::scale). */
+
+#include "libnr/nr-matrix-ops.h"
---
+++ src/libnr/nr-matrix-translate-ops.h
@@ -0,0 +1,3 @@
+/** \file Declarations (and definition if inline) of operator blah (NR::Matrix, NR::translate). */
+
+#include "libnr/nr-matrix-ops.h"
