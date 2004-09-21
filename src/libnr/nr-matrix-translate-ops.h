a87f191f2dbae0c6aae96c2816ac9d3e9addddca
Peter Moulder <peter.moulder@monash.edu>
2004-09-21 13:28:27 +0000

    operator/(Matrix, translate): new
    (bzr r4182)

--- src/libnr/nr-matrix-translate-ops.h
+++ src/libnr/nr-matrix-translate-ops.h
@@ -1,3 +1,26 @@
+#ifndef SEEN_LIBNR_NR_MATRIX_TRANSLATE_OPS_H
+#define SEEN_LIBNR_NR_MATRIX_TRANSLATE_OPS_H
+
 /** \file Declarations (and definition if inline) of operator blah (NR::Matrix, NR::translate). */
 
 #include "libnr/nr-matrix-ops.h"
+
+inline NR::Matrix
+operator/(NR::Matrix const &numer, NR::translate const &denom)
+{
+    return numer * NR::translate(-denom.offset);
+}
+
+
+#endif /* !SEEN_LIBNR_NR_MATRIX_TRANSLATE_OPS_H */
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
