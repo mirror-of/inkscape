a0bbfae0c52ae1d35e3af18612861726930ac2f7
Peter Moulder <peter.moulder@monash.edu>
2004-09-21 13:27:34 +0000

    operator/(Matrix, scale): new
    (bzr r4181)

--- src/libnr/nr-matrix-scale-ops.h
+++ src/libnr/nr-matrix-scale-ops.h
@@ -1,3 +1,10 @@
+#ifndef SEEN_LIBNR_NR_MATRIX_SCALE_OPS_H
+#define SEEN_LIBNR_NR_MATRIX_SCALE_OPS_H
 /** \file Declarations (and definition if inline) of operator blah (NR::Matrix, NR::scale). */
 
 #include "libnr/nr-matrix-ops.h"
+
+NR::Matrix operator/(NR::Matrix const &m, NR::scale const &s);
+
+
+#endif /* !SEEN_LIBNR_NR_MATRIX_SCALE_OPS_H */
