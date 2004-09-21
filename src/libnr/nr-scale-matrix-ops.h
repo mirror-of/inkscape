b119e384852b4467cbaf1f2d828b06727763c8af
Peter Moulder <peter.moulder@monash.edu>
2004-09-21 13:44:19 +0000

    New file nr-scale-matrix-ops.h
    (bzr r4184)

---
+++ src/libnr/nr-scale-matrix-ops.h
@@ -0,0 +1,8 @@
+#ifndef SEEN_LIBNR_NR_SCALE_MATRIX_OPS_H
+#define SEEN_LIBNR_NR_SCALE_MATRIX_OPS_H
+/** \file Declarations (and definition if inline) of operator blah (NR::scale, NR::Matrix). */
+
+#include "libnr/nr-matrix-ops.h"
+
+
+#endif /* !SEEN_LIBNR_NR_SCALE_MATRIX_OPS_H */
