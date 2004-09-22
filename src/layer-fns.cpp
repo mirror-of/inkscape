9ca05f79cc2a8144ad730bc0f6d25a7446acdaa6
MenTaLguY <mental@rydia.net>
2004-09-22 03:30:17 +0000

    added skeletal layer-fns.cpp
    (bzr r4203)

---
+++ src/layer-fns.cpp
@@ -0,0 +1,45 @@
+/*
+ * Inkscape::SelectionDescriber - shows messages describing selection
+ *
+ * Authors:
+ *   MenTaLguY <mental@rydia.net>
+ *
+ * Copyright (C) 2004 MenTaLguY
+ *
+ * Released under GNU GPL, read the file 'COPYING' for more information
+ */
+
+#include "../config.h"
+#include <cstdlib>
+#include "layer-fns.h"
+
+namespace Inkscape {
+
+SPObject *create_layer(SPObject *parent) {
+    std::abort();
+    return NULL;
+}
+
+SPObject *next_layer(SPObject *root, SPObject *current) {
+    std::abort();
+    return NULL;
+}
+
+SPObject *previous_layer(SPObject *root, SPObject *parent) {
+    std::abort();
+    return NULL;
+}
+
+
+}
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
