2480201a44205493e76303d6055fbe055b592611
bulia byak <buliabyak@gmail.com>
2004-10-10 03:58:38 +0000

    add -white, imagemagick tweaks
    (bzr r4356)

--- share/extensions/inkscape-shadow.sh
+++ share/extensions/inkscape-shadow.sh
@@ -1,1 +1,1 @@
-convert -mattecolor "#ffff" -frame $((${2} * 2))x$((${2} * 2)) -fx '1-a' -channel A -blur $((${2} * 3))x${2}  $1 $1 
+convert -mattecolor "#000f" -frame $((${2} * 3))x$((${2} * 3)) -fx '0' -channel A -blur $((${2} * 3))x${2} $1 $1
