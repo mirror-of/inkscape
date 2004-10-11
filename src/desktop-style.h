bbdcc8e33418968e36bd75cc0ff26319200457eb
Peter Moulder <peter.moulder@monash.edu>
2004-10-11 03:52:59 +0000

    Add forward declarations for referenced types.
    (bzr r4381)

--- src/desktop-style.h
+++ src/desktop-style.h
@@ -13,11 +13,16 @@
  */
 
 #include "xml/xml-forward.h"
+class ColorRGBA;
+struct SPCSSAttr;
+struct SPDesktop;
+struct SPObject;
+struct SPRepr;
 
-void sp_desktop_apply_css_recursive (SPObject *o, SPCSSAttr *css, bool skip_lines);
-void sp_desktop_set_color (SPDesktop *desktop, const ColorRGBA &color, bool is_relative, bool fill);
-void sp_desktop_set_style (SPDesktop *desktop, SPCSSAttr *css, bool change = true);
-SPCSSAttr *sp_desktop_get_style (SPDesktop *desktop, bool with_text);
-void sp_desktop_apply_style_tool (SPDesktop *desktop, SPRepr *repr, const char *tool, bool with_text);
+void sp_desktop_apply_css_recursive(SPObject *o, SPCSSAttr *css, bool skip_lines);
+void sp_desktop_set_color(SPDesktop *desktop, ColorRGBA const &color, bool is_relative, bool fill);
+void sp_desktop_set_style(SPDesktop *desktop, SPCSSAttr *css, bool change = true);
+SPCSSAttr *sp_desktop_get_style(SPDesktop *desktop, bool with_text);
+void sp_desktop_apply_style_tool(SPDesktop *desktop, SPRepr *repr, char const *tool, bool with_text);
 
 #endif
