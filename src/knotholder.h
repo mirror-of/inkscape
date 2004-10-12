2745ee215612465e0ef6309fcacbaa86e7a48eac
Peter Moulder <peter.moulder@monash.edu>
2004-10-12 08:58:00 +0000

    Move SPKnotHolderEntity to new file knot-holder-entity.h. whitespace.
    (bzr r4422)

--- src/knotholder.h
+++ src/knotholder.h
@@ -16,8 +16,9 @@
  */
 
 #include <glib/gtypes.h>
-#include "knot.h"
+#include "knot-enums.h"
 #include "forward.h"
+#include <libnr/nr-forward.h>
 #include <xml/xml-forward.h>
 
 typedef void (* SPKnotHolderSetFunc) (SPItem *item, NR::Point const &p, guint state);
@@ -25,44 +26,49 @@ typedef NR::Point (* SPKnotHolderGetFunc) (SPItem *item);
 /* fixme: Think how to make callbacks most sensitive (Lauris) */
 typedef void (* SPKnotHolderReleasedFunc) (SPItem *item);
 
-class SPKnotHolderEntity;
-class       SPKnotHolder;
-
-
-struct SPKnotHolderEntity {
-	SPKnot *knot;
-	guint   handler_id;
-	void (* knot_set) (SPItem *item, NR::Point const &p, guint state);
-	NR::Point (* knot_get) (SPItem *item);
-	void (* knot_click) (SPItem *item, guint state);
-};
-
 struct SPKnotHolder {
-	SPDesktop *desktop;
-	SPItem *item;
-	GSList *entity;
+    SPDesktop *desktop;
+    SPItem *item;
+    GSList *entity;
 
-	SPKnotHolderReleasedFunc released;
+    SPKnotHolderReleasedFunc released;
 
-	SPRepr *repr; // repr of the item, for setting and releasing listeners
+    SPRepr *repr; ///< repr of the item, for setting and releasing listeners.
 
-	gboolean local_change; // if true, no need to recreate knotholder if repr was changed
+    gboolean local_change; ///< if true, no need to recreate knotholder if repr was changed.
 };
 
 
 /* fixme: As a temporary solution, if released is NULL knotholder flushes undo itself (Lauris) */
-SPKnotHolder *sp_knot_holder_new (SPDesktop *desktop, SPItem *item, SPKnotHolderReleasedFunc relhandler);
+SPKnotHolder *sp_knot_holder_new(SPDesktop *desktop, SPItem *item, SPKnotHolderReleasedFunc relhandler);
 
-void sp_knot_holder_destroy (SPKnotHolder *knots);
+void sp_knot_holder_destroy(SPKnotHolder *knots);
 
-void sp_knot_holder_add (SPKnotHolder *knot_holder, SPKnotHolderSetFunc knot_set, SPKnotHolderGetFunc knot_get, void (* knot_click) (SPItem *item, guint state), const gchar *tip);
-void sp_knot_holder_add_full (SPKnotHolder *knot_holder,
-			      SPKnotHolderSetFunc knot_set,
-			      SPKnotHolderGetFunc knot_get,
-                         void (* knot_click) (SPItem *item, guint state),
-			      SPKnotShapeType shape,
-			      SPKnotModeType mode,
-				const gchar *tip);
+void sp_knot_holder_add(SPKnotHolder *knot_holder,
+                        SPKnotHolderSetFunc knot_set,
+                        SPKnotHolderGetFunc knot_get,
+                        void (* knot_click) (SPItem *item, guint state),
+                        gchar const *tip);
+
+void sp_knot_holder_add_full(SPKnotHolder *knot_holder,
+                             SPKnotHolderSetFunc knot_set,
+                             SPKnotHolderGetFunc knot_get,
+                             void (* knot_click) (SPItem *item, guint state),
+                             SPKnotShapeType shape,
+                             SPKnotModeType mode,
+                             gchar const *tip);
 
 
-#endif
+#endif /* !__SP_KNOTHOLDER_H__ */
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
+
