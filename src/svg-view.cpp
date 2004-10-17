04a63ba4a630d600d4f6be46044648bd7ad0476f
bulia byak <buliabyak@gmail.com>
2004-10-17 20:34:20 +0000

    try to remove shadow in about box
    (bzr r4491)

--- src/svg-view.cpp
+++ src/svg-view.cpp
@@ -317,6 +317,7 @@ sp_svg_view_widget_init (SPSVGViewWidget *vw)
 
 	/* ScrolledWindow */
 	vw->sw = gtk_scrolled_window_new (NULL, NULL);
+	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW(vw->sw), GTK_SHADOW_NONE);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (vw->sw), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 	gtk_container_add (GTK_CONTAINER (vw), vw->sw);
 	gtk_widget_show (vw->sw);
