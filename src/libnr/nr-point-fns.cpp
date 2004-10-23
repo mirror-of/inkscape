b814835f7610b7dd53e8961c6d9b9c10adeee8e3
Peter Moulder <peter.moulder@monash.edu>
2004-10-23 04:26:26 +0000

    is_zero: new function. whitespace.
    (bzr r4569)

--- src/libnr/nr-point-fns.cpp
+++ src/libnr/nr-point-fns.cpp
@@ -8,29 +8,52 @@ using NR::Point;
 
 /** Compute the L infinity, or maximum, norm of \a p. */
 NR::Coord NR::LInfty(Point const &p) {
-	NR::Coord const a(fabs(p[NR::X]));
-	NR::Coord const b(fabs(p[NR::Y]));
+    NR::Coord const a(fabs(p[0]));
+    NR::Coord const b(fabs(p[1]));
     return ( a < b || isNaN(b)
-		 ? b
-		 : a );
+             ? b
+             : a );
+}
+
+/** Returns true iff p is a zero vector, i.e.\ Point(0, 0).
+ *
+ *  (NaN is considered non-zero.)
+ */
+bool
+NR::is_zero(Point const &p)
+{
+    return ( p[0] == 0 &&
+             p[1] == 0   );
 }
 
 NR::Coord NR::atan2(Point const p) {
-	return std::atan2(p[NR::Y], p[NR::X]);
+    return std::atan2(p[NR::Y], p[NR::X]);
 }
 
 Point NR::unit_vector(Point const &a)
 {
-	Point ret(a);
-	ret.normalize();
-	return ret;
+    Point ret(a);
+    ret.normalize();
+    return ret;
 }
 
 NR::Point abs(NR::Point const &b)
 {
-	NR::Point ret;
-	for ( int i = 0 ; i < 2 ; i++ ) {
-		ret[i] = fabs(b[i]);
-	}
-	return ret;
+    NR::Point ret;
+    for ( int i = 0 ; i < 2 ; i++ ) {
+        ret[i] = fabs(b[i]);
+    }
+    return ret;
 }
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
