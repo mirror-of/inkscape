06485e0f8354ef8d20cca329764c8e00a41ee94c
Peter Moulder <peter.moulder@monash.edu>
2004-10-23 11:21:56 +0000

    operator*(scale, Matrix): move here from nr-matrix-ops.h
    (bzr r4583)

--- src/libnr/nr-scale-matrix-ops.h
+++ src/libnr/nr-scale-matrix-ops.h
@@ -2,7 +2,9 @@
 #define SEEN_LIBNR_NR_SCALE_MATRIX_OPS_H
 /** \file Declarations (and definition if inline) of operator blah (NR::scale, NR::Matrix). */
 
-#include "libnr/nr-matrix-ops.h"
+#include "libnr/nr-forward.h"
+
+NR::Matrix operator*(NR::scale const &s, NR::Matrix const &m);
 
 
 #endif /* !SEEN_LIBNR_NR_SCALE_MATRIX_OPS_H */
