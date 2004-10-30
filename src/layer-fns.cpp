01e312fe5e352d726bee3be2e6e23eb36e4470b4
MenTaLguY <mental@rydia.net>
2004-10-30 05:26:53 +0000

    make ordering of layers complete (not just among siblings)
    (bzr r4702)

--- src/layer-fns.cpp
+++ src/layer-fns.cpp
@@ -57,41 +57,116 @@ bool is_layer(SPObject &object) {
            SP_GROUP(&object)->layerMode() == SPGroup::LAYER;
 }
 
-}
+SPObject *next_sibling_layer(SPObject *layer) {
+    using std::find_if;
 
-/** Advances to the next layer within object \a root.
- *  Returns NULL if there are no layers within \a root
- */
-SPObject *next_layer(SPObject *root, SPObject *layer) {
-    if ( layer == root ) {
-        return NULL;
-    }
-
-    // TODO: look to cousins, children, and ancestors also (depth-last order)
-    return std::find_if<SPObject::SiblingIterator>(
-        SP_OBJECT_NEXT(layer),
-        NULL,
-        &is_layer
+    return find_if<SPObject::SiblingIterator>(
+        SP_OBJECT_NEXT(layer), NULL, &is_layer
     );
 }
 
+SPObject *previous_sibling_layer(SPObject *layer) {
+    using Inkscape::Algorithms::find_last_if;
 
-/** Returns the layer object preceeding \a layer within \a root.
- *  Returns NULL if there are no layers within root.
+    SPObject *sibling(find_last_if<SPObject::SiblingIterator>(
+        SP_OBJECT_PARENT(layer)->firstChild(), layer, &is_layer
+    ));
+
+    return ( sibling != layer ) ? sibling : NULL;
+}
+
+SPObject *first_descendant_layer(SPObject *layer) {
+    using std::find_if;
+
+    SPObject *first_descendant=NULL;
+    while (layer) {
+        layer = find_if<SPObject::SiblingIterator>(
+            layer->firstChild(), NULL, &is_layer
+        );
+        if (layer) {
+            first_descendant = layer;
+        }
+    }
+
+    return first_descendant;
+}
+
+SPObject *last_child_layer(SPObject *layer) {
+    using Inkscape::Algorithms::find_last_if;
+
+    return find_last_if<SPObject::SiblingIterator>(
+        layer->firstChild(), NULL, &is_layer
+    );
+}
+
+SPObject *last_elder_layer(SPObject *root, SPObject *layer) {
+    using Inkscape::Algorithms::find_last_if;
+
+    while ( layer != root ) {
+        SPObject *sibling(previous_sibling_layer(layer));
+        if (sibling) {
+            return sibling;
+        }
+        layer = SP_OBJECT_PARENT(layer);
+    }
+
+    return NULL;
+}
+
+}
+
+/** Finds the next layer under \a root, relative to \a layer in
+ *  depth-first order.
+ *
+ *  @returns NULL if there are no further layers under \a root
+ */
+SPObject *next_layer(SPObject *root, SPObject *layer) {
+    using std::find_if;
+
+    g_return_val_if_fail(layer != NULL, NULL);
+
+    SPObject *sibling(next_sibling_layer(layer));
+    if (sibling) {
+        SPObject *descendant(first_descendant_layer(sibling));
+        if (descendant) {
+            return descendant;
+        } else {
+            return sibling;
+        }
+    } else {
+        SPObject *parent=SP_OBJECT_PARENT(layer);
+        if ( parent != root ) {
+            return parent;
+        } else {
+            return NULL;
+        }
+    }
+}
+
+
+/** Finds the previous layer under \a root, relative to \a layer in
+ *  depth-first order.
+ *
+ *  @returns NULL if there are no prior layers under \a root.
  */
 SPObject *previous_layer(SPObject *root, SPObject *layer) {
     using Inkscape::Algorithms::find_last_if;
 
-    if ( layer == root ) {
-        return NULL;
-    }
+    g_return_val_if_fail(layer != NULL, NULL);
 
-    // TODO: look to cousins, children, and ancestors also (depth-last order)
-    return find_last_if<SPObject::SiblingIterator>(
-        SP_OBJECT_PARENT(layer)->firstChild(),
-        layer,
-        &is_layer
-    );
+    SPObject *child(last_child_layer(layer));
+    if (child) {
+        return child;
+    } else if ( layer != root ) {
+        SPObject *sibling(previous_sibling_layer(layer));
+        if (sibling) {
+            return sibling;
+        } else {
+            return last_elder_layer(root, SP_OBJECT_PARENT(layer));
+        }
+    } 
+
+    return NULL;
 }
 
 }
