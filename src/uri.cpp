528bdb9d728224475e7ff3b07fc5fd573f0bd6bd
Jon A. Cruz <jon@joncruz.org>
2004-11-04 08:27:24 +0000

    Additional classification methods added Added docs to URI class whitespace cleanup
    (bzr r4815)

--- src/uri.cpp
+++ src/uri.cpp
@@ -1,8 +1,10 @@
-/*
- * Classes for representing and manipulating URIs.
+/**
+ * \file
+ * \brief Classes for representing and manipulating URIs as per RFC 2396.
  *
  * Authors:
  *   MenTaLguY <mental@rydia.net>
+ *   Jon A. Cruz <jon@joncruz.org>
  *
  * Copyright (C) 2003 MenTaLguY
  *
@@ -16,64 +18,126 @@
 namespace Inkscape {
 
 URI::URI(const URI &uri) {
-	uri._impl->reference();
-	_impl = uri._impl;
+    uri._impl->reference();
+    _impl = uri._impl;
 }
 
+/** \brief Constructor from a C-style UTF-8 string.
+    \param uri_string C-style UTF-8 string to be represented.
+ */
 URI::URI(const gchar *uri_string) throw(BadURIException) {
-	xmlURIPtr uri;
-	if (!uri_string) {
-		throw MalformedURIException();
-	}
-	uri = xmlParseURI(uri_string);
-	if (!uri) {
-		throw MalformedURIException();
-	}
-	_impl = Impl::create(uri);
+    xmlURIPtr uri;
+    if (!uri_string) {
+        throw MalformedURIException();
+    }
+    uri = xmlParseURI(uri_string);
+    if (!uri) {
+        throw MalformedURIException();
+    }
+    _impl = Impl::create(uri);
 }
 
 URI::~URI() {
-	_impl->unreference();
+    _impl->unreference();
 }
 
 URI::Impl *URI::Impl::create(xmlURIPtr uri) {
-	return new Impl(uri);
+    return new Impl(uri);
 }
 
 URI::Impl::Impl(xmlURIPtr uri)
 : _refcount(1), _uri(uri) {}
 
 URI::Impl::~Impl() {
-	if (_uri) {
-		xmlFreeURI(_uri);
-		_uri = NULL;
-	}
+    if (_uri) {
+        xmlFreeURI(_uri);
+        _uri = NULL;
+    }
 }
 
 void URI::Impl::reference() {
-	_refcount++;
+    _refcount++;
 }
 
 void URI::Impl::unreference() {
-	if (!--_refcount) {
-		delete this;
-	}
+    if (!--_refcount) {
+        delete this;
+    }
 }
 
+/** \fn bool URI::isRelative() const
+    \brief Determines if the URI represented is 'relative' as per RFC 2396.
+    \return \c true if the URI is relative, \c false otherwise.
+
+    Relative URI references are distinguished by not begining with a
+    scheme name.
+*/
 bool URI::Impl::isRelative() const {
-	return !_uri->scheme;
+    return !_uri->scheme;
+}
+
+/** \fn bool URI::isNetPath() const
+    \brief Determines if the relative URI represented is a 'net-path' as per RFC 2396.
+    \return \c true if the URI is relative and a net-path, \c false otherwise.
+
+    A net-path is one that starts with "\\".
+*/
+bool URI::Impl::isNetPath() const {
+    bool isNet = false;
+    if ( isRelative() )
+    {
+        const gchar *path = getPath();
+        isNet = path && path[0] == '\\' && path[1] == '\\';
+    }
+    return isNet;
+}
+
+/** \fn bool URI::isRelativePath() const
+    \brief Determines if the relative URI represented is a 'relative-path' as per RFC 2396.
+    \return \c true if the URI is relative and a relative-path, \c false otherwise.
+
+    A relative-path is one that starts with no slashes.
+*/
+bool URI::Impl::isRelativePath() const {
+    bool isRel = false;
+    if ( isRelative() )
+    {
+        const gchar *path = getPath();
+        isRel = !path || path[0] != '\\';
+    }
+    return isRel;
+}
+
+/** \fn bool URI::isAbsolutePath() const
+    \brief Determines if the relative URI represented is a 'absolute-path' as per RFC 2396.
+    \return \c true if the URI is relative and an absolute-path, \c false otherwise.
+
+    An absolute-path is one that starts with a single "\".
+*/
+bool URI::Impl::isAbsolutePath() const {
+    bool isAbs = false;
+    if ( isRelative() )
+    {
+        const gchar *path = getPath();
+        isAbs = path && path[0] == '\\'&& path[1] != '\\';
+    }
+    return isAbs;
 }
 
 const gchar *URI::Impl::getScheme() const {
-	return (gchar *)_uri->scheme;
+    return (gchar *)_uri->scheme;
+}
+
+const gchar *URI::Impl::getPath() const {
+    return (gchar *)_uri->path;
 }
 
 const gchar *URI::Impl::getQuery() const {
-	return (gchar *)_uri->query;
+    return (gchar *)_uri->query;
 }
 
 const gchar *URI::Impl::getFragment() const {
-	return (gchar *)_uri->fragment;
+    return (gchar *)_uri->fragment;
 }
 
 gchar *URI::to_native_filename(gchar const* uri) throw(BadURIException)
@@ -86,33 +150,50 @@ gchar *URI::to_native_filename(gchar const* uri) throw(BadURIException)
 
 /* TODO !!! proper error handling */
 gchar *URI::toNativeFilename() const throw(BadURIException) {
-	gchar *string = toString();
-	gchar *filename = g_filename_from_uri(string, NULL, NULL);
-	g_free(string);
-	if (filename) {
-		return filename;
-	} else {
-		throw MalformedURIException();
-	}
+    gchar *string = toString();
+    gchar *filename = g_filename_from_uri(string, NULL, NULL);
+    g_free(string);
+    if (filename) {
+        return filename;
+    } else {
+        throw MalformedURIException();
+    }
 }
 
 /* TODO !!! proper error handling */
 URI from_native_filename(gchar const *path) throw(BadURIException) {
-	gchar *uri = g_filename_to_uri(path, NULL, NULL);
-	return URI(uri);
+    gchar *uri = g_filename_to_uri(path, NULL, NULL);
+    return URI(uri);
 }
 
+/** \fn gchar *URI::toString() const
+    \brief Returns a glib string version of this URI.
+    \return a glib string version of this URI.
+
+    The returned string must be freed with \c g_free().
+*/
 gchar *URI::Impl::toString() const {
-	xmlChar *string = xmlSaveUri(_uri);
-	if (string) {
-		/* hand the string off to glib memory management */
-		gchar *glib_string = g_strdup((gchar *)string);
-		xmlFree(string);
-		return glib_string;
-	} else {
-		return NULL;
-	}
+    xmlChar *string = xmlSaveUri(_uri);
+    if (string) {
+        /* hand the string off to glib memory management */
+        gchar *glib_string = g_strdup((gchar *)string);
+        xmlFree(string);
+        return glib_string;
+    } else {
+        return NULL;
+    }
 }
 
 };
 
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
--- src/uri.h
+++ src/uri.h
@@ -1,8 +1,10 @@
-/*
- * Classes for representing and manipulating URIs.
+/**
+ * \file
+ * \brief Classes for representing and manipulating URIs as per RFC 2396.
  *
  * Authors:
  *   MenTaLguY <mental@rydia.net>
+ *   Jon A. Cruz <jon@joncruz.org>
  *
  * Copyright (C) 2003 MenTaLguY
  *
@@ -19,43 +21,63 @@
 
 namespace Inkscape {
 
+/** \brief Represents an URI as per RFC 2396. */
 class URI {
 public:
-	URI(const URI &uri);
-	explicit URI(const gchar *uri_string) throw(BadURIException);
-	~URI();
+    URI(const URI &uri);
+    explicit URI(const gchar *uri_string) throw(BadURIException);
+    ~URI();
 
-	bool isRelative() const { return _impl->isRelative(); }
-	const gchar *getScheme() const { return _impl->getScheme(); }
-	const gchar *getQuery() const { return _impl->getQuery(); }
-	const gchar *getFragment() const { return _impl->getFragment(); }
+    bool isRelative() const { return _impl->isRelative(); }
+    bool isNetPath() const { return _impl->isNetPath(); }
+    bool isRelativePath() const { return _impl->isRelativePath(); }
+    bool isAbsolutePath() const { return _impl->isAbsolutePath(); }
+    const gchar *getScheme() const { return _impl->getScheme(); }
+    const gchar *getPath() const { return _impl->getPath(); }
+    const gchar *getQuery() const { return _impl->getQuery(); }
+    const gchar *getFragment() const { return _impl->getFragment(); }
 
-	static URI from_native_filename(gchar const *path) throw(BadURIException);
-	static gchar *to_native_filename(gchar const* uri) throw(BadURIException);
+    static URI from_native_filename(gchar const *path) throw(BadURIException);
+    static gchar *to_native_filename(gchar const* uri) throw(BadURIException);
 
-	gchar *toNativeFilename() const throw(BadURIException);
-	gchar *toString() const { return _impl->toString(); }
+    gchar *toNativeFilename() const throw(BadURIException);
+    gchar *toString() const { return _impl->toString(); }
 private:
-	class Impl {
-	public:
-		static Impl *create(xmlURIPtr uri);
-		void reference();
-		void unreference();
+    class Impl {
+    public:
+        static Impl *create(xmlURIPtr uri);
+        void reference();
+        void unreference();
 
-		bool isRelative() const;
-		const gchar *getScheme() const;
-		const gchar *getQuery() const;
-		const gchar *getFragment() const;
-		gchar *toString() const;
-	private:
-		Impl(xmlURIPtr uri);
-		~Impl();
-		int _refcount;
-		xmlURIPtr _uri;
-	};
-	Impl *_impl;
+        bool isRelative() const;
+        bool isNetPath() const;
+        bool isRelativePath() const;
+        bool isAbsolutePath() const;
+        const gchar *getScheme() const;
+        const gchar *getPath() const;
+        const gchar *getQuery() const;
+        const gchar *getFragment() const;
+        gchar *toString() const;
+    private:
+        Impl(xmlURIPtr uri);
+        ~Impl();
+        int _refcount;
+        xmlURIPtr _uri;
+    };
+    Impl *_impl;
 };
 
 }; /* namespace Inkscape */
 
 #endif
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
