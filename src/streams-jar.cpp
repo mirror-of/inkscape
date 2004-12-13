37b0efb5a9cdced90ec8722c8d556a3e6e96476b
Peter Moulder <peter.moulder@monash.edu>
2004-12-13 07:44:27 +0000

    noop: Add emacsen/vim mode lines.
    (bzr r5243)

--- src/streams-jar.cpp
+++ src/streams-jar.cpp
@@ -133,3 +133,14 @@ guint16 JarBuffer::unpack_2bytes(guint8 *data, int offset)
 }
 
 } // namespace Inkscape
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
