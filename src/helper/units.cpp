007d3a7e4932d50fcdcf522cdc8a8b524a4a9fc3
Peter Moulder <peter.moulder@monash.edu>
2004-12-22 09:17:57 +0000

    noop: CodingStyle
    (bzr r5339)

--- src/helper/units.cpp
+++ src/helper/units.cpp
@@ -19,9 +19,9 @@
 #include <math.h>
 #include <string.h>
 #include <glib.h>
-#include "units.h"
-#include "sp-intl.h"
-#include "../sp-metrics.h"
+#include "helper/units.h"
+#include "helper/sp-intl.h"
+#include "sp-metrics.h"
 
 /* todo: use some fancy unit program */
 
@@ -32,20 +32,20 @@
  * calls sp_unit_table_sane) to ensure that the two are in sync.
  */
 SPUnit const sp_units[] = {
-	{SP_UNIT_SCALE, SP_UNIT_DIMENSIONLESS, 1.0, 0, N_("Unit"), "", N_("Units"), ""},
-	{SP_UNIT_PT, SP_UNIT_ABSOLUTE, 1.0, SP_PT, N_("Point"), N_("pt"), N_("Points"), N_("Pt")},
-	{SP_UNIT_PX, SP_UNIT_DEVICE, 0.8, SP_PX, N_("Pixel"), N_("px"), N_("Pixels"), N_("Px")}, // yet another place where the 90dpi is hard-coded
-	/* Volatiles do not have default, so there are none here */
-	/* You can add new elements from this point forward */
-	{SP_UNIT_PERCENT, SP_UNIT_DIMENSIONLESS, 0.01, 0, N_("Percent"), N_("%"), N_("Percents"), N_("%")},
-	{SP_UNIT_MM, SP_UNIT_ABSOLUTE, (720. / 254.), SP_MM, N_("Millimeter"), N_("mm"), N_("Millimeters"), N_("mm")},
-	{SP_UNIT_CM, SP_UNIT_ABSOLUTE, (7200. / 254.), SP_CM, N_("Centimeter"), N_("cm"), N_("Centimeters"), N_("cm")},
-	{SP_UNIT_M, SP_UNIT_ABSOLUTE, (720000. / 254.), SP_M, N_("Meter"), N_("m"), N_("Meters"), N_("m")},
-	{SP_UNIT_IN, SP_UNIT_ABSOLUTE, (72.0), SP_IN, N_("Inch"), N_("in"), N_("Inches"), N_("in")},
-	// TRANSLATORS: for info, see http://www.w3.org/TR/REC-CSS2/syndata.html#length-units
-	{SP_UNIT_EM, SP_UNIT_VOLATILE, 1.0, 0, N_("Em square"), N_("em"), N_("Em squares"), N_("em")},
-	// TRANSLATORS: for info, see http://www.w3.org/TR/REC-CSS2/syndata.html#length-units
-	{SP_UNIT_EX, SP_UNIT_VOLATILE, 1.0, 0, N_("Ex square"), N_("ex"), N_("Ex squares"), N_("ex")},
+    {SP_UNIT_SCALE, SP_UNIT_DIMENSIONLESS, 1.0, 0, N_("Unit"), "", N_("Units"), ""},
+    {SP_UNIT_PT, SP_UNIT_ABSOLUTE, 1.0, SP_PT, N_("Point"), N_("pt"), N_("Points"), N_("Pt")},
+    {SP_UNIT_PX, SP_UNIT_DEVICE, 0.8, SP_PX, N_("Pixel"), N_("px"), N_("Pixels"), N_("Px")}, // yet another place where the 90dpi is hard-coded
+    /* Volatiles do not have default, so there are none here */
+    /* You can add new elements from this point forward */
+    {SP_UNIT_PERCENT, SP_UNIT_DIMENSIONLESS, 0.01, 0, N_("Percent"), N_("%"), N_("Percents"), N_("%")},
+    {SP_UNIT_MM, SP_UNIT_ABSOLUTE, (720. / 254.), SP_MM, N_("Millimeter"), N_("mm"), N_("Millimeters"), N_("mm")},
+    {SP_UNIT_CM, SP_UNIT_ABSOLUTE, (7200. / 254.), SP_CM, N_("Centimeter"), N_("cm"), N_("Centimeters"), N_("cm")},
+    {SP_UNIT_M, SP_UNIT_ABSOLUTE, (720000. / 254.), SP_M, N_("Meter"), N_("m"), N_("Meters"), N_("m")},
+    {SP_UNIT_IN, SP_UNIT_ABSOLUTE, (72.0), SP_IN, N_("Inch"), N_("in"), N_("Inches"), N_("in")},
+    // TRANSLATORS: for info, see http://www.w3.org/TR/REC-CSS2/syndata.html#length-units
+    {SP_UNIT_EM, SP_UNIT_VOLATILE, 1.0, 0, N_("Em square"), N_("em"), N_("Em squares"), N_("em")},
+    // TRANSLATORS: for info, see http://www.w3.org/TR/REC-CSS2/syndata.html#length-units
+    {SP_UNIT_EX, SP_UNIT_VOLATILE, 1.0, 0, N_("Ex square"), N_("ex"), N_("Ex squares"), N_("ex")},
 };
 
 #define sp_num_units G_N_ELEMENTS(sp_units)
@@ -54,156 +54,156 @@ SPUnit const sp_units[] = {
 /* Base units are the ones used by gnome-print and paper descriptions */
 
 /* todo: Change param to SPUnitBase. */
-const SPUnit *
-sp_unit_get_identity (guint base)
+SPUnit const *
+sp_unit_get_identity(guint const base)
 {
-	SPUnitId ret_id;
-	switch (base) {
-	case SP_UNIT_DIMENSIONLESS:
-		ret_id = SP_UNIT_SCALE;
-		break;
-	case SP_UNIT_ABSOLUTE:
-		ret_id = SP_UNIT_PT;
-		break;
-	case SP_UNIT_DEVICE:
-		ret_id = SP_UNIT_PX;
-		break;
-	default:
-		g_warning ("file %s: line %d: Illegal unit base 0x%x", __FILE__, __LINE__, base);
-		return NULL;
-		break;
-	}
-	SPUnit const &ret = sp_units[ret_id];
-	g_assert(guint(ret.base) == base);
-	g_assert(ret.unittobase == 1.0);
-	return &ret;
+    SPUnitId ret_id;
+    switch (base) {
+        case SP_UNIT_DIMENSIONLESS:
+            ret_id = SP_UNIT_SCALE;
+            break;
+        case SP_UNIT_ABSOLUTE:
+            ret_id = SP_UNIT_PT;
+            break;
+        case SP_UNIT_DEVICE:
+            ret_id = SP_UNIT_PX;
+            break;
+        default:
+            g_warning("file %s: line %d: Illegal unit base 0x%x", __FILE__, __LINE__, base);
+            return NULL;
+            break;
+    }
+    SPUnit const &ret = sp_units[ret_id];
+    g_assert(guint(ret.base) == base);
+    g_assert(ret.unittobase == 1.0);
+    return &ret;
 }
 
-const SPUnit *
-sp_unit_get_by_abbreviation (const gchar *abbreviation)
+SPUnit const *
+sp_unit_get_by_abbreviation(gchar const *abbreviation)
 {
-	g_return_val_if_fail (abbreviation != NULL, NULL);
+    g_return_val_if_fail(abbreviation != NULL, NULL);
 
-	for (unsigned i = 0 ; i < sp_num_units ; i++) {
-		if (!g_strcasecmp (abbreviation, sp_units[i].abbr)) return &sp_units[i];
-		if (!g_strcasecmp (abbreviation, sp_units[i].abbr_plural)) return &sp_units[i];
-	}
+    for (unsigned i = 0 ; i < sp_num_units ; i++) {
+        if (!g_strcasecmp(abbreviation, sp_units[i].abbr)) return &sp_units[i];
+        if (!g_strcasecmp(abbreviation, sp_units[i].abbr_plural)) return &sp_units[i];
+    }
 
-	return NULL;
+    return NULL;
 }
 
-const gchar *
-sp_unit_get_abbreviation (const SPUnit *unit)
+gchar const *
+sp_unit_get_abbreviation(SPUnit const *unit)
 {
-	g_return_val_if_fail (unit != NULL, NULL);
+    g_return_val_if_fail(unit != NULL, NULL);
 
-	return unit->abbr;
+    return unit->abbr;
 }
 
 guint
-sp_unit_get_metric (const SPUnit *unit)
+sp_unit_get_metric(SPUnit const *unit)
 {
-	g_return_val_if_fail (unit != NULL, 0);
+    g_return_val_if_fail(unit != NULL, 0);
 
-	return unit->metric;
+    return unit->metric;
 }
 
 GSList *
-sp_unit_get_list (guint bases)
+sp_unit_get_list(guint bases)
 {
-	g_return_val_if_fail ((bases & ~SP_UNITS_ALL) == 0, NULL);
+    g_return_val_if_fail((bases & ~SP_UNITS_ALL) == 0, NULL);
 
-	GSList *units = NULL;
-	for (unsigned i = sp_num_units ; i--; ) {
-		if (bases & sp_units[i].base) {
-			units = g_slist_prepend (units, (gpointer) &sp_units[i]);
-		}
-	}
+    GSList *units = NULL;
+    for (unsigned i = sp_num_units ; i--; ) {
+        if (bases & sp_units[i].base) {
+            units = g_slist_prepend(units, (gpointer) &sp_units[i]);
+        }
+    }
 
-	return units;
+    return units;
 }
 
 void
-sp_unit_free_list (GSList *units)
+sp_unit_free_list(GSList *units)
 {
-	g_slist_free (units);
+    g_slist_free(units);
 }
 
 /* These are pure utility */
 /* Return TRUE if conversion is possible */
 gboolean
-sp_convert_distance (gdouble *distance, const SPUnit *from, const SPUnit *to)
+sp_convert_distance(gdouble *distance, SPUnit const *from, SPUnit const *to)
 {
-	g_return_val_if_fail (distance != NULL, FALSE);
-	g_return_val_if_fail (from != NULL, FALSE);
-	g_return_val_if_fail (to != NULL, FALSE);
+    g_return_val_if_fail(distance != NULL, FALSE);
+    g_return_val_if_fail(from != NULL, FALSE);
+    g_return_val_if_fail(to != NULL, FALSE);
 
-	if (from == to) return TRUE;
-	if ((from->base == SP_UNIT_DIMENSIONLESS) || (to->base == SP_UNIT_DIMENSIONLESS)) {
-		*distance = *distance * from->unittobase / to->unittobase;
-		return TRUE;
-	}
-	if ((from->base == SP_UNIT_VOLATILE) || (to->base == SP_UNIT_VOLATILE)) return FALSE;
+    if (from == to) return TRUE;
+    if ((from->base == SP_UNIT_DIMENSIONLESS) || (to->base == SP_UNIT_DIMENSIONLESS)) {
+        *distance = *distance * from->unittobase / to->unittobase;
+        return TRUE;
+    }
+    if ((from->base == SP_UNIT_VOLATILE) || (to->base == SP_UNIT_VOLATILE)) return FALSE;
 
-	if ((from->base == SP_UNIT_DEVICE) && (to->base == SP_UNIT_ABSOLUTE) || 
-			(from->base == SP_UNIT_ABSOLUTE) && (to->base == SP_UNIT_DEVICE)) {
-		*distance = *distance * from->unittobase / to->unittobase;
-		return TRUE;
-	}
+    if ((from->base == SP_UNIT_DEVICE) && (to->base == SP_UNIT_ABSOLUTE) ||
+        (from->base == SP_UNIT_ABSOLUTE) && (to->base == SP_UNIT_DEVICE)) {
+        *distance = *distance * from->unittobase / to->unittobase;
+        return TRUE;
+    }
 
-	if (from->base != to->base) return FALSE;
+    if (from->base != to->base) return FALSE;
 
-	*distance = *distance * from->unittobase / to->unittobase;
-	return TRUE;
+    *distance = *distance * from->unittobase / to->unittobase;
+    return TRUE;
 }
 
 /** @param devicetransform for device units. */
 /* TODO: Remove the ctmscale parameter given that we no longer have SP_UNIT_USERSPACE. */
 gdouble
 sp_convert_distance_full(gdouble const from_dist, SPUnit const &from, SPUnit const &to,
-			 gdouble const devicescale)
+                         gdouble const devicescale)
 {
-	if (&from == &to) {
-		return from_dist;
-	}
-	if (from.base == to.base) {
-		gdouble ret = from_dist;
-		bool const succ = sp_convert_distance(&ret, &from, &to);
-		g_assert(succ);
-		return ret;
-	}
-	if ((from.base == SP_UNIT_DIMENSIONLESS)
-	    || (to.base == SP_UNIT_DIMENSIONLESS))
-	{
-		return from_dist * from.unittobase / to.unittobase;
-	}
-	g_return_val_if_fail(((from.base != SP_UNIT_VOLATILE)
-			      && (to.base != SP_UNIT_VOLATILE)),
-			     from_dist);
+    if (&from == &to) {
+        return from_dist;
+    }
+    if (from.base == to.base) {
+        gdouble ret = from_dist;
+        bool const succ = sp_convert_distance(&ret, &from, &to);
+        g_assert(succ);
+        return ret;
+    }
+    if ((from.base == SP_UNIT_DIMENSIONLESS)
+        || (to.base == SP_UNIT_DIMENSIONLESS))
+    {
+        return from_dist * from.unittobase / to.unittobase;
+    }
+    g_return_val_if_fail(((from.base != SP_UNIT_VOLATILE)
+                          && (to.base != SP_UNIT_VOLATILE)),
+                         from_dist);
 
-	gdouble absolute;
-	switch (from.base) {
-	case SP_UNIT_ABSOLUTE:
-	case SP_UNIT_DEVICE:
-		absolute = from_dist * from.unittobase;
-		break;
-	default:
-		g_warning("file %s: line %d: Illegal unit (base 0x%x)", __FILE__, __LINE__, from.base);
-		return from_dist;
-	}
+    gdouble absolute;
+    switch (from.base) {
+        case SP_UNIT_ABSOLUTE:
+        case SP_UNIT_DEVICE:
+            absolute = from_dist * from.unittobase;
+            break;
+        default:
+            g_warning("file %s: line %d: Illegal unit (base 0x%x)", __FILE__, __LINE__, from.base);
+            return from_dist;
+    }
 
-	gdouble ret;
-	switch (to.base) {
-	default:
-		g_warning("file %s: line %d: Illegal unit (base 0x%x)", __FILE__, __LINE__, to.base);
-		/* FALL-THROUGH */
-	case SP_UNIT_ABSOLUTE:
-	case SP_UNIT_DEVICE:
-		ret = absolute / to.unittobase;
-		break;
-	}
+    gdouble ret;
+    switch (to.base) {
+        default:
+            g_warning("file %s: line %d: Illegal unit (base 0x%x)", __FILE__, __LINE__, to.base);
+            /* FALL-THROUGH */
+        case SP_UNIT_ABSOLUTE:
+        case SP_UNIT_DEVICE:
+            ret = absolute / to.unittobase;
+            break;
+    }
 
-	return ret;
+    return ret;
 }
 
 /* Some more convenience */
@@ -212,32 +212,44 @@ sp_convert_distance_full(gdouble const from_dist, SPUnit const &from, SPUnit con
 gdouble
 sp_units_get_points(gdouble const units, SPUnit const &unit)
 {
-	if (unit.base == SP_UNIT_ABSOLUTE || unit.base == SP_UNIT_DEVICE) {
-		return units * unit.unittobase;
-	} else {
-		g_warning("Different unit bases: No exact unit conversion available");
-		return units * unit.unittobase;
-	}
+    if (unit.base == SP_UNIT_ABSOLUTE || unit.base == SP_UNIT_DEVICE) {
+        return units * unit.unittobase;
+    } else {
+        g_warning("Different unit bases: No exact unit conversion available");
+        return units * unit.unittobase;
+    }
 }
 
 gdouble
 sp_points_get_units(gdouble const points, SPUnit const &unit)
 {
-	if (unit.base == SP_UNIT_ABSOLUTE || unit.base == SP_UNIT_DEVICE) {
-		return points / unit.unittobase;
-	} else {
-		g_warning("Different unit bases: No exact unit conversion available");
-		return points / unit.unittobase;
-	}
+    if (unit.base == SP_UNIT_ABSOLUTE || unit.base == SP_UNIT_DEVICE) {
+        return points / unit.unittobase;
+    } else {
+        g_warning("Different unit bases: No exact unit conversion available");
+        return points / unit.unittobase;
+    }
 }
 
 bool
 sp_units_table_sane()
 {
-	for (unsigned i = 0; i < G_N_ELEMENTS(sp_units); ++i) {
-		if (unsigned(sp_units[i].unit_id) != i) {
-			return false;
-		}
-	}
-	return true;
+    for (unsigned i = 0; i < G_N_ELEMENTS(sp_units); ++i) {
+        if (unsigned(sp_units[i].unit_id) != i) {
+            return false;
+        }
+    }
+    return true;
 }
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
--- src/sp-namedview.h
+++ src/sp-namedview.h
@@ -1,5 +1,5 @@
-#ifndef __SP_NAMEDVIEW_H__
-#define __SP_NAMEDVIEW_H__
+#ifndef INKSCAPE_SP_NAMEDVIEW_H
+#define INKSCAPE_SP_NAMEDVIEW_H
 
 /*
  * <sodipodi:namedview> implementation
@@ -12,11 +12,11 @@
  * Released under GNU GPL, read the file 'COPYING' for more information
  */
 
-#define SP_TYPE_NAMEDVIEW (sp_namedview_get_type ())
-#define SP_NAMEDVIEW(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), SP_TYPE_NAMEDVIEW, SPNamedView))
-#define SP_NAMEDVIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), SP_TYPE_NAMEDVIEW, SPNamedViewClass))
-#define SP_IS_NAMEDVIEW(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), SP_TYPE_NAMEDVIEW))
-#define SP_IS_NAMEDVIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), SP_TYPE_NAMEDVIEW))
+#define SP_TYPE_NAMEDVIEW (sp_namedview_get_type())
+#define SP_NAMEDVIEW(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), SP_TYPE_NAMEDVIEW, SPNamedView))
+#define SP_NAMEDVIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), SP_TYPE_NAMEDVIEW, SPNamedViewClass))
+#define SP_IS_NAMEDVIEW(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), SP_TYPE_NAMEDVIEW))
+#define SP_IS_NAMEDVIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), SP_TYPE_NAMEDVIEW))
 
 #include "helper/helper-forward.h"
 #include "sp-object-group.h"
@@ -26,82 +26,90 @@
 
 
 enum {
-	SP_BORDER_LAYER_BOTTOM,
-	SP_BORDER_LAYER_TOP
+    SP_BORDER_LAYER_BOTTOM,
+    SP_BORDER_LAYER_TOP
 };
 
 struct SPNamedView : public SPObjectGroup {
-	unsigned int editable : 1;
-	unsigned int showgrid : 1;
-	unsigned int showguides : 1;
-	unsigned int showborder : 1;
-	unsigned int borderlayer : 2;
+    unsigned int editable : 1;
+    unsigned int showgrid : 1;
+    unsigned int showguides : 1;
+    unsigned int showborder : 1;
+    unsigned int borderlayer : 2;
 
-	double zoom;
-	double cx;
-	double cy;
-	gint window_width;
-	gint window_height;
-	gint window_x;
-	gint window_y;
+    double zoom;
+    double cx;
+    double cy;
+    gint window_width;
+    gint window_height;
+    gint window_x;
+    gint window_y;
 
-	GridSnapper grid_snapper;
-	GuideSnapper guide_snapper;
+    GridSnapper grid_snapper;
+    GuideSnapper guide_snapper;
 
-	const SPUnit *gridunit;
-	/* Grid data is in points regardless of unit */
-	NR::Point gridorigin;
-	gdouble gridspacing[2];
-	gint gridempspacing;
+    SPUnit const *gridunit;
+    /* Grid data is in points regardless of unit */
+    NR::Point gridorigin;
+    gdouble gridspacing[2];
+    gint gridempspacing;
 
-	const SPUnit *doc_units;
+    SPUnit const *doc_units;
 
-	const SPUnit *gridtoleranceunit;
-	gdouble gridtolerance;
+    SPUnit const *gridtoleranceunit;
+    gdouble gridtolerance;
 
-	const SPUnit *guidetoleranceunit;
-	gdouble guidetolerance;
+    SPUnit const *guidetoleranceunit;
+    gdouble guidetolerance;
 
-	GQuark default_layer_id;
+    GQuark default_layer_id;
 
-	guint32 gridcolor;
-	guint32 gridempcolor;
-	guint32 guidecolor;
-	guint32 guidehicolor;
-	guint32 bordercolor;
-	guint32 pagecolor;
-	guint32 pageshadow;
+    guint32 gridcolor;
+    guint32 gridempcolor;
+    guint32 guidecolor;
+    guint32 guidehicolor;
+    guint32 bordercolor;
+    guint32 pagecolor;
+    guint32 pageshadow;
 
-	GSList * guides;
-	GSList * views;
-	GSList * gridviews;
-	gint viewcount;
+    GSList *guides;
+    GSList *views;
+    GSList *gridviews;
+    gint viewcount;
 };
 
 struct SPNamedViewClass {
-	SPObjectGroupClass parent_class;
+    SPObjectGroupClass parent_class;
 };
 
-GType sp_namedview_get_type (void);
+GType sp_namedview_get_type(void);
 
-void sp_namedview_show (SPNamedView * namedview, gpointer desktop);
-void sp_namedview_hide (SPNamedView * namedview, gpointer desktop);
+void sp_namedview_show(SPNamedView *namedview, gpointer desktop);
+void sp_namedview_hide(SPNamedView *namedview, gpointer desktop);
 
-void sp_namedview_activate_guides (SPNamedView * nv, gpointer desktop, gboolean active);
-guint sp_namedview_viewcount (SPNamedView * nv);
-const gchar * sp_namedview_get_name (SPNamedView * nv);
-const GSList * sp_namedview_view_list (SPNamedView * nv);
+void sp_namedview_activate_guides(SPNamedView *nv, gpointer desktop, gboolean active);
+guint sp_namedview_viewcount(SPNamedView *nv);
+gchar const *sp_namedview_get_name(SPNamedView *nv);
+GSList const *sp_namedview_view_list(SPNamedView *nv);
 
-SPNamedView *sp_document_namedview (SPDocument * document, const gchar * name);
+SPNamedView *sp_document_namedview(SPDocument *document, gchar const *name);
 
-void sp_namedview_window_from_document (SPDesktop *desktop);
-void sp_namedview_document_from_window (SPDesktop *desktop);
+void sp_namedview_window_from_document(SPDesktop *desktop);
+void sp_namedview_document_from_window(SPDesktop *desktop);
 
-void sp_namedview_toggle_guides (SPDocument *doc, SPRepr *repr);
-void sp_namedview_toggle_grid (SPDocument *doc, SPRepr *repr);
+void sp_namedview_toggle_guides(SPDocument *doc, SPRepr *repr);
+void sp_namedview_toggle_grid(SPDocument *doc, SPRepr *repr);
 
-#endif
-
-
+#endif /* !INKSCAPE_SP_NAMEDVIEW_H */
 
 
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
