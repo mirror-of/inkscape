7f489d88229bd2ed2bbaebd6664105261aedc5f8
bulia byak <buliabyak@gmail.com>
2005-01-02 06:52:39 +0000

    more constants for px
    (bzr r5454)

--- src/unit-constants.h
+++ src/unit-constants.h
@@ -8,6 +8,7 @@
 
 #define PT_PER_IN 72.0
 #define PT_PER_PX DEVICESCALE
+#define PX_PER_PT 1/DEVICESCALE
 #define PX_PER_IN PT_PER_IN / PT_PER_PX 
 #define M_PER_IN 0.0254
 #define CM_PER_IN 2.54
@@ -19,13 +20,17 @@
 #define IN_PER_CM (1 / CM_PER_IN)
 #define IN_PER_MM (1 / MM_PER_IN)
 #define PT_PER_CM (PT_PER_IN / CM_PER_IN)
+#define PX_PER_CM (PX_PER_IN / CM_PER_IN)
 #define M_PER_PT (M_PER_IN / PT_PER_IN)
 #define PT_PER_M (PT_PER_IN / M_PER_IN)
+#define PX_PER_M (PX_PER_IN / M_PER_IN)
 #define CM_PER_PT (CM_PER_IN / PT_PER_IN)
 #define MM_PER_PT (MM_PER_IN / PT_PER_IN)
 #define PT_PER_MM (PT_PER_IN / MM_PER_IN)
+#define PX_PER_MM (PX_PER_IN / MM_PER_IN)
 #define MM_PER_PX (MM_PER_IN / PX_PER_IN)
 #define PT_PER_PT 1.0
 #define IN_PER_IN 1.0
+#define PX_PER_PX 1.0
 
 #endif /* !INKSCAPE_UNIT_CONSTANTS_H */
