23066d4bc4e80e7481e87a553a7d0d3bcf4b8494
bulia byak <buliabyak@gmail.com>
2005-01-02 19:49:06 +0000

    more constants
    (bzr r5461)

--- src/unit-constants.h
+++ src/unit-constants.h
@@ -2,8 +2,8 @@
 #define INKSCAPE_UNIT_CONSTANTS_H
 
 // 72 points per inch divided by the SVG-recommended value of 90 pixels per inch for computer screen
-// For now it is constant throughout Inkscape, later we may make it changeable 
-// (though this define is far from the only place to change it!)
+// For now it is constant throughout Inkscape, later we may make it changeable.
+// Ideally this should be the only place to change it, but this is not guaranteed (be careful!)
 #define DEVICESCALE 0.8    
 
 #define PT_PER_IN 72.0
@@ -17,6 +17,7 @@
 #define MM_PER_CM 10.0
 #define MM_PER_M 1000.0
 #define IN_PER_PT (1 / PT_PER_IN)
+#define IN_PER_PX (1 / PX_PER_IN)
 #define IN_PER_CM (1 / CM_PER_IN)
 #define IN_PER_MM (1 / MM_PER_IN)
 #define PT_PER_CM (PT_PER_IN / CM_PER_IN)
@@ -25,6 +26,7 @@
 #define PT_PER_M (PT_PER_IN / M_PER_IN)
 #define PX_PER_M (PX_PER_IN / M_PER_IN)
 #define CM_PER_PT (CM_PER_IN / PT_PER_IN)
+#define CM_PER_PX (CM_PER_IN / PX_PER_IN)
 #define MM_PER_PT (MM_PER_IN / PT_PER_IN)
 #define PT_PER_MM (PT_PER_IN / MM_PER_IN)
 #define PX_PER_MM (PX_PER_IN / MM_PER_IN)
