8bace9557dd8dd29b2127404a8f29f6db731bb4e
Bob Jamison <ishmalius@gmail.com>
2005-01-03 20:11:23 +0000

    smoothe before quant on multi
    (bzr r5486)

--- src/trace/potrace/inkscape-potrace.cpp
+++ src/trace/potrace/inkscape-potrace.cpp
@@ -321,7 +321,9 @@ filterIndexed(PotraceTracingEngine &engine, GdkPixbuf * pixbuf)
     if (engine.getTraceType() == TRACE_QUANT_COLOR)
         {
         RgbMap *gm = gdkPixbufToRgbMap(pixbuf);
-        newGm = rgbMapQuantize(gm, 4, engine.getQuantScanNrColors());
+        RgbMap *gaussMap = rgbMapGaussian(gm);
+        newGm = rgbMapQuantize(gaussMap, 4, engine.getQuantScanNrColors());
+        gaussMap->destroy(gaussMap);
         gm->destroy(gm);
         }
 
@@ -329,7 +331,9 @@ filterIndexed(PotraceTracingEngine &engine, GdkPixbuf * pixbuf)
     else if (engine.getTraceType() == TRACE_QUANT_MONO)
         {
         RgbMap *gm = gdkPixbufToRgbMap(pixbuf);
-        newGm      = rgbMapQuantize(gm, 4, engine.getQuantScanNrColors());
+        RgbMap *gaussMap = rgbMapGaussian(gm);
+        newGm = rgbMapQuantize(gaussMap, 4, engine.getQuantScanNrColors());
+        gaussMap->destroy(gaussMap);
         gm->destroy(gm);
 
         //Turn to grays
