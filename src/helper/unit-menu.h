5d46cb0f38e38ca57e610f2b99c7ce8c1b267038
Peter Moulder <peter.moulder@monash.edu>
2005-01-03 00:51:32 +0000

    CodingStyle changes
    (bzr r5470)

--- src/helper/unit-menu.h
+++ src/helper/unit-menu.h
@@ -18,30 +18,30 @@
 
 /* Unit selector Widget */
 
-#define SP_TYPE_UNIT_SELECTOR (sp_unit_selector_get_type ())
-#define SP_UNIT_SELECTOR(o) (GTK_CHECK_CAST ((o), SP_TYPE_UNIT_SELECTOR, SPUnitSelector))
-#define SP_UNIT_SELECTOR_CLASS(k) (GTK_CHECK_CLASS_CAST ((k), SP_TYPE_UNIT_SELECTOR, SPUnitSelectorClass))
-#define SP_IS_UNIT_SELECTOR(o) (GTK_CHECK_TYPE ((o), SP_TYPE_UNIT_SELECTOR))
-#define SP_IS_UNIT_SELECTOR_CLASS(k) (GTK_CHECK_CLASS_TYPE ((k), SP_TYPE_UNIT_SELECTOR))
+#define SP_TYPE_UNIT_SELECTOR (sp_unit_selector_get_type())
+#define SP_UNIT_SELECTOR(o) (GTK_CHECK_CAST((o), SP_TYPE_UNIT_SELECTOR, SPUnitSelector))
+#define SP_UNIT_SELECTOR_CLASS(k) (GTK_CHECK_CLASS_CAST((k), SP_TYPE_UNIT_SELECTOR, SPUnitSelectorClass))
+#define SP_IS_UNIT_SELECTOR(o) (GTK_CHECK_TYPE((o), SP_TYPE_UNIT_SELECTOR))
+#define SP_IS_UNIT_SELECTOR_CLASS(k) (GTK_CHECK_CLASS_TYPE((k), SP_TYPE_UNIT_SELECTOR))
 
-GType sp_unit_selector_get_type (void);
+GType sp_unit_selector_get_type(void);
 
-GtkWidget *sp_unit_selector_new (guint bases);
-void sp_unit_selector_setsize (GtkWidget *us, guint w, guint h);
+GtkWidget *sp_unit_selector_new(guint bases);
+void sp_unit_selector_setsize(GtkWidget *us, guint w, guint h);
 
-const SPUnit *sp_unit_selector_get_unit (SPUnitSelector *selector);
+SPUnit const *sp_unit_selector_get_unit(SPUnitSelector *selector);
 
-void sp_unit_selector_set_bases (SPUnitSelector *selector, guint bases);
-void sp_unit_selector_add_unit (SPUnitSelector *selector, const SPUnit *unit, int position);
+void sp_unit_selector_set_bases(SPUnitSelector *selector, guint bases);
+void sp_unit_selector_add_unit(SPUnitSelector *selector, SPUnit const *unit, int position);
 
-void sp_unit_selector_set_unit (SPUnitSelector *selector, const SPUnit *unit);
-void sp_unit_selector_add_adjustment (SPUnitSelector *selector, GtkAdjustment *adjustment);
-void sp_unit_selector_remove_adjustment (SPUnitSelector *selector, GtkAdjustment *adjustment);
+void sp_unit_selector_set_unit(SPUnitSelector *selector, SPUnit const *unit);
+void sp_unit_selector_add_adjustment(SPUnitSelector *selector, GtkAdjustment *adjustment);
+void sp_unit_selector_remove_adjustment(SPUnitSelector *selector, GtkAdjustment *adjustment);
 
-gboolean sp_unit_selector_update_test (SPUnitSelector const *selector);
+gboolean sp_unit_selector_update_test(SPUnitSelector const *selector);
 
-float sp_unit_selector_get_value_in_pixels (SPUnitSelector *selector, GtkAdjustment *adj);
-void sp_unit_selector_set_value_in_pixels (SPUnitSelector *selector, GtkAdjustment *adj, float value);
+float sp_unit_selector_get_value_in_pixels(SPUnitSelector *selector, GtkAdjustment *adj);
+void sp_unit_selector_set_value_in_pixels(SPUnitSelector *selector, GtkAdjustment *adj, float value);
 
 
 
