bc206b816fec7cbbeeb5c658e96773bf17ccd423
Peter Moulder <peter.moulder@monash.edu>
2005-01-04 07:17:43 +0000

    CodingStyle changes
    (bzr r5491)

--- src/sp-text.h
+++ src/sp-text.h
@@ -26,11 +26,11 @@
 #include "svg/svg-types.h"
 
 
-#define SP_TYPE_TEXT (sp_text_get_type ())
-#define SP_TEXT(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), SP_TYPE_TEXT, SPText))
-#define SP_TEXT_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), SP_TYPE_TEXT, SPTextClass))
-#define SP_IS_TEXT(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), SP_TYPE_TEXT))
-#define SP_IS_TEXT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), SP_TYPE_TEXT))
+#define SP_TYPE_TEXT (sp_text_get_type())
+#define SP_TEXT(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), SP_TYPE_TEXT, SPText))
+#define SP_TEXT_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), SP_TYPE_TEXT, SPTextClass))
+#define SP_IS_TEXT(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), SP_TYPE_TEXT))
+#define SP_IS_TEXT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), SP_TYPE_TEXT))
 
 /* Text specific flags */
 #define SP_TEXT_CONTENT_MODIFIED_FLAG SP_OBJECT_USER_MODIFIED_FLAG_A
@@ -43,43 +43,54 @@ class flow_res;
 /* SPText */
 
 struct SPText : public SPItem {
-	div_flow_src   contents;
-	SPSVGLength		 x,y;
-	SPSVGLength    linespacing;
+    div_flow_src contents;
+    SPSVGLength x,y;
+    SPSVGLength linespacing;
 	
-	guint relayout : 1;
+    guint relayout : 1;
 	
-	flow_src*      f_src;
-	flow_res*      f_res;
+    flow_src *f_src;
+    flow_res *f_res;
 
-	void           ClearFlow(NRArenaGroup* in_arena);
-	void           BuildFlow(NRArenaGroup* in_arena, NRRect *paintbox);
-	void					 UpdateFlowSource(void);
-	void					 ComputeFlowRes(void);
+    void ClearFlow(NRArenaGroup *in_arena);
+    void BuildFlow(NRArenaGroup *in_arena, NRRect *paintbox);
+    void UpdateFlowSource();
+    void ComputeFlowRes();
 };
 
 struct SPTextClass {
-	SPItemClass parent_class;
+    SPItemClass parent_class;
 };
 
-#define SP_TEXT_CHILD_STRING(c) (SP_IS_TSPAN (c) ? SP_TSPAN_STRING (c) : SP_STRING (c))
+#define SP_TEXT_CHILD_STRING(c) ( SP_IS_TSPAN(c) ? SP_TSPAN_STRING(c) : SP_STRING(c) )
 
-GType sp_text_get_type ();
+GType sp_text_get_type();
 
-gchar *sp_text_get_string_multiline (SPText *text);
-void sp_text_set_repr_text_multiline (SPText *text, const gchar *str);
+gchar *sp_text_get_string_multiline(SPText *text);
+void sp_text_set_repr_text_multiline(SPText *text, gchar const *str);
 
-SPCurve *sp_text_normalized_bpath (SPText *text);
+SPCurve *sp_text_normalized_bpath(SPText *text);
 
 /* fixme: Think about these (Lauris) */
 
-SPTSpan *sp_text_append_line (SPText *text);
-int sp_text_insert_line (SPText *text, gint pos);
+SPTSpan *sp_text_append_line(SPText *text);
+int sp_text_insert_line(SPText *text, gint pos);
 
-gint sp_text_append (SPText *text, const gchar *utf8);
+gint sp_text_append(SPText *text, gchar const *utf8);
 
-void sp_adjust_kerning_screen (SPText *text, gint pos, SPDesktop *desktop, NR::Point by);
-void sp_adjust_tspan_letterspacing_screen (SPText *text, gint pos, SPDesktop *desktop, gdouble by);
-void sp_adjust_linespacing_screen (SPText *text, SPDesktop *desktop, gdouble by);
+void sp_adjust_kerning_screen(SPText *text, gint pos, SPDesktop *desktop, NR::Point by);
+void sp_adjust_tspan_letterspacing_screen(SPText *text, gint pos, SPDesktop *desktop, gdouble by);
+void sp_adjust_linespacing_screen(SPText *text, SPDesktop *desktop, gdouble by);
 
 #endif
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
