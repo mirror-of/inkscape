6a711bf1757dda56d21e8c4bae4409fdd22f85e4
Jon A. Cruz <jon@joncruz.org>
2005-02-13 08:48:18 +0000

    Added in config.h that wasn't included by a side-effect any more
    (bzr r5822)

--- src/extension/plugin/gimpgrad.cpp
+++ src/extension/plugin/gimpgrad.cpp
@@ -13,6 +13,10 @@
  * Released under GNU GPL, read the file 'COPYING' for more information
  */
 
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include <glibmm/ustring.h>
 #include <extension/implementation/implementation.h>
 #include <color-rgba.h>
