fc9067abd9340e0a24061c8ed3220a3d3c12d6c7
Peter Moulder <peter.moulder@monash.edu>
2005-02-16 05:19:04 +0000

    noop: CodingStyle: Move declarations to first use, and add braces.
    (bzr r5859)

--- src/prefs-utils.cpp
+++ src/prefs-utils.cpp
@@ -21,8 +21,7 @@
 void
 prefs_set_int_attribute(gchar const *path, gchar const *attr, gint value)
 {
-    SPRepr *repr;
-    repr = inkscape_get_repr(INKSCAPE, path);
+    SPRepr *repr = inkscape_get_repr(INKSCAPE, path);
     if (repr) {
         sp_repr_set_int(repr, attr, value);
     }
@@ -31,12 +30,12 @@ prefs_set_int_attribute(gchar const *path, gchar const *attr, gint value)
 gint
 prefs_get_int_attribute(gchar const *path, gchar const *attr, gint def)
 {
-    SPRepr *repr;
-    repr = inkscape_get_repr(INKSCAPE, path);
+    SPRepr *repr = inkscape_get_repr(INKSCAPE, path);
     if (repr) {
         return sp_repr_get_int_attribute(repr, attr, def);
-    } else 
+    } else {
         return def;
+    }
 }
 
 /**
@@ -45,25 +44,23 @@ prefs_get_int_attribute(gchar const *path, gchar const *attr, gint def)
 gint
 prefs_get_int_attribute_limited(gchar const *path, gchar const *attr, gint def, gint min, gint max)
 {
-    gint v; 
-
-    SPRepr *repr;
-    repr = inkscape_get_repr(INKSCAPE, path);
+    SPRepr *repr = inkscape_get_repr(INKSCAPE, path);
     if (repr) {
-        v = sp_repr_get_int_attribute(repr, attr, def);
-        if (v >= min && v <= max)
+        gint const v = sp_repr_get_int_attribute(repr, attr, def);
+        if (v >= min && v <= max) {
             return v;
-        else
+        } else {
             return def;
-    } else 
+        }
+    } else {
         return def;
+    }
 }
 
 void
 prefs_set_double_attribute(gchar const *path, gchar const *attr, double value)
 {
-    SPRepr *repr;
-    repr = inkscape_get_repr(INKSCAPE, path);
+    SPRepr *repr = inkscape_get_repr(INKSCAPE, path);
     if (repr) {
         sp_repr_set_double(repr, attr, value);
     }
@@ -72,8 +69,7 @@ prefs_set_double_attribute(gchar const *path, gchar const *attr, double value)
 double
 prefs_get_double_attribute(gchar const *path, gchar const *attr, double def)
 {
-    SPRepr *repr;
-    repr = inkscape_get_repr(INKSCAPE, path);
+    SPRepr *repr = inkscape_get_repr(INKSCAPE, path);
     if (repr) {
         return sp_repr_get_double_attribute(repr, attr, def);
     } else 
@@ -86,25 +82,23 @@ prefs_get_double_attribute(gchar const *path, gchar const *attr, double def)
 double
 prefs_get_double_attribute_limited(gchar const *path, gchar const *attr, double def, double min, double max)
 {
-    double v; 
-
-    SPRepr *repr;
-    repr = inkscape_get_repr(INKSCAPE, path);
+    SPRepr *repr = inkscape_get_repr(INKSCAPE, path);
     if (repr) {
-        v = sp_repr_get_double_attribute(repr, attr, def);
-        if (v >= min && v <= max)
+        double const v = sp_repr_get_double_attribute(repr, attr, def);
+        if (v >= min && v <= max) {
             return v;
-        else
+        } else {
             return def;
-    } else 
+        }
+    } else {
         return def;
+    }
 }
 
 gchar const *
 prefs_get_string_attribute(gchar const *path, gchar const *attr)
 {
-    SPRepr *repr;
-    repr = inkscape_get_repr(INKSCAPE, path);
+    SPRepr *repr = inkscape_get_repr(INKSCAPE, path);
     if (repr) {
         return (char *) sp_repr_attr(repr, attr);
     }
@@ -114,18 +108,16 @@ prefs_get_string_attribute(gchar const *path, gchar const *attr)
 void
 prefs_set_string_attribute(gchar const *path, gchar const *attr, gchar const *value)
 {
-    SPRepr *repr;
-    repr = inkscape_get_repr(INKSCAPE, path);
+    SPRepr *repr = inkscape_get_repr(INKSCAPE, path);
     if (repr) {
         sp_repr_set_attr(repr, attr, value);
     }
 }
 
 void
-prefs_set_recent_file(const gchar * uri, const gchar * name)
+prefs_set_recent_file(gchar const *uri, gchar const *name)
 {
-    int i;
-    int max_documents = prefs_get_int_attribute("options.maxrecentdocuments", "value", 20);
+    int const max_documents = prefs_get_int_attribute("options.maxrecentdocuments", "value", 20);
 
     if (uri != NULL) {
         SPRepr *recent;
@@ -139,7 +131,7 @@ prefs_set_recent_file(const gchar * uri, const gchar * name)
                 if (recent->childCount() >= max_documents) {
                     child = recent->firstChild();
                     // count to the last
-                    for (i = 0; i < max_documents - 2; i ++) child = child->next();
+                    for (int i = 0; i < max_documents - 2; i ++) child = child->next();
                     // remove all after the last
                     while (child->next()) sp_repr_unparent(child->next());
                 }
@@ -152,28 +144,22 @@ prefs_set_recent_file(const gchar * uri, const gchar * name)
     }
 }
 
-const gchar **
-prefs_get_recent_files(void)
+gchar const **
+prefs_get_recent_files()
 {
-    SPRepr *recent;
-
-    recent = inkscape_get_repr(INKSCAPE, "documents.recent");
-
+    SPRepr *recent = inkscape_get_repr(INKSCAPE, "documents.recent");
     if (recent) {
-        SPRepr *child;
-        const gchar ** datalst;
+        unsigned const docs = recent->childCount();
+        gchar const **datalst = (gchar const **) g_malloc(sizeof(gchar *) * ((docs * 2) + 1));
+
         gint i;
-        gint docs = recent->childCount();
-
-        datalst = (const gchar **)g_malloc(sizeof(gchar *) * ((docs * 2) + 1));
-
+        SPRepr *child;
         for (i = 0, child = recent->firstChild();
              child != NULL;
-             child = child->next(), i += 2) {
-            const gchar *uri, *name;
-            uri = sp_repr_attr(child, "uri");
-            name = sp_repr_attr(child, "name");
-
+             child = child->next(), i += 2)
+        {
+            gchar const *uri = sp_repr_attr(child, "uri");
+            gchar const *name = sp_repr_attr(child, "name");
             datalst[i]     = uri;
             datalst[i + 1] = name;
         }
