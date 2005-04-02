96d6f278fd026558845dd35302ffdd48b3c7242d
bulia byak <buliabyak@gmail.com>
2005-04-02 21:24:51 +0000

    no preview above 1.5 megs
    (bzr r6444)

--- src/dialogs/filedialog.cpp
+++ src/dialogs/filedialog.cpp
@@ -542,7 +542,7 @@ bool SVGPreview::set(Glib::ustring &fileName, int dialogType)
             return FALSE;
             }
         long fileLen = info.st_size;
-        if (fileLen > 0x100000L)
+        if (fileLen > 0x150000L)
             {
             showingNoPreview = false;
             showTooLarge(fileLen);
