d1fd84c55893e22069084cac2df5fa1488821eed
Peter Moulder <peter.moulder@monash.edu>
2005-04-18 11:26:33 +0000

    Further regularization: Move some (Point,Matrix) operators from nr-matrix-ops.h to new file nr-point-matrix-ops.h
    (bzr r6567)

--- src/libnr/nr-matrix-ops.h
+++ src/libnr/nr-matrix-ops.h
@@ -3,7 +3,7 @@
 #define SEEN_NR_MATRIX_OPS_H
 
 #include <libnr/nr-matrix.h>
-
+#include <libnr/nr-point-matrix-ops.h>  // todo: get rid of this temporary #include.
 
 namespace NR {
 
@@ -22,29 +22,6 @@ inline bool operator!=(Matrix const &a, Matrix const &b)
     return !( a == b );
 }
 
-inline Point operator*(Point const &v, Matrix const &m)
-{
-#if 1  /* Which code makes it easier to see what's happening? */
-    NR::Point const xform_col0(m[0],
-                               m[2]);
-    NR::Point const xform_col1(m[1],
-                               m[3]);
-    NR::Point const xlate(m[4], m[5]);
-    return ( Point(dot(v, xform_col0),
-                   dot(v, xform_col1))
-             + xlate );
-#else
-    return Point(v[X] * m[0]  +  v[Y] * m[2]  +  m[4],
-                 v[X] * m[1]  +  v[Y] * m[3]  +  m[5]);
-#endif
-}
-
-inline Point &Point::operator*=(Matrix const &m)
-{
-    *this = *this * m;
-    return *this;
-}
-
 Matrix operator*(Matrix const &a, Matrix const &b);
 
 inline Matrix operator*(Matrix const &a, NRMatrix const &b)
@@ -61,9 +38,9 @@ inline Matrix operator*(Matrix const &a, NRMatrix const &b)
   Local Variables:
   mode:c++
   c-file-style:"stroustrup"
-  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
   indent-tabs-mode:nil
   fill-column:99
   End:
 */
-// vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
