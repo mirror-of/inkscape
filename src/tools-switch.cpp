b26a33365457e64d996931c352628e877c643468
bulia byak <buliabyak@gmail.com>
2005-04-23 14:25:52 +0000

    gradient tool statusbar fix
    (bzr r6614)

--- src/tools-switch.cpp
+++ src/tools-switch.cpp
@@ -190,7 +190,7 @@ tools_switch (SPDesktop *dt, int num)
 		sp_desktop_set_event_context (dt, SP_TYPE_GRADIENT_CONTEXT, tool_names[num]);
 		sp_desktop_activate_guides (dt, FALSE);
 		inkscape_eventcontext_set (SP_DT_EVENTCONTEXT (dt));
-		SP_VIEW(dt)->tipsMessageContext()->set(Inkscape::NORMAL_MESSAGE, _("<b>Drag</b> to create a gradient on selected objects, <b>drag handles</b> to adjust gradients."));
+		SP_VIEW(dt)->tipsMessageContext()->set(Inkscape::NORMAL_MESSAGE, _("<b>Drag</b> or <b>double click</b> to create a gradient on selected objects, <b>drag handles</b> to adjust gradients."));
 		break;
 	case TOOLS_ZOOM:
 		sp_desktop_set_event_context (dt, SP_TYPE_ZOOM_CONTEXT, tool_names[num]);
