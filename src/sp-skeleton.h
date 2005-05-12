6a0517dd442b59d3b97e558cd6f3af097a471225
Peter Moulder <peter.moulder@monash.edu>
2005-05-12 22:43:51 +0000

    CodingStyle, fix Doxygen usage.
    (bzr r6761)

--- src/sp-skeleton.h
+++ src/sp-skeleton.h
@@ -1,9 +1,10 @@
-#ifndef __SP_SKELETON_H__
-#define __SP_SKELETON_H__
+#ifndef SP_SKELETON_H_SEEN
+#define SP_SKELETON_H_SEEN
 
+/** \file
+ * SVG <skeleton> implementation, see sp-skeleton.cpp.
+ */
 /*
- * SVG <skeleton> implementation, see sp-skeleton.cpp
- *
  * Authors:
  *   Kees Cook <kees@outflux.net>
  *
@@ -16,9 +17,9 @@
 
 /* Skeleton base class */
 
-#define SP_TYPE_SKELETON (sp_skeleton_get_type ())
-#define SP_SKELETON(o) (G_TYPE_CHECK_INSTANCE_CAST ((o), SP_TYPE_SKELETON, SPSkeleton))
-#define SP_IS_SKELETON(o) (G_TYPE_CHECK_INSTANCE_TYPE ((o), SP_TYPE_SKELETON))
+#define SP_TYPE_SKELETON (sp_skeleton_get_type())
+#define SP_SKELETON(o) (G_TYPE_CHECK_INSTANCE_CAST((o), SP_TYPE_SKELETON, SPSkeleton))
+#define SP_IS_SKELETON(o) (G_TYPE_CHECK_INSTANCE_TYPE((o), SP_TYPE_SKELETON))
 
 class SPSkeleton;
 class SPSkeletonClass;
@@ -27,10 +28,21 @@ struct SPSkeleton : public SPObject {
 };
 
 struct SPSkeletonClass {
-	SPObjectClass parent_class;
+    SPObjectClass parent_class;
 };
 
-GType sp_skeleton_get_type (void);
+GType sp_skeleton_get_type();
 
-#endif
-// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
+
+#endif /* !SP_SKELETON_H_SEEN */
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
