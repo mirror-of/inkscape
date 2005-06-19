6ec2696bc6f4243e96524fd37dd94884272e0481
bulia byak <buliabyak@gmail.com>
2005-06-19 05:30:36 +0000

    initializer patch
    (bzr r7002)

--- src/dialogs/swatches.cpp
+++ src/dialogs/swatches.cpp
@@ -178,7 +178,7 @@ Gtk::Widget* ColorItem::getPreview(PreviewStyle style, ViewType view, Gtk::Built
 
         eek_preview_set_details( preview, (::PreviewStyle)style, (::ViewType)view, (::GtkIconSize)size );
 
-        GValue val = {0, {0, 0}};
+        GValue val = {0, {{0}, {0}}};
         g_value_init( &val, G_TYPE_BOOLEAN );
         g_value_set_boolean( &val, FALSE );
         g_object_set_property( G_OBJECT(preview), "focus-on-click", &val );
