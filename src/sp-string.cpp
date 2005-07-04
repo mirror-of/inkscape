eddf6c155f63ae4a15e9d53648df9d5804b8e6f8
bulia byak <buliabyak@gmail.com>
2005-07-04 14:56:37 +0000

    fix for text-ws-02-t.svg: tab is whitespace too
    (bzr r7140)

--- src/sp-string.cpp
+++ src/sp-string.cpp
@@ -131,7 +131,7 @@ sp_string_read_content(SPObject *object)
     if (object->xml_space.value == SP_XML_SPACE_PRESERVE) {
         for ( ; *xml_string ; xml_string = g_utf8_next_char(xml_string) ) {
             gunichar c = g_utf8_get_char(xml_string);
-            if (c == '\n') c = ' ';
+            if (c == '\n' || c == '\t') c = ' ';
             string->string += c;
         }
     }
