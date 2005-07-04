3657cc94f4786716fe0eafa8a766594e5199a64b
Peter Moulder <peter.moulder@monash.edu>
2005-07-04 03:53:48 +0000

    doc
    (bzr r7127)

--- src/sp-gradient-vector.h
+++ src/sp-gradient-vector.h
@@ -5,12 +5,23 @@
 #include <vector>
 #include "color.h"
 
+/**
+ * Differs from SPStop in that SPStop mirrors the \<stop\> element in the document, whereas
+ * SPGradientStop shows more the effective stop color.
+ *
+ * For example, SPGradientStop has no currentColor option: currentColor refers to the color
+ * property value of the gradient where currentColor appears, so we interpret currentColor before
+ * copying from SPStop to SPGradientStop.
+ */
 struct SPGradientStop {
     gdouble offset;
     SPColor color;
     gfloat opacity;
 };
 
+/**
+ * The effective gradient vector, after copying stops from the referenced gradient if necessary.
+ */
 struct SPGradientVector {
     bool built;
     std::vector<SPGradientStop> stops;
