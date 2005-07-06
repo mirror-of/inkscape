8746bf5db6506f2948faa03f0f7890303f4ffb5f
mjwybrow <mjwybrow@users.sourceforge.net>
2005-07-06 09:11:46 +0000

    o  Changes and new files required to build an Inkscape dmg on Mac OS X.
    (bzr r7170)

--- packaging/osx-app.sh
+++ packaging/osx-app.sh
@@ -2,24 +2,35 @@
 # Copyright 2005, Kees Cook <kees@outflux.net>
 # Licensed under GNU General Public License
 #
-# Usage: osx-app Inkscape /path/to/bin/inkscape
+# Usage: osx-app /path/to/bin/inkscape Info.plist /path/to/packaging/macosx
 #
 # This attempts to build an Inkscape.app package for OSX, resolving
 # Dynamic libraries, etc.
 #
 # Thanks to GNUnet's "build_app" script for help with library dep resolution.
 # https://gnunet.org/svn/GNUnet/contrib/OSX/build_app
+# 
+# Fixes and modifications to use Gimp.app style launcher:
+# Michael Wybrow <mjwybrow@users.sourceforge.net>
+#
+#
+# Notes:
+# The Info.plist file can be found in the base inkscape directory once
+# configure has been run.
+# The macosx directory is in the inkscape/packaging directory.
+#
+# Thus, the usual use of this file would be to run it from the within the
+# inkscape/packaging directory, substituting in the inkscape binary path:
+# 	./osx-app.sh /path/to/bin/inkscape ../Info.plist macosx
 
 SW=/sw
 
-pkg=$1
-if [ -z $pkg ]; then
-        echo "What do you want to call the package?" >&2
-        exit 1
-fi
-shift
-
+# Package always has the same name.  Version information is stored in
+# the Info.plist file which is filled in by the configure script.
+pkg=Inkscape
 package=$pkg.app
+
+# TODO: Rewrite handling of command line args and make more robust.
 
 binary=$1
 if [ ! -x $binary ]; then
@@ -33,7 +44,14 @@ if [ ! -f $plist ]; then
 	echo "Need plist file" >&2
 	exit 1
 fi
+shift
 
+resdir=$1
+if [ ! -d $resdir ]; then
+	echo "Need the macosx packaging directory" >&2
+	exit 1
+fi
+shift
 
 # Fix a given executable or library to be relocatable
 fixlib () {
@@ -65,11 +83,11 @@ fi
 
 
 
-mkdir -p $package/Contents/{Frameworks,Resources}
-mkdir -p $package/Contents/MacOS/{bin,lib}
+mkdir -p $package/Contents/Resources/{bin,lib}
+mkdir -p $package/Contents/MacOS
 
 binname=`basename $binary`
-binpath=$package/Contents/MacOS/bin/$binname
+binpath=$package/Contents/Resources/bin/inkscape-bin
 
 cp $binary $binpath
 
@@ -79,10 +97,10 @@ nfiles=0
 endl=true
 while $endl; do
   echo "Looking for dependencies. Round " $a
-  libs="`otool -L $package/Contents/MacOS/{bin,lib}/* 2>/dev/null | fgrep compatibility | cut -d\( -f1 | grep $SW | sort | uniq`"
-  cp -f $libs $package/Contents/MacOS/lib
+  libs="`otool -L $package/Contents/Resources/lib/* $binary 2>/dev/null | fgrep compatibility | cut -d\( -f1 | grep $SW | sort | uniq`"
+  cp -f $libs $package/Contents/Resources/lib
   let "a+=1"  
-  nnfiles=`ls $package/Contents/MacOS/lib | wc -l`
+  nnfiles=`ls $package/Contents/Resources/lib | wc -l`
   if [ $nnfiles = $nfiles ]; then
     endl=false
   else
@@ -90,19 +108,64 @@ while $endl; do
   fi
 done
 
+# NOTE: This works for all the dylibs but causes GTK to crash at startup.
+#       Instead we leave them with their original install_names and set
+#       DYLD_LIBRARY_PATH within the app bundle before running Inkscape.
+#
 # Fix package deps
-(cd $package/Contents/MacOS/bin
- for file in *; do
-    fixlib $file
- done
- cd ../lib
- for file in *; do
-    fixlib $file
- done)
+#(cd $package/Contents/MacOS/bin
+# for file in *; do
+#    fixlib $file
+# done
+# cd ../lib
+# for file in *; do
+#    fixlib $file
+# done)
+
+
+# Build and add the launcher.
+(
+  cd $resdir/ScriptExec
+  xcodebuild -buildstyle Deployment clean build
+)
+cp $resdir/ScriptExec/build/ScriptExec.app/Contents/MacOS/ScriptExec $package/Contents/MacOS/Inkscape
 
 # Pull down all the share files
 rsync -av `dirname $binary`/../share/$binname/* $package/Contents/Resources/
 cp $plist $package/Contents/Info.plist
+
+# PkgInfo must match bundle type and creator code from Info.plist
+echo "APPLInks" > $package/Contents/PkgInfo
+
+# Pull in extra requirements.
+pkgetc="$package/Contents/Resources/etc"
+mkdir -p $pkgetc/pango
+cp $SW/etc/pango/pangox.aliases $pkgetc/pango/
+# Need to adjust path and quote incase of spaces in path.
+sed -e "s,$SW,\"\${CWD},g" -e 's,\.so ,.so" ,g' $SW/etc/pango/pango.modules > $pkgetc/pango/pango.modules
+cat > $pkgetc/pango/pangorc <<END_PANGO
+[Pango]
+ModuleFiles = "\${HOME}/.inkscape-etc/pango.modules"
+[PangoX]
+AliasFiles = "\${HOME}/.inkscape-etc/pangox.aliases"
+END_PANGO
+
+mkdir -p $pkgetc/gtk-2.0
+sed -e "s,$SW,\${CWD},g" $SW/etc/gtk-2.0/gdk-pixbuf.loaders > $pkgetc/gtk-2.0/gdk-pixbuf.loaders
+sed -e "s,$SW,\${CWD},g" $SW/etc/gtk-2.0/gtk.immodules > $pkgetc/gtk-2.0/gtk.immodules
+
+pkglib="$package/Contents/Resources/lib"
+mkdir -p $pkglib/pango/1.4.0/modules
+cp $SW/lib/pango/1.4.0/modules/*.so $pkglib/pango/1.4.0/modules
+
+mkdir -p $pkglib/gtk-2.0/2.4.0/{engines,immodules,loaders}
+cp -r $SW/lib/gtk-2.0/2.4.0/engines/* $pkglib/gtk-2.0/2.4.0/engines/
+cp $SW/lib/gtk-2.0/2.4.0/immodules/*.so $pkglib/gtk-2.0/2.4.0/immodules/
+cp $SW/lib/gtk-2.0/2.4.0/loaders/*.so $pkglib/gtk-2.0/2.4.0/loaders/
+
+# Get all the icons and the rest of the script framework
+rsync -av $resdir/Resources/* $package/Contents/Resources/
+
 
 # Make an image
 /usr/bin/hdiutil create -srcfolder $pkg.app $pkg.dmg
