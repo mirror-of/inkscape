26c82ea94a277f8e87c5a97b8fd3c24052b59f4a
Ralf Stephan <ralf@ark.in-berlin.de>
2005-07-25 14:06:04 +0000

    removed legacyMessageContext() member doxygen documentation improvements
    (bzr r7356)

--- src/ui/view/view.h
+++ src/ui/view/view.h
@@ -1,7 +1,7 @@
 #ifndef __SP_VIEW_H__
 #define __SP_VIEW_H__
 
-/*
+/** \file
  * Abstract base class for all SVG document views
  *
  * Authors:
@@ -40,11 +40,17 @@ void     sp_view_set_position (SPView *view, gdouble x, gdouble y);
 gboolean sp_view_shutdown (SPView *view);
 void     sp_view_request_redraw (SPView *view);
 
+/**
+ * Calls sp_view_set_position() with the point's coordinates.
+ */
 inline void sp_view_set_position(SPView *view, NR::Point const &p)
 {
     sp_view_set_position(view, p[NR::X], p[NR::Y]);
 }
 
+/**
+ * Abstract base class for all SVG document views
+ */
 class SPView : public GObject {
  public:
     GObject object;  // TODO:  Remove this
@@ -54,17 +60,14 @@ class SPView : public GObject {
     static void init(SPView *view);
     static void dispose(GObject *obj);
 
+    /// Returns a pointer to the view's message stack.
     Inkscape::MessageStack *messageStack() {
 	return _message_stack;
     }
 
+    /// Returns a pointer to the view's tipsMessageContext.
     Inkscape::MessageContext *tipsMessageContext() {
 	return _tips_message_context;
-    }
-
-    /* don't use this in new code */
-    Inkscape::MessageContext *legacyMessageContext() {
-	return _legacy_message_context;
     }
 
     // Wrappers for C versions of routines
@@ -110,7 +113,6 @@ private:
 
     Inkscape::MessageStack *_message_stack;
     Inkscape::MessageContext *_tips_message_context;
-    Inkscape::MessageContext *_legacy_message_context;
 
     sigc::connection _message_changed_connection;
  public: // for now...
@@ -118,25 +120,28 @@ private:
     sigc::connection _document_resized_connection;
 };
 
+/**
+ * The Glib-style vtable for the SPView class.
+ */
 struct SPViewClass {
 	GObjectClass parent_class;
 
-	/* Request shutdown */
+	/// Request shutdown
 	gboolean (* shutdown) (SPView *view);
-	/* Request redraw of visible area */
+	/// Request redraw of visible area
 	void (* request_redraw) (SPView *view);
-	/* Virtual method to set/change/remove document link */
+	/// Virtual method to set/change/remove document link
 	void (* set_document) (SPView *view, SPDocument *doc);
-	/* Virtual method about document size change */
+	/// Virtual method about document size change
 	void (* document_resized) (SPView *view, SPDocument *doc, gdouble width, gdouble height);
 
-	/* Signal of view uri change */
+	/// Signal of view uri change
 	void (* uri_set) (SPView *view, const guchar *uri);
-	/* Signal of view size change */
+	/// Signal of view size change
 	void (* resized) (SPView *view, gdouble width, gdouble height);
-	/* Cursor position */
+	/// Cursor position
 	void (* position_set) (SPView *view, gdouble x, gdouble y);
-	/* Status */
+	/// Status
 	void (* set_status_message) (SPView *view, Inkscape::MessageType type, gchar const *message);
 };
 
@@ -154,12 +159,15 @@ GType sp_view_widget_get_type (void);
 
 void sp_view_widget_set_view (SPViewWidget *vw, SPView *view);
 
-/* Allows presenting 'save changes' dialog, FALSE - continue, TRUE - cancel */
+/// Allows presenting 'save changes' dialog, FALSE - continue, TRUE - cancel
 gboolean sp_view_widget_shutdown (SPViewWidget *vw);
 
-/* Create a new SPViewWidget (which happens to be a SPDesktopWidget). */
+/// Create a new SPViewWidget (which happens to be a SPDesktopWidget). 
 SPViewWidget *sp_desktop_widget_new (SPNamedView *namedview);
 
+/**
+ * An SPViewWidget contains a GtkEventBox and a pointer to an SPView.
+ */
 struct SPViewWidget {
 	GtkEventBox eventbox;
 
@@ -180,12 +188,15 @@ struct SPViewWidget {
 
 };
 
+/**
+ * The Glib-style vtable for the SPViewWidget class.
+ */
 struct SPViewWidgetClass {
     GtkEventBoxClass parent_class;
 
-    /* Vrtual method to set/change/remove view */
+    /// Vrtual method to set/change/remove view
     void (* set_view) (SPViewWidget *vw, SPView *view);
-    /* Virtual method about view size change */
+    /// Virtual method about view size change
     void (* view_resized) (SPViewWidget *vw, SPView *view, gdouble width, gdouble height);
 
     gboolean (* shutdown) (SPViewWidget *vw);
