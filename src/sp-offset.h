f49fc0818c290d7e4723cb4f5ea3ea2d2b406ca3
Ralf Stephan <ralf@ark.in-berlin.de>
2005-07-25 10:24:24 +0000

    doxygen documentation improvements added editor footer
    (bzr r7344)

--- src/sp-offset.h
+++ src/sp-offset.h
@@ -1,13 +1,13 @@
 #ifndef __SP_OFFSET_H__
 #define __SP_OFFSET_H__
 
-/*
- * <sodipodi:offset> implementation
+/** \file
+ * SPOffset class.
  *
- * Authors (of the sp-spiral.h upon which this file was created):
+ * Authors:  
  *   Mitsuru Oka <oka326@parkcity.ne.jp>
  *   Lauris Kaplinski <lauris@kaplinski.com>
- *
+ * (of the sp-spiral.h upon which this file was created)
  * Copyright (C) 1999-2002 Lauris Kaplinski
  * Copyright (C) 2000-2001 Ximian, Inc.
  *
@@ -28,17 +28,40 @@ class SPOffset;
 class SPOffsetClass;
 class SPUseReference;
 
+/**
+ * SPOffset class.
+ *
+ * An offset is defined by curve and radius. The original curve is kept as 
+ * a path in a sodipodi:original attribute. It's not possible to change 
+ * the original curve.
+ *
+ * SPOffset is a derivative of SPShape, much like the SPSpiral or SPRect.
+ * The goal is to have a source shape (= originalPath), an offset (= radius) 
+ * and compute the offset of the source by the radius. To get it to work, 
+ * one needs to know what the source is and what the radius is, and how it's 
+ * stored in the xml representation. The object itself is a "path" element, 
+ * to get lots of shape functionality for free. The source is the easy part: 
+ * it's stored in a "inkscape:original" attribute in the path. In case of 
+ * "linked" offset, as they've been dubbed, there is an additional
+ * "inkscape:href" that contains the id of an element of the svg. 
+ * When built, the object will attach a listener vector to that object and 
+ * rebuild the "inkscape:original" whenever the href'd object changes. This 
+ * is of course grossly inefficient, and also does not react to changes 
+ * to the href'd during context stuff (like changing the shape of a star by 
+ * dragging control points) unless the path of that object is changed during 
+ * the context (seems to be the case for SPEllipse). The computation of the 
+ * offset is done in sp_offset_set_shape(), a function that is called whenever 
+ * a change occurs to the offset (change of source or change of radius).
+ * just like the sp-star and other, this path derivative can make control 
+ * points, or more precisely one control point, that's enough to define the 
+ * radius (look in object-edit).
+ */
 struct SPOffset : public SPShape {
-  /*
-   * offset is defined by curve and radius
-   * the original curve is kept as a path in a sodipodi:original attribute
-   * it's not possible to change the original curve
-   */
-  void *originalPath; // will be a livarot Path, just don't declare it here to please the gcc linker
-  char *original;     // SVG description of the source path
-  float rad;			/* offset radius */
+  void *originalPath; ///< will be a livarot Path, just don't declare it here to please the gcc linker
+  char *original;     ///< SVG description of the source path
+  float rad;          ///< offset radius
 
-	// for interactive setting of the radius
+  /// for interactive setting of the radius
   bool knotSet;
   NR::Point knot;
 	
@@ -47,7 +70,7 @@ struct SPOffset : public SPShape {
 
 	gchar					 *sourceHref;
 	SPUseReference *sourceRef;
-  Inkscape::XML::Node         *sourceRepr; // the repr associated with that id
+  Inkscape::XML::Node         *sourceRepr; ///< the repr associated with that id
 	SPObject			 *sourceObject;
 	
 	gulong           _modified_connection;
@@ -56,6 +79,7 @@ struct SPOffset : public SPShape {
 	sigc::connection _transformed_connection;
 };
 
+/// The SPOffset vtable.
 struct SPOffsetClass
 {
   SPShapeClass parent_class;
@@ -71,3 +95,14 @@ void sp_offset_top_point (SPOffset * offset, NR::Point *px);
 SPItem *sp_offset_get_source (SPOffset *offset);
 
 #endif
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
