cedeea9bfbd2cdb1084e2af99734d729182fae7c
Bob Jamison <ishmalius@gmail.com>
2005-07-29 18:19:54 +0000

    remove old hack for gtk_window_set_default_icon_from_file
    (bzr r7403)

--- src/main.cpp
+++ src/main.cpp
@@ -93,9 +93,6 @@
 #ifndef HAVE_BIND_TEXTDOMAIN_CODESET
 #define bind_textdomain_codeset(p,c)
 #endif
-#ifndef HAVE_GTK_WINDOW_SET_DEFAULT_ICON_FROM_FILE
-#define gtk_window_set_default_icon_from_file(f,v)
-#endif
 
 #include "application/application.h"
 
