ed4bc4f2b7cd033759969dc4179df944db76afd6
mjwybrow <mjwybrow@users.sourceforge.net>
2005-10-13 00:31:56 +0000

    * src/xml/simple-node.cpp: undo accidental commit of some debugging code.
    (bzr r7972)

--- src/xml/simple-node.cpp
+++ src/xml/simple-node.cpp
@@ -15,8 +15,6 @@
  *
  */
 
- bool set_shape_active = false;
-
 #include <glib/gquark.h>
 #include <glib/gmessages.h>
 #include <glib/gstrfuncs.h>
@@ -389,7 +387,6 @@ void SimpleNode::setContent(gchar const *content) {
     }
 }
 
-
 void
 SimpleNode::setAttribute(gchar const *name, gchar const *value, bool const is_interactive)
 {
@@ -436,13 +433,11 @@ SimpleNode::setAttribute(gchar const *name, gchar const *value, bool const is_in
     }
 
     if ( new_value != old_value ) {
-        if (!set_shape_active) {
         if (_logger) {
             _logger->notifyAttributeChanged(*this, key, old_value, new_value);
         }
 
         _observers.notifyAttributeChanged(*this, key, old_value, new_value);
-        }
     }
 }
 
