901328342bafb3285bf2599c0338daea3e32df0a
Ralf Stephan <ralf@ark.in-berlin.de>
2005-11-09 16:30:11 +0000

    reapplying overwritten changes
    (bzr r8147)

--- src/application/editor.h
+++ src/application/editor.h
@@ -124,15 +124,16 @@ private:
 } // namespace NSApplication
 } // namespace Inkscape
 
-#endif // INKSCAPE_APPLICATION_EDITOR_H
+
+#endif /* !INKSCAPE_APPLICATION_EDITOR_H */
 
 /*
   Local Variables:
   mode:c++
   c-file-style:"stroustrup"
-  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
   indent-tabs-mode:nil
   fill-column:99
   End:
 */
-// vim: filetype=c++:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
