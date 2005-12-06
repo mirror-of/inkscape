fa500980b21a4fa551d89ddbd51d6a35206f5e79
Peter Moulder <peter.moulder@monash.edu>
2005-12-06 11:19:59 +0000

    Add forward declaration of class Rectangle.
    (bzr r8409)

--- src/removeoverlap/remove_rectangle_overlap.h
+++ src/removeoverlap/remove_rectangle_overlap.h
@@ -12,6 +12,9 @@
  *
  * Released under GNU GPL.  Read the file 'COPYING' for more information.
  */
+
+class Rectangle;
+
 void removeRectangleOverlap(Rectangle *rs[], int n, double xBorder, double yBorder);
 
 
