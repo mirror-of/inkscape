26d579456943e8227f91b0bd237ffe6af247bcbf
Peter Moulder <peter.moulder@monash.edu>
2005-12-08 00:00:06 +0000

    Ensure that each rectangle has width&height of at least 1.
    (bzr r8437)

--- src/removeoverlap/removeoverlap.cpp
+++ src/removeoverlap/removeoverlap.cpp
@@ -25,7 +25,7 @@
 * such that rectangular bounding boxes are separated by at least xGap
 * horizontally and yGap vertically
 */
-void removeoverlap(GSList const *items, double xGap, double yGap) {
+void removeoverlap(GSList const *const items, double const xGap, double const yGap) {
 	if(!items) {
 		return;
 	}
@@ -46,25 +46,26 @@ void removeoverlap(GSList const *items, double xGap, double yGap) {
 		it != selected.end();
 		++it)
 	{
-		NR::Rect item_box = sp_item_bbox_desktop (*it);
-		NR::Point min = item_box.min();
-		NR::Point max = item_box.max();
-		rs[i++]=new Rectangle(min[NR::X],max[NR::X],min[NR::Y],max[NR::Y]);
+		using NR::X; using NR::Y;
+		NR::Rect const item_box(sp_item_bbox_desktop(*it));
+		NR::Point const min(item_box.min());
+		NR::Point const max(item_box.max());
+		/* The current algorithm requires widths & heights to be strictly positive. */
+		rs[i++] = new Rectangle(min[X], std::max(min[X] + 1, max[X] + xGap),
+					min[Y], std::max(min[Y] + 1, max[Y] + yGap));		
 	}
-	removeRectangleOverlap(rs,n,xGap,yGap);
+	removeRectangleOverlap(rs, n, 0.0, 0.0);
 	i=0;
 	for (std::list<SPItem *>::iterator it(selected.begin());
 		it != selected.end();
 		++it)
 	{
-		NR::Rect item_box = sp_item_bbox_desktop (*it);
-		Rectangle *r=rs[i++];
-
-		NR::Point min = item_box.min();
-		double x = r->getMinX() - min[NR::X];
-		double y = r->getMinY() - min[NR::Y];
-		NR::Point t = NR::Point (x, y);
-		sp_item_move_rel(*it, NR::translate(t));
+		NR::Rect const item_box(sp_item_bbox_desktop(*it));
+		Rectangle *r = rs[i++];
+		NR::Point const curr(item_box.min());
+		NR::Point const dest(r->getMinX(),
+				     r->getMinY());
+		sp_item_move_rel(*it, NR::translate(dest - curr));
 		delete r;
 	}
 	delete [] rs;
