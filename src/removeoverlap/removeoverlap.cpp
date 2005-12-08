5842a46475a9550169d0bc461d4039f43dfe5ebe
Peter Moulder <peter.moulder@monash.edu>
2005-12-08 22:31:33 +0000

    Add equal gap to each side instead of all at end, given that the added gap can vary between rectangles.
    (bzr r8457)

--- src/removeoverlap/removeoverlap.cpp
+++ src/removeoverlap/removeoverlap.cpp
@@ -1,6 +1,7 @@
-/**
-* \brief Remove overlaps function
-*
+/** \file
+ * Interface between Inkscape code (SPItem) and remove-overlaps function.
+ */
+/*
 * Authors:
 *   Tim Dwyer <tgdwyer@gmail.com>
 *
@@ -9,16 +10,15 @@
 * Released under GNU GPL.  Read the file 'COPYING' for more information.
 */
 #include <glib/gslist.h>
-#include <stdio.h>
-#include "../inkscape.h"
-#include "../util/glib-list-iterators.h"
-#include "../libnr/nr-rect.h"
-#include "../libnr/nr-point.h"
 #include <list>
-#include "../sp-item.h"
-#include "../sp-item-transform.h"
-#include "generate-constraints.h"
-#include "remove_rectangle_overlap.h"
+#include "inkscape.h"
+#include "util/glib-list-iterators.h"
+#include "libnr/nr-rect.h"
+#include "libnr/nr-point.h"
+#include "sp-item.h"
+#include "sp-item-transform.h"
+#include "removeoverlap/generate-constraints.h"
+#include "removeoverlap/remove_rectangle_overlap.h"
 
 /**
 * Takes a list of inkscape items and moves them as little as possible
@@ -42,17 +42,30 @@ void removeoverlap(GSList const *const items, double const xGap, double const yG
 	Rectangle **rs = new Rectangle*[n];
 	int i=0;
 
+	NR::Point const gap(xGap, yGap);
 	for (std::list<SPItem *>::iterator it(selected.begin());
 		it != selected.end();
 		++it)
 	{
 		using NR::X; using NR::Y;
 		NR::Rect const item_box(sp_item_bbox_desktop(*it));
-		NR::Point const min(item_box.min());
-		NR::Point const max(item_box.max());
+		
 		/* The current algorithm requires widths & heights to be strictly positive. */
-		rs[i++] = new Rectangle(min[X], std::max(min[X] + 1, max[X] + xGap),
-					min[Y], std::max(min[Y] + 1, max[Y] + yGap));		
+		NR::Point min(item_box.min());
+		NR::Point max(item_box.max());
+		for (unsigned d = 0; d < 2; ++d) {
+			double const minsize = 1; // arbitrary positive number
+			if (max[d] - min[d] + gap[d] < minsize) {
+				double const mid = .5 * (min[d] + max[d]);
+				min[d] = mid - .5*minsize;
+				max[d] = mid + .5*minsize;
+			} else {
+				min[d] -= .5*gap[d];
+				max[d] += .5*gap[d];
+			}
+		}
+		rs[i++] = new Rectangle(min[X], max[X],
+					min[Y], max[Y]);
 	}
 	removeRectangleOverlap(rs, n, 0.0, 0.0);
 	i=0;
@@ -62,9 +75,9 @@ void removeoverlap(GSList const *const items, double const xGap, double const yG
 	{
 		NR::Rect const item_box(sp_item_bbox_desktop(*it));
 		Rectangle *r = rs[i++];
-		NR::Point const curr(item_box.min());
-		NR::Point const dest(r->getMinX(),
-				     r->getMinY());
+		NR::Point const curr(item_box.midpoint());
+		NR::Point const dest(r->getCentreX(),
+				     r->getCentreY());
 		sp_item_move_rel(*it, NR::translate(dest - curr));
 		delete r;
 	}
