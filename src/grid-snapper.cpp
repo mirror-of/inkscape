243f67e0afdb364bb3f6ebe5b620fbb6df02df1c
Carl Hetherington <cth@carlh.net>
2005-12-16 15:15:34 +0000

    Snap only to visible grid lines (patch from mtou)
    (bzr r8549)

--- src/grid-snapper.cpp
+++ src/grid-snapper.cpp
@@ -16,6 +16,7 @@
 #include "libnr/nr-point-fns.h"
 #include "sp-namedview.h"
 #include "grid-snapper.h"
+#include "display/canvas-grid.h"
 
 /**
  * \return x rounded to the nearest multiple of c1 plus c0.
@@ -40,10 +41,26 @@ Inkscape::GridSnapper::GridSnapper(SPNamedView const *nv, NR::Coord const d) : L
 Inkscape::LineSnapper::LineList Inkscape::GridSnapper::_getSnapLines(NR::Point const &p) const
 {
     LineList s;
+
+    SPCGrid *griditem = NULL; 
+    for (GSList *l = _named_view->gridviews; l != NULL; l = l->next) {
+        // FIXME : this is a hack since there is only one view for now
+        //                 but when we'll handle multiple views, snapping should
+        //                 must be rethought and maybe only the current view
+        //                 should give back it's SHOWN lines to snap to
+        //                 For now, the last SPCGrid in _named_view->gridviews will be used.
+        griditem = SP_CGRID(l->data);
+    }
+
+    g_assert(griditem != NULL);
     
     for (unsigned int i = 0; i < 2; ++i) {
+
+        /* This is to make sure we snap to only visible grid lines */
+        double const scale = griditem->scaled[i] ? griditem->empspacing : 1;
+
         NR::Coord const rounded = round_to_nearest_multiple_plus(p[i],
-                                                                 _named_view->gridspacing[i],
+                                                                 _named_view->gridspacing[i] * scale,
                                                                  _named_view->gridorigin[i]);
         
         s.push_back(std::make_pair(NR::Dim2(i), rounded));
