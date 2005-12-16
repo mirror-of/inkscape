1130357eda01892ab1900578ddf222875b65d2e5
bulia byak <buliabyak@gmail.com>
2005-12-16 18:09:46 +0000

    moved scale transform solver here, rewrote it to solve in both dimensions simultaneously
    (bzr r8553)

--- src/sp-item-transform.h
+++ src/sp-item-transform.h
@@ -5,6 +5,7 @@
 namespace NR {
 class translate;
 class rotate;
+class Rect;
 }
 
 void sp_item_rotate_rel(SPItem *item, NR::rotate const &rotation);
@@ -12,6 +13,7 @@ void sp_item_scale_rel (SPItem *item, NR::scale const &scale);
 void sp_item_skew_rel (SPItem *item, double skewX, double skewY);
 void sp_item_move_rel(SPItem *item, NR::translate const &tr);
 
+NR::Matrix get_scale_transform_with_stroke (NR::Rect &bbox, gdouble strokewidth, bool transform_stroke, gdouble x0, gdouble y0, gdouble x1, gdouble y1);
 
 #endif /* !SP_ITEM_TRANSFORM_H */
 
