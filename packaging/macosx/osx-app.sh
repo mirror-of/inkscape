b312c05a7e2d623b6dbac33d1b4d37947de0a1e0
mjwybrow <mjwybrow@users.sourceforge.net>
2007-03-16 01:46:28 +0000

    * packaging/osx-app.sh, packaging/macosx/osx-app.sh,     packaging/macosx/osx-build.sh, packaging/macosx/inkscape_python.ds_store,     packaging/macosx/dmg_background.svg, packaging/macosx/osx-dmg.sh,     packaging/macosx/dmg_background.png, packaging/macosx/dmg_set_style.scpt,     packaging/macosx/inkscape.ds_store, Makefile.am:
        Patch #1681805.  Mac OS X packaging updates, fixes and improvements from
        the main branch.
    
    (bzr r11026)

--- packaging/macosx/osx-app.sh
+++ packaging/macosx/osx-app.sh
@@ -1,32 +1,39 @@
 #! /bin/bash
-# Copyright 2005, Kees Cook <kees@outflux.net>
-# Licensed under GNU General Public License
 #
-# Usage: osx-app [-s] /path/to/bin/inkscape Info.plist /path/to/packaging/macosx
+# Usage: osx-app [-s] /path/to/bin/inkscape Info.plist
 #
-# This attempts to build an Inkscape.app package for OSX, resolving
-# Dynamic libraries, etc.  Strips the executable and libraries if
+# This script attempts to build an Inkscape.app package for OS X, resolving
+# dynamic libraries, etc.  It strips the executable and libraries if
 # '-s' is given.
+#
+#
+# Authors:
+#    Kees Cook <kees@outflux.net>
+#    Michael Wybrow <mjwybrow@users.sourceforge.net>
+#    Jean-Olivier Irisson <jo.irisson@gmail.com>
+# 
+# Copyright (C) 2005 Kees Cook
+# Copyright (C) 2005-2007 Michael Wybrow
+# Copyright (C) 2007 Jean-Olivier Irisson
+#
+#
+# Released under GNU GPL, read the file 'COPYING' for more information
 #
 # Thanks to GNUnet's "build_app" script for help with library dep resolution.
 # https://gnunet.org/svn/GNUnet/contrib/OSX/build_app
 # 
-# Fixes and modifications to use Gimp.app style launcher:
-#   Michael Wybrow <mjwybrow@users.sourceforge.net>
-#
 #
 # Notes:
 # The Info.plist file can be found in the base inkscape directory once
 # configure has been run.
-# The macosx directory is in the inkscape/packaging directory.
 #
 # When packaging Inkscape for OS X, configure should be run with the 
 # "--enable-osxapp" option which sets the correct paths for support
 # files inside the app bundle.
 # 
 # Thus, the usual use of this file would be to run it from the within the
-# inkscape/packaging directory, substituting in the inkscape binary path:
-# 	./osx-app.sh /path/to/bin/inkscape ../Info.plist macosx
+# inkscape/packaging/macosx directory, substituting in the inkscape binary path:
+# 	./osx-app.sh /path/to/bin/inkscape ../../Info.plist
 
 
 
@@ -40,7 +47,10 @@ if [ "$VERSION" -ge "4" ]; then
   # libXinerama.1.dylib is not installed as part of X11 on Panther but
   # is introduced as a dependency if Inkscape is compiled on Tiger or
   # later.  Thus, add the library to the bundle for Panther users
-  EXTRALIBS="/usr/X11R6/lib/libXinerama.1.dylib"
+  # Building on Tiger gives app bundles incompatible with Panther anyway
+  # so we do not need to add this now
+  # EXTRALIBS="/usr/X11R6/lib/libXinerama.1.dylib"
+  EXTRALIBS=""
 else
   # Panther (10.3) or earlier.
   XCODEFLAGS="-buildstyle Deployment"
@@ -48,42 +58,45 @@ else
   EXTRALIBS=""
 fi
 
+# set LIBPREFIX only if it has not been set before 
+# (by osx-build.sh for example)
+if [ -z $LIBPREFIX ]
+then
+	export LIBPREFIX="$HOME/ws-fat"
+fi
 
-SW=/sw
 
 # Package always has the same name.  Version information is stored in
 # the Info.plist file which is filled in by the configure script.
 pkg=Inkscape
 package="$pkg.app"
 
-# TODO: Rewrite handling of command line args and make more robust.
 
 strip=false
-if [ "$1" = "-s" ]; then
-	strip=true
-	shift
-fi
+while getopts 's' flag; do
+	case $flag in
+		s)  strip=true ;;
+		\?) echo "Invalid command line option."; exit 1 ;;
+		*)  echo "Internal error in getopts."; exit 2 ;;
+	esac
+done
+shift `expr $OPTIND - 1`;
+
 
 binary="$1"
 if [ ! -x "$binary" ]; then
         echo "Not executable: $binary" >&2
         exit 1
 fi
-shift
 
-plist="$1"
+plist="$2"
 if [ ! -f "$plist" ]; then
 	echo "Need plist file" >&2
 	exit 1
 fi
-shift
 
-resdir=$1
-if [ ! -d $resdir ]; then
-	echo "Need the macosx packaging directory" >&2
-	exit 1
-fi
-shift
+# Set the 'macosx' directory, usually the current directory.
+resdir=`pwd`
 
 # Fix a given executable or library to be relocatable
 fixlib () {
@@ -148,48 +161,54 @@ echo "APPLInks" > $package/Contents/PkgInfo
 # Pull in extra requirements.
 pkgetc="$package/Contents/Resources/etc"
 mkdir -p $pkgetc/pango
-cp $SW/etc/pango/pangox.aliases $pkgetc/pango/
+cp $LIBPREFIX/etc/pango/pangox.aliases $pkgetc/pango/
 # Need to adjust path and quote incase of spaces in path.
-sed -e "s,$SW,\"\${CWD},g" -e 's,\.so ,.so" ,g' $SW/etc/pango/pango.modules > $pkgetc/pango/pango.modules
+sed -e "s,$LIBPREFIX,\"\${CWD},g" -e 's,\.so ,.so" ,g' $LIBPREFIX/etc/pango/pango.modules > $pkgetc/pango/pango.modules
 cat > $pkgetc/pango/pangorc <<END_PANGO
 [Pango]
-ModuleFiles = "\${HOME}/.inkscape-etc/pango.modules"
+ModuleFiles=\${HOME}/.inkscape-etc/pango.modules
 [PangoX]
-AliasFiles = "\${HOME}/.inkscape-etc/pangox.aliases"
+AliasFiles=\${HOME}/.inkscape-etc/pangox.aliases
 END_PANGO
 
 # We use a modified fonts.conf file so only need the dtd
 mkdir -p $pkgetc/fonts
-cp /etc/fonts/fonts.dtd $pkgetc/fonts/
+cp $LIBPREFIX/etc/fonts/fonts.dtd $pkgetc/fonts/
+cp -r $LIBPREFIX/etc/fonts/conf.avail $pkgetc/fonts/
+cp -r $LIBPREFIX/etc/fonts/conf.d $pkgetc/fonts/
 
 mkdir -p $pkgetc/gtk-2.0
-sed -e "s,$SW,\${CWD},g" $SW/etc/gtk-2.0/gdk-pixbuf.loaders > $pkgetc/gtk-2.0/gdk-pixbuf.loaders
-sed -e "s,$SW,\${CWD},g" $SW/etc/gtk-2.0/gtk.immodules > $pkgetc/gtk-2.0/gtk.immodules
+sed -e "s,$LIBPREFIX,\${CWD},g" $LIBPREFIX/etc/gtk-2.0/gdk-pixbuf.loaders > $pkgetc/gtk-2.0/gdk-pixbuf.loaders
+sed -e "s,$LIBPREFIX,\${CWD},g" $LIBPREFIX/etc/gtk-2.0/gtk.immodules > $pkgetc/gtk-2.0/gtk.immodules
 
 for item in gnome-vfs-mime-magic gnome-vfs-2.0
 do
-  cp -r $SW/etc/$item $pkgetc/
+  cp -r $LIBPREFIX/etc/$item $pkgetc/
 done
 
-pkglib="$package/Contents/Resources/lib"
-mkdir -p $pkglib/pango/1.4.0/modules
-cp $SW/lib/pango/1.4.0/modules/*.so $pkglib/pango/1.4.0/modules/
 
-mkdir -p $pkglib/gtk-2.0/2.4.0/{engines,immodules,loaders}
-cp -r $SW/lib/gtk-2.0/2.4.0/engines/* $pkglib/gtk-2.0/2.4.0/engines/
-cp $SW/lib/gtk-2.0/2.4.0/immodules/*.so $pkglib/gtk-2.0/2.4.0/immodules/
-cp $SW/lib/gtk-2.0/2.4.0/loaders/*.so $pkglib/gtk-2.0/2.4.0/loaders/
+pkglib="$package/Contents/Resources/lib"
+pangover=`ls $LIBPREFIX/lib/pango/`
+mkdir -p $pkglib/pango/$pangover/modules
+cp $LIBPREFIX/lib/pango/$pangover/modules/*.so $pkglib/pango/$pangover/modules/
+
+gtkver=`ls $LIBPREFIX/lib/gtk-2.0/ | grep "2\."`
+mkdir -p $pkglib/gtk-2.0/$gtkver/{engines,immodules,loaders}
+cp -r $LIBPREFIX/lib/gtk-2.0/$gtkver/engines/* $pkglib/gtk-2.0/$gtkver/engines/
+cp $LIBPREFIX/lib/gtk-2.0/$gtkver/immodules/*.so $pkglib/gtk-2.0/$gtkver/immodules/
+cp $LIBPREFIX/lib/gtk-2.0/$gtkver/loaders/*.so $pkglib/gtk-2.0/$gtkver/loaders/
 
 mkdir -p $pkglib/gnome-vfs-2.0/modules
-cp $SW/lib/gnome-vfs-2.0/modules/*.so $pkglib/gnome-vfs-2.0/modules/
+cp $LIBPREFIX/lib/gnome-vfs-2.0/modules/*.so $pkglib/gnome-vfs-2.0/modules/
 
-# Find out libs we need from fink (e.g. $SW) - loop until no changes
+# Find out libs we need from fink, darwinports, or from a custom install
+# (e.g. $LIBPREFIX), then loop until no changes.
 a=1
 nfiles=0
 endl=true
 while $endl; do
   echo "Looking for dependencies. Round " $a
-  libs="`otool -L $pkglib/gtk-2.0/2.4.0/loaders/* $pkglib/gtk-2.0/2.4.0/immodules/* $pkglib/gtk-2.0/2.4.0/engines/*.so $pkglib/pango/1.4.0/modules/* $pkglib/gnome-vfs-2.0/modules/* $package/Contents/Resources/lib/* $binary 2>/dev/null | fgrep compatibility | cut -d\( -f1 | grep $SW | sort | uniq`"
+  libs="`otool -L $pkglib/gtk-2.0/$gtkver/loaders/* $pkglib/gtk-2.0/$gtkver/immodules/* $pkglib/gtk-2.0/$gtkver/engines/*.so $pkglib/pango/$pangover/modules/* $pkglib/gnome-vfs-2.0/modules/* $package/Contents/Resources/lib/* $binary 2>/dev/null | fgrep compatibility | cut -d\( -f1 | grep $LIBPREFIX | sort | uniq`"
   cp -f $libs $package/Contents/Resources/lib
   let "a+=1"  
   nnfiles=`ls $package/Contents/Resources/lib | wc -l`
@@ -228,7 +247,6 @@ fi
 # Get all the icons and the rest of the script framework
 rsync -av $resdir/Resources/* $package/Contents/Resources/
 
-
 # Make an image
 #/usr/bin/hdiutil create -srcfolder "$pkg.app" "$pkg.dmg"
-( cd macosx && ./osx-dmg.sh )
+# ./osx-dmg.sh
