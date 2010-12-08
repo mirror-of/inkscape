46b5976d69caaedb740e12ed19ab8d80f7d19597
Jesper <Jesper@Farnsworth>
2010-12-08 13:11:11 +0100

    updating swedish translation
    (bzr r9943)

--- mingwenv.bat
+++ mingwenv.bat
@@ -1,6 +1,6 @@
 @echo Setting environment variables for MinGw build of Inkscape
 IF "%DEVLIBS_PATH%"=="" set DEVLIBS_PATH=c:\devlibs
-IF "%MINGW_PATH%"=="" set MINGW_PATH=c:\mingw
+IF "%MINGW_PATH%"=="" set MINGW_PATH=C:\MinGW32
 set MINGW_BIN=%MINGW_PATH%\bin
 set PKG_CONFIG_PATH=%DEVLIBS_PATH%\lib\pkgconfig
 set PATH=%DEVLIBS_PATH%\bin;%DEVLIBS_PATH%\python;%MINGW_BIN%;%PATH%
