e62b2eaabe0888193b4a7e04816fcb55ea2d2691
Gellule Xg <gellule.xg@gmail.com>
2012-08-26 21:22:43 -1000

    Fixed PYTHONPATH to propery find lxml.
    (bzr r11624)

--- packaging/macosx/environment.sh
+++ packaging/macosx/environment.sh
@@ -3,7 +3,7 @@
 export VERSIONER_PYTHON_VERSION=2.7
 ARCH=`arch`
 PYTHON_VERS=`python -V 2>&1 | cut -c 8-10`
-export PYTHONPATH="$bundle_res/python/site-packages/$ARCH/$PYTHON_VERS"
+export PYTHONPATH="$bundle_res/python/site-package"
 export INKSCAPE_LOCALEDIR="$bundle_res/locale"
 export GNOME_VFS_MODULE_CONFIG_PATH="$bundle_etc/gnome-vfs-2.0/modules"
 export GNOME_VFS_MODULE_PATH="$bundle_lib/gnome-vfs-2.0/modules"
