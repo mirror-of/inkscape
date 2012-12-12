69a5cdb1e0684980aba450dcb87187b019d0a7dd
su_v <suv-sf@users.sourceforge.net>
2012-12-12 17:21:10 +0100

    Backporting r11903 (UI consistency: Fix undo history entry for layer renaming)
    (bzr r9937)

--- src/ui/dialog/layers.cpp
+++ src/ui/dialog/layers.cpp
@@ -522,7 +522,7 @@ void LayersPanel::_handleRowChange( Gtk::TreeModel::Path const& /*path*/, Gtk::T
                 _desktop->layer_manager->renameLayer( obj, tmp.c_str(), FALSE );
                 row[_model->_colLabel] = obj->label();
                 sp_document_done( _desktop->doc() , SP_VERB_NONE,
-                                                    _("Renamed layer"));
+                                                    _("Rename layer"));
             }
         }
     }
