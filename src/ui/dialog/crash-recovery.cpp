3982b2204c3e30151135b1a9f3b39870a95b1e2d
Nicolas Dufour <nicoduf@yahoo.fr>
2013-08-23 17:44:08 +0200

    Missing dialog files
    (bzr r12486)

---
+++ src/ui/dialog/crash-recovery.cpp
@@ -0,0 +1,39 @@
+/*
+ * Authors:
+ *
+ * Copyright (C) 2012 Authors
+ * Released under GNU GPL.  Read the file 'COPYING' for more information.
+ */
+
+#include "crash-recovery.h"
+#include "preferences.h"
+#include "verbs.h"
+
+namespace Inkscape {
+namespace UI {
+namespace Dialog {
+
+CrashRecovery &CrashRecovery::getInstance()
+{
+    return *new CrashRecovery();
+}
+
+CrashRecovery::CrashRecovery(gchar const *prefsPath) :
+    Inkscape::UI::Widget::Panel("", prefsPath, SP_VERB_DIALOG_CRASH_RECOVERY, "", false)
+{
+}
+
+CrashRecovery::~CrashRecovery()
+{
+}
+
+} // namespace Dialog
+} // namespace UI
+} // namespace Inkscape
+
+
+
+
+
+
+
---
+++ src/ui/dialog/crash-recovery.h
@@ -0,0 +1,46 @@
+/** @file
+ * @brief Crash Recovery dialog
+ */
+/* Authors:
+ *   
+ *
+ * Copyright (C) 2013 Authors
+ *
+ * Released under GNU GPL.  Read the file 'COPYING' for more information.
+ */
+
+#ifndef SEEN_INKSCAPE_UI_DIALOG_CRASH_RECOVERY_H
+#define SEEN_INKSCAPE_UI_DIALOG_CRASH_RECOVERY_H
+
+#include <glibmm/ustring.h>
+#include "ui/widget/panel.h"
+
+class SPDocument;
+
+namespace Inkscape {
+namespace UI {
+namespace Dialog {
+
+class CrashRecovery : public UI::Widget::Panel {
+public:
+    CrashRecovery(gchar const *prefsPath = "/dialogs/crashrecovery");
+    virtual ~CrashRecovery();
+
+    /**
+     * Helper function which returns a new instance of the dialog.
+     * getInstance is needed by the dialog manager (Inkscape::UI::Dialog::DialogManager).
+     */
+    static CrashRecovery &getInstance();
+
+protected:
+
+private:
+    CrashRecovery(CrashRecovery const &); // no copy
+    CrashRecovery &operator=(CrashRecovery const &); // no assign
+};
+
+} // namespace Dialog
+} // namespace UI
+} // namespace Inkscape
+
+#endif // SEEN_INKSCAPE_UI_DIALOG_CRASH_RECOVERY_H
