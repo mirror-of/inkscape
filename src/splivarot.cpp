28d5512881c0f474db45dab394108e79764b9937
Krzysztof Kosi??ski <tweenk.pl@gmail.com>
2014-06-09 15:43:30 +0200

    Fix crash when undoing in specific circumstances. Backport of fix for 1290573 applied in r13323 of trunk.
    Fixed bugs:
      - https://launchpad.net/bugs/1290573
    
    (bzr r10036)

--- src/splivarot.cpp
+++ src/splivarot.cpp
@@ -1188,13 +1188,22 @@ sp_selected_path_outline(SPDesktop *desktop)
                                                              g_repr, xml_doc, doc );
                     }
                 }
+                //bug 1290573: completely destroy the old object first to prevent
+                //an ID clash, which has issues on undo
+                curve->unref();
+                selection->remove(item);
+                item->deleteObject(false);
 
                 selection->add(g_repr);
 
                 Inkscape::GC::release(g_repr);
 
-
             } else {
+                //bug 1290573: completely destroy the old object first to prevent
+                //an ID clash, which has issues on undo
+                curve->unref();
+                selection->remove(item);
+                item->deleteObject(false);
 
                 // add the new repr to the parent
                 parent->appendChild(repr);
@@ -1219,10 +1228,6 @@ sp_selected_path_outline(SPDesktop *desktop)
             }
 
             Inkscape::GC::release(repr);
-
-            curve->unref();
-            selection->remove(item);
-            SP_OBJECT(item)->deleteObject(false);
 
         }
         if (title) g_free(title);
