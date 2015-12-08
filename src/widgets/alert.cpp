d59a44df824073b34646efb1cfe8e38976a9efba
Martin Owens <doctormo@gmail.com>
2015-12-08 10:33:15 -0500

    Alert init
    (bzr r14518)

---
+++ src/widgets/alert.cpp
@@ -0,0 +1,26 @@
+/*
+ *  Show an update message to the user if needed.
+ *
+ * Authors:
+ *   Martin Owens <doctormo@gmail.com>
+ *
+ * Copyright (C) 2015 authors
+ *
+ * Released under GNU GPL, read the file 'COPYING' for more information
+ */
+
+#include <cstring>
+#include <cstdio>
+
+#include "alert.h"
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
---
+++ src/widgets/alert.h
@@ -0,0 +1,15 @@
+/*
+ * Alert box that pops up tp tell the user about events.
+ *
+ * Authors:
+ *   Martin Owens <doctormo@gmail.com>
+ *
+ * Copyright (C) 2015 authors
+ *
+ * Released under GNU GPL, read the file 'COPYING' for more information
+ */
+
+#include <gtk/gtk.h>
+#include <iostream>
+#include <glib.h>
+
