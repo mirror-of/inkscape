3f9d951351092dd579038ceac7c701b1e7f37145
Ted Gould <ted@gould.cx>
2016-08-29 10:42:55 -0500

    Fixing home directory
    (bzr r15052)

--- packaging/snappy/snapcraft.sh
+++ packaging/snappy/snapcraft.sh
@@ -3,4 +3,8 @@
 export INKSCAPE_PORTABLE_PROFILE_DIR="${SNAP_USER_DATA}"
 export INKSCAPE_LOCALEDIR="${SNAP}/share/locale/"
 
+if [ -d "/home/${USER}" ]; then
+	export HOME="/home/${USER}"
+fi
+
 exec $@
