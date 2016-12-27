192e2be896b7ca40448acc6a6184285ca8cb6b3b
Jabiertxof <jtx@jtx>
2016-12-27 18:15:54 +0100

    Backport from trunk. Fixes bug: 1652465 Bug tritle: Mirror LPE problem when modifying curve points location No string freeze broke Fix confirmed on trunk at r.15366 * #9 Bug problem: When edit a path with this LPE's applied, return unwanted reations/crashes because attemp to write point parameters not changed in the LPE. Bug fix: To me seems strange, this not be to happend because the parameter write is in the "safe" function "doBeforeEffect", anyway I check before write if this point parameters are changed to write.
    Fixed bugs:
      - https://launchpad.net/bugs/1652465
    
    (bzr r15284)

--- src/live_effects/lpe-mirror_symmetry.cpp
+++ src/live_effects/lpe-mirror_symmetry.cpp
@@ -54,8 +54,8 @@ LPEMirrorSymmetry::LPEMirrorSymmetry(LivePathEffectObject *lpeobject) :
     registerParameter( &start_point);
     registerParameter( &end_point);
     registerParameter( &center_point);
-    apply_to_clippath_and_mask = true;
     previous_center = Geom::Point(0,0);
+    apply_to_clippath_and_mask = true;
 }
 
 LPEMirrorSymmetry::~LPEMirrorSymmetry()
@@ -113,16 +113,35 @@ LPEMirrorSymmetry::doBeforeEffect (SPLPEItem const* lpeitem)
         point_a = Geom::Point(center_point[X],boundingbox_Y.min());
         point_b = Geom::Point(center_point[X],boundingbox_Y.max());
     }
-    if ((Geom::Point)start_point == (Geom::Point)end_point && (Geom::Point)start_point == Geom::Point(0,0)) {
+    if ((Geom::Point)start_point == (Geom::Point)end_point) {
         start_point.param_setValue(point_a, true);
         end_point.param_setValue(point_b, true);
         previous_center = Geom::middle_point((Geom::Point)start_point, (Geom::Point)end_point);
         center_point.param_setValue(previous_center, true);
+        return;
     }
     if ( mode == MT_X || mode == MT_Y ) {
-        start_point.param_setValue(point_a, true);
-        end_point.param_setValue(point_b, true);
-        center_point.param_setValue(Geom::middle_point(point_a, point_b), true);
+        if (!are_near(previous_center, (Geom::Point)center_point, 0.01)) {
+            center_point.param_setValue(Geom::middle_point(point_a, point_b), true);
+            end_point.param_setValue(point_b, true);
+            start_point.param_setValue(point_a, true);
+        } else {
+            if ( mode == MT_X ) {
+                if (!are_near(start_point[X], point_a[X], 0.01)) {
+                    start_point.param_setValue(point_a, true);
+                }
+                if (!are_near(end_point[X], point_b[X], 0.01)) {
+                    end_point.param_setValue(point_b, true);
+                }
+            } else {  //MT_Y
+                if (!are_near(start_point[Y], point_a[Y], 0.01)) {
+                    start_point.param_setValue(point_a, true);
+                }
+                if (!are_near(end_point[Y], point_b[Y], 0.01)) {
+                    end_point.param_setValue(point_b, true);
+                }
+            }
+        }
     } else if ( mode == MT_FREE) {
         if (are_near(previous_center, (Geom::Point)center_point, 0.01)) {
             center_point.param_setValue(Geom::middle_point((Geom::Point)start_point, (Geom::Point)end_point), true);
@@ -139,7 +158,6 @@ LPEMirrorSymmetry::doBeforeEffect (SPLPEItem const* lpeitem)
         Geom::Point ep = Geom::Point(document->getWidth().value("px")/2.0, document->getHeight().value("px")) * transform;
         end_point.param_setValue(ep, true);
         center_point.param_setValue(Geom::middle_point((Geom::Point)start_point, (Geom::Point)end_point), true);
-        previous_center = center_point;
     } else { //horizontal page
         SPDocument * document = SP_ACTIVE_DOCUMENT;
         Geom::Affine transform = i2anc_affine(SP_OBJECT(lpeitem), NULL).inverse();
@@ -147,10 +165,6 @@ LPEMirrorSymmetry::doBeforeEffect (SPLPEItem const* lpeitem)
         start_point.param_setValue(sp, true);
         Geom::Point ep = Geom::Point(document->getWidth().value("px"), document->getHeight().value("px")/2.0) * transform;
         end_point.param_setValue(ep, true);
-        center_point.param_setValue(Geom::middle_point((Geom::Point)start_point, (Geom::Point)end_point), true);
-        previous_center = center_point;
-    }
-    if (!are_near(previous_center, (Geom::Point)center_point, 0.01)) {
         center_point.param_setValue(Geom::middle_point((Geom::Point)start_point, (Geom::Point)end_point), true);
     }
     previous_center = center_point;
