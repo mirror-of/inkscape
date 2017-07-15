nullmastermind/inkscape-for-trace

cmake/MSYS2: Try to make the installation a bit more lenient towards version numbers in library names. This should be more future-proof against package updates but also allows to build with slightly outdated libraries.

CMakeScripts/InstallMSYS2.cmake
@@ -15,116 +15,114 @@ if(WIN32)
 
   # mingw-w64 dlls
   #   (use msys2checkdeps.py to list required libraries / check for missing libraries)
-  install(FILES
+  file(GLOB MINGW_LIBS
     ${MINGW_BIN}/LIBEAY32.dll
     ${MINGW_BIN}/SSLEAY32.dll
-    ${MINGW_BIN}/libMagick++-6.Q16HDRI-6.dll
-    ${MINGW_BIN}/libMagickCore-6.Q16HDRI-2.dll
-    ${MINGW_BIN}/libMagickWand-6.Q16HDRI-2.dll
-    ${MINGW_BIN}/libaspell-15.dll
-    ${MINGW_BIN}/libatk-1.0-0.dll
-    ${MINGW_BIN}/libatkmm-1.6-1.dll
-    ${MINGW_BIN}/libbz2-1.dll
-    ${MINGW_BIN}/libcairo-2.dll
-    ${MINGW_BIN}/libcairo-gobject-2.dll
-    ${MINGW_BIN}/libcairomm-1.0-1.dll
-    ${MINGW_BIN}/libcdr-0.1.dll
-    ${MINGW_BIN}/libcroco-0.6-3.dll
-    ${MINGW_BIN}/libcurl-4.dll
+    ${MINGW_BIN}/libMagick*.dll
+    ${MINGW_BIN}/libaspell-[0-9]*.dll
+    ${MINGW_BIN}/libatk-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libatkmm-1.6-[0-9]*.dll
+    ${MINGW_BIN}/libbz2-[0-9]*.dll
+    ${MINGW_BIN}/libcairo-[0-9]*.dll
+    ${MINGW_BIN}/libcairo-gobject-[0-9]*.dll
+    ${MINGW_BIN}/libcairomm-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libcdr-0.[0-9]*.dll
+    ${MINGW_BIN}/libcroco-0.6-[0-9]*.dll
+    ${MINGW_BIN}/libcurl-[0-9]*.dll
     ${MINGW_BIN}/libenchant.dll
-    ${MINGW_BIN}/libepoxy-0.dll
-    ${MINGW_BIN}/libexpat-1.dll
-    ${MINGW_BIN}/libexslt-0.dll
-    ${MINGW_BIN}/libffi-6.dll
-    ${MINGW_BIN}/libfftw3-3.dll
-    ${MINGW_BIN}/libfontconfig-1.dll
-    ${MINGW_BIN}/libfreetype-6.dll
-    ${MINGW_BIN}/libgc-1.dll
-    ${MINGW_BIN}/libgdbm-4.dll
-    ${MINGW_BIN}/libgdk-3-0.dll
-    ${MINGW_BIN}/libgdk_pixbuf-2.0-0.dll
-    ${MINGW_BIN}/libgdkmm-3.0-1.dll
-    ${MINGW_BIN}/libgdl-3-5.dll
-    ${MINGW_BIN}/libgfortran-4.dll
-    ${MINGW_BIN}/libgio-2.0-0.dll
-    ${MINGW_BIN}/libgiomm-2.4-1.dll
-    ${MINGW_BIN}/libglib-2.0-0.dll
-    ${MINGW_BIN}/libglibmm-2.4-1.dll
-    ${MINGW_BIN}/libgmodule-2.0-0.dll
-    ${MINGW_BIN}/libgmp-10.dll
-    ${MINGW_BIN}/libgnutls-30.dll
-    ${MINGW_BIN}/libgobject-2.0-0.dll
-    ${MINGW_BIN}/libgomp-1.dll
-    ${MINGW_BIN}/libgraphite2.dll
-    ${MINGW_BIN}/libgsl-19.dll
-    ${MINGW_BIN}/libgslcblas-0.dll
-    ${MINGW_BIN}/libgtk-3-0.dll
-    ${MINGW_BIN}/libgtkmm-3.0-1.dll
-    ${MINGW_BIN}/libgtkspell3-3-0.dll
-    ${MINGW_BIN}/libharfbuzz-0.dll
-    ${MINGW_BIN}/libhogweed-4.dll
-    ${MINGW_BIN}/libiconv-2.dll
-    ${MINGW_BIN}/libicudt58.dll
-    ${MINGW_BIN}/libicuin58.dll
-    ${MINGW_BIN}/libicuuc58.dll
-    ${MINGW_BIN}/libidn-11.dll
-    ${MINGW_BIN}/libintl-8.dll
-    ${MINGW_BIN}/libjpeg-8.dll
-    ${MINGW_BIN}/liblcms2-2.dll
-    ${MINGW_BIN}/liblqr-1-0.dll
-    ${MINGW_BIN}/libltdl-7.dll
-    ${MINGW_BIN}/liblzma-5.dll
-    ${MINGW_BIN}/libnettle-6.dll
-    ${MINGW_BIN}/libnghttp2-14.dll
-    ${MINGW_BIN}/libnspr4.dll
+    ${MINGW_BIN}/libepoxy-[0-9]*.dll
+    ${MINGW_BIN}/libexpat-[0-9]*.dll
+    ${MINGW_BIN}/libexslt-[0-9]*.dll
+    ${MINGW_BIN}/libffi-[0-9]*.dll
+    ${MINGW_BIN}/libfftw3-[0-9]*.dll
+    ${MINGW_BIN}/libfontconfig-[0-9]*.dll
+    ${MINGW_BIN}/libfreetype-[0-9]*.dll
+    ${MINGW_BIN}/libgc-[0-9]*.dll
+    ${MINGW_BIN}/libgdbm-[0-9]*.dll
+    ${MINGW_BIN}/libgdk-3-[0-9]*.dll
+    ${MINGW_BIN}/libgdk_pixbuf-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgdkmm-3.0-[0-9]*.dll
+    ${MINGW_BIN}/libgdl-3-[0-9]*.dll
+    ${MINGW_BIN}/libgfortran-[0-9]*.dll
+    ${MINGW_BIN}/libgio-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgiomm-2.4-[0-9]*.dll
+    ${MINGW_BIN}/libglib-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libglibmm-2.4-[0-9]*.dll
+    ${MINGW_BIN}/libgmodule-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgmp-[0-9]*.dll
+    ${MINGW_BIN}/libgnutls-[0-9]*.dll
+    ${MINGW_BIN}/libgobject-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgomp-[0-9]*.dll
+    ${MINGW_BIN}/libgraphite[0-9]*.dll
+    ${MINGW_BIN}/libgsl-[0-9]*.dll
+    ${MINGW_BIN}/libgslcblas-[0-9]*.dll
+    ${MINGW_BIN}/libgtk-3-[0-9]*.dll
+    ${MINGW_BIN}/libgtkmm-3.0-[0-9]*.dll
+    ${MINGW_BIN}/libgtkspell3-3-[0-9]*.dll
+    ${MINGW_BIN}/libharfbuzz-[0-9]*.dll
+    ${MINGW_BIN}/libhogweed-[0-9]*.dll
+    ${MINGW_BIN}/libiconv-[0-9]*.dll
+    ${MINGW_BIN}/libicudt[0-9]*.dll
+    ${MINGW_BIN}/libicuin[0-9]*.dll
+    ${MINGW_BIN}/libicuuc[0-9]*.dll
+    ${MINGW_BIN}/libidn-[0-9]*.dll
+    ${MINGW_BIN}/libintl-[0-9]*.dll
+    ${MINGW_BIN}/libjpeg-[0-9]*.dll
+    ${MINGW_BIN}/liblcms2-[0-9]*.dll
+    ${MINGW_BIN}/liblqr-1-[0-9]*.dll
+    ${MINGW_BIN}/libltdl-[0-9]*.dll
+    ${MINGW_BIN}/liblzma-[0-9]*.dll
+    ${MINGW_BIN}/libnettle-[0-9]*.dll
+    ${MINGW_BIN}/libnghttp2-[0-9]*.dll
+    ${MINGW_BIN}/libnspr[0-9]*.dll
     ${MINGW_BIN}/libopenblas.dll
-    ${MINGW_BIN}/libopenjp2-7.dll
-    ${MINGW_BIN}/libp11-kit-0.dll
-    ${MINGW_BIN}/libpango-1.0-0.dll
-    ${MINGW_BIN}/libpangocairo-1.0-0.dll
-    ${MINGW_BIN}/libpangoft2-1.0-0.dll
-    ${MINGW_BIN}/libpangomm-1.4-1.dll
-    ${MINGW_BIN}/libpangowin32-1.0-0.dll
-    ${MINGW_BIN}/libpcre-1.dll
-    ${MINGW_BIN}/libpixman-1-0.dll
-    ${MINGW_BIN}/libplc4.dll
-    ${MINGW_BIN}/libplds4.dll
-    ${MINGW_BIN}/libpng16-16.dll
-    ${MINGW_BIN}/libpoppler-67.dll
-    ${MINGW_BIN}/libpoppler-glib-8.dll
-    ${MINGW_BIN}/libpopt-0.dll
-    ${MINGW_BIN}/libpotrace-0.dll
-    ${MINGW_BIN}/libquadmath-0.dll
-    ${MINGW_BIN}/libreadline7.dll
-    ${MINGW_BIN}/librevenge-0.0.dll
-    ${MINGW_BIN}/librevenge-stream-0.0.dll
-    ${MINGW_BIN}/librsvg-2-2.dll
-    ${MINGW_BIN}/librtmp-1.dll
-    ${MINGW_BIN}/libsigc-2.0-0.dll
-    ${MINGW_BIN}/libsoup-2.4-1.dll
-    ${MINGW_BIN}/libsqlite3-0.dll
-    ${MINGW_BIN}/libssh2-1.dll
-    ${MINGW_BIN}/libstdc++-6.dll
-    ${MINGW_BIN}/libtasn1-6.dll
-    ${MINGW_BIN}/libtermcap-0.dll
-    ${MINGW_BIN}/libtiff-5.dll
-    ${MINGW_BIN}/libunistring-2.dll
-    ${MINGW_BIN}/libvisio-0.1.dll
-    ${MINGW_BIN}/libwebp-7.dll
-    ${MINGW_BIN}/libwebpmux-3.dll
-    ${MINGW_BIN}/libwinpthread-1.dll
-    ${MINGW_BIN}/libwpd-0.10.dll
-    ${MINGW_BIN}/libwpg-0.3.dll
-    ${MINGW_BIN}/libxml2-2.dll
-    ${MINGW_BIN}/libxslt-1.dll
-    ${MINGW_BIN}/libyaml-0-2.dll
-    ${MINGW_BIN}/nss3.dll
-    ${MINGW_BIN}/nssutil3.dll
-    ${MINGW_BIN}/smime3.dll
-    ${MINGW_BIN}/tcl86.dll
-    ${MINGW_BIN}/tk86.dll
-    ${MINGW_BIN}/zlib1.dll
-    DESTINATION .)
+    ${MINGW_BIN}/libopenjp2-[0-9]*.dll
+    ${MINGW_BIN}/libp11-kit-[0-9]*.dll
+    ${MINGW_BIN}/libpango-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libpangocairo-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libpangoft2-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libpangomm-1.4-[0-9]*.dll
+    ${MINGW_BIN}/libpangowin32-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libpcre-[0-9]*.dll
+    ${MINGW_BIN}/libpixman-1-[0-9]*.dll
+    ${MINGW_BIN}/libplc[0-9]*.dll
+    ${MINGW_BIN}/libplds[0-9]*.dll
+    ${MINGW_BIN}/libpng16-[0-9]*.dll
+    ${MINGW_BIN}/libpoppler-[0-9]*.dll
+    ${MINGW_BIN}/libpoppler-glib-[0-9]*.dll
+    ${MINGW_BIN}/libpopt-[0-9]*.dll
+    ${MINGW_BIN}/libpotrace-[0-9]*.dll
+    ${MINGW_BIN}/libquadmath-[0-9]*.dll
+    ${MINGW_BIN}/libreadline[0-9]*.dll
+    ${MINGW_BIN}/librevenge-0.[0-9]*.dll
+    ${MINGW_BIN}/librevenge-stream-0.[0-9]*.dll
+    ${MINGW_BIN}/librsvg-2-[0-9]*.dll
+    ${MINGW_BIN}/librtmp-[0-9]*.dll
+    ${MINGW_BIN}/libsigc-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libsoup-2.4-[0-9]*.dll
+    ${MINGW_BIN}/libsqlite3-[0-9]*.dll
+    ${MINGW_BIN}/libssh2-[0-9]*.dll
+    ${MINGW_BIN}/libstdc++-[0-9]*.dll
+    ${MINGW_BIN}/libtasn1-[0-9]*.dll
+    ${MINGW_BIN}/libtermcap-[0-9]*.dll
+    ${MINGW_BIN}/libtiff-[0-9]*.dll
+    ${MINGW_BIN}/libunistring-[0-9]*.dll
+    ${MINGW_BIN}/libvisio-0.[0-9]*.dll
+    ${MINGW_BIN}/libwebp-[0-9]*.dll
+    ${MINGW_BIN}/libwebpmux-[0-9]*.dll
+    ${MINGW_BIN}/libwinpthread-[0-9]*.dll
+    ${MINGW_BIN}/libwpd-0.[0-9]*.dll
+    ${MINGW_BIN}/libwpg-0.[0-9]*.dll
+    ${MINGW_BIN}/libxml2-[0-9]*.dll
+    ${MINGW_BIN}/libxslt-[0-9]*.dll
+    ${MINGW_BIN}/libyaml-0-[0-9]*.dll
+    ${MINGW_BIN}/nss[0-9]*.dll
+    ${MINGW_BIN}/nssutil[0-9]*.dll
+    ${MINGW_BIN}/smime[0-9]*.dll
+    ${MINGW_BIN}/tcl[0-9]*.dll
+    ${MINGW_BIN}/tk[0-9]*.dll
+    ${MINGW_BIN}/zlib1.dll)
+  INSTALL(FILES ${MINGW_LIBS} DESTINATION .)
   # There are differences for 64-Bit and 32-Bit build environments.
   if(HAVE_MINGW64)
     install(FILES

mirror-of/inkscape

(backported from commit d68e3e8e36ec03ea6123d615f5c0c3857b3684eb)

CMakeScripts/InstallMSYS2.cmake
@@ -16,109 +16,112 @@ if(WIN32)
   install(DIRECTORY doc
     DESTINATION .)
 
-  # mingw dlls
-  install(FILES
+  # mingw-w64 dlls
+  #   (use msys2checkdeps.py to list required libraries / check for missing libraries)
+  file(GLOB MINGW_LIBS
     ${MINGW_BIN}/LIBEAY32.dll
     ${MINGW_BIN}/SSLEAY32.dll
-    ${MINGW_BIN}/libMagick++-6.Q16HDRI-6.dll
-    ${MINGW_BIN}/libMagickCore-6.Q16HDRI-2.dll
-    ${MINGW_BIN}/libMagickWand-6.Q16HDRI-2.dll
-    ${MINGW_BIN}/libaspell-15.dll
-    ${MINGW_BIN}/libatk-1.0-0.dll
-    ${MINGW_BIN}/libatkmm-1.6-1.dll
-    ${MINGW_BIN}/libbz2-1.dll
-    ${MINGW_BIN}/libcairo-2.dll
-    ${MINGW_BIN}/libcairo-gobject-2.dll
-    ${MINGW_BIN}/libcairomm-1.0-1.dll
-    ${MINGW_BIN}/libcdr-0.1.dll
-    ${MINGW_BIN}/libcurl-4.dll
+    ${MINGW_BIN}/libMagick*.dll
+    ${MINGW_BIN}/libaspell-[0-9]*.dll
+    ${MINGW_BIN}/libatk-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libatkmm-1.6-[0-9]*.dll
+    ${MINGW_BIN}/libbz2-[0-9]*.dll
+    ${MINGW_BIN}/libcairo-[0-9]*.dll
+    ${MINGW_BIN}/libcairomm-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libcdr-0.[0-9]*.dll
+    ${MINGW_BIN}/libcroco-0.6-[0-9]*.dll
+    ${MINGW_BIN}/libcurl-[0-9]*.dll
     ${MINGW_BIN}/libenchant.dll
-    ${MINGW_BIN}/libepoxy-0.dll
-    ${MINGW_BIN}/libexpat-1.dll
-    ${MINGW_BIN}/libffi-6.dll
-    ${MINGW_BIN}/libfftw3-3.dll
-    ${MINGW_BIN}/libfontconfig-1.dll
-    ${MINGW_BIN}/libfreetype-6.dll
-    ${MINGW_BIN}/libgc-1.dll
-    ${MINGW_BIN}/libgdk-win32-2.0-0.dll
-    ${MINGW_BIN}/libgdk_pixbuf-2.0-0.dll
-    ${MINGW_BIN}/libgdkmm-2.4-1.dll
-    ${MINGW_BIN}/libgio-2.0-0.dll
-    ${MINGW_BIN}/libgiomm-2.4-1.dll
-    ${MINGW_BIN}/libglib-2.0-0.dll
-    ${MINGW_BIN}/libglibmm-2.4-1.dll
-    ${MINGW_BIN}/libgmodule-2.0-0.dll
-    ${MINGW_BIN}/libgmp-10.dll
-    ${MINGW_BIN}/libgnutls-30.dll
-    ${MINGW_BIN}/libgobject-2.0-0.dll
-    ${MINGW_BIN}/libgomp-1.dll
-    ${MINGW_BIN}/libgraphite2.dll
-    ${MINGW_BIN}/libgsl-19.dll
-    ${MINGW_BIN}/libgslcblas-0.dll
-    ${MINGW_BIN}/libgtk-win32-2.0-0.dll
-    ${MINGW_BIN}/libgtkmm-2.4-1.dll
-    ${MINGW_BIN}/libgtkspell-0.dll
-    ${MINGW_BIN}/libharfbuzz-0.dll
-    ${MINGW_BIN}/libhogweed-4.dll
-    ${MINGW_BIN}/libiconv-2.dll
-    ${MINGW_BIN}/libicudt57.dll
-    ${MINGW_BIN}/libicuin57.dll
-    ${MINGW_BIN}/libicuuc57.dll
-    ${MINGW_BIN}/libidn-11.dll
-    ${MINGW_BIN}/libintl-8.dll
-    ${MINGW_BIN}/libjpeg-8.dll
-    ${MINGW_BIN}/liblcms2-2.dll
-    ${MINGW_BIN}/liblqr-1-0.dll
-    ${MINGW_BIN}/libltdl-7.dll
-    ${MINGW_BIN}/liblzma-5.dll
-    ${MINGW_BIN}/libnettle-6.dll
-    ${MINGW_BIN}/libnghttp2-14.dll
-    ${MINGW_BIN}/libnspr4.dll
-    ${MINGW_BIN}/libopenjp2-7.dll
-    ${MINGW_BIN}/libp11-kit-0.dll
-    ${MINGW_BIN}/libpango-1.0-0.dll
-    ${MINGW_BIN}/libpangocairo-1.0-0.dll
-    ${MINGW_BIN}/libpangoft2-1.0-0.dll
-    ${MINGW_BIN}/libpangomm-1.4-1.dll
-    ${MINGW_BIN}/libpangowin32-1.0-0.dll
-    ${MINGW_BIN}/libpcre-1.dll
-    ${MINGW_BIN}/libpixman-1-0.dll
-    ${MINGW_BIN}/libplc4.dll
-    ${MINGW_BIN}/libplds4.dll
-    ${MINGW_BIN}/libpng16-16.dll
-    ${MINGW_BIN}/libpoppler-66.dll
-    ${MINGW_BIN}/libpoppler-glib-8.dll
-    ${MINGW_BIN}/libpopt-0.dll
-    ${MINGW_BIN}/libpotrace-0.dll
-    ${MINGW_BIN}/librevenge-0.0.dll
-    ${MINGW_BIN}/librevenge-stream-0.0.dll
-    ${MINGW_BIN}/librtmp-1.dll
-    ${MINGW_BIN}/libsigc-2.0-0.dll
-    ${MINGW_BIN}/libssh2-1.dll
-    ${MINGW_BIN}/libstdc++-6.dll
-    ${MINGW_BIN}/libtasn1-6.dll
-    ${MINGW_BIN}/libtiff-5.dll
-    ${MINGW_BIN}/libunistring-2.dll
-    ${MINGW_BIN}/libvisio-0.1.dll
-    ${MINGW_BIN}/libwinpthread-1.dll
-    ${MINGW_BIN}/libwpd-0.10.dll
-    ${MINGW_BIN}/libwpg-0.3.dll
-    ${MINGW_BIN}/libxml2-2.dll
-    ${MINGW_BIN}/libxslt-1.dll
-    ${MINGW_BIN}/nss3.dll
-    ${MINGW_BIN}/nssutil3.dll
-    ${MINGW_BIN}/smime3.dll
-    ${MINGW_BIN}/zlib1.dll
-    # these are not picked up by 'ldd' but are required for SVG support in gdk-pixbuf-2.0
-    ${MINGW_BIN}/libcroco-0.6-3.dll
-    ${MINGW_BIN}/librsvg-2-2.dll
-    # required by python2-lxml
-    ${MINGW_BIN}/libexslt-0.dll
-    # required by python2-numpy
-    ${MINGW_BIN}/libgfortran-3.dll
+    ${MINGW_BIN}/libexpat-[0-9]*.dll
+    ${MINGW_BIN}/libexslt-[0-9]*.dll
+    ${MINGW_BIN}/libffi-[0-9]*.dll
+    ${MINGW_BIN}/libfftw3-[0-9]*.dll
+    ${MINGW_BIN}/libfontconfig-[0-9]*.dll
+    ${MINGW_BIN}/libfreetype-[0-9]*.dll
+    ${MINGW_BIN}/libgailutil-[0-9][0-9].dll
+    ${MINGW_BIN}/libgc-[0-9]*.dll
+    ${MINGW_BIN}/libgdbm-[0-9]*.dll
+    ${MINGW_BIN}/libgdk-win32-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgdk_pixbuf-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgdkmm-2.4-[0-9]*.dll
+    ${MINGW_BIN}/libgfortran-[0-9]*.dll
+    ${MINGW_BIN}/libgio-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgiomm-2.4-[0-9]*.dll
+    ${MINGW_BIN}/libglib-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libglibmm-2.4-[0-9]*.dll
+    ${MINGW_BIN}/libgmodule-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgmp-[0-9]*.dll
+    ${MINGW_BIN}/libgnutls-[0-9]*.dll
+    ${MINGW_BIN}/libgobject-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgomp-[0-9]*.dll
+    ${MINGW_BIN}/libgraphite[0-9]*.dll
+    ${MINGW_BIN}/libgsl-[0-9]*.dll
+    ${MINGW_BIN}/libgslcblas-[0-9]*.dll
+    ${MINGW_BIN}/libgtk-win32-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libgtkmm-2.4-[0-9]*.dll
+    ${MINGW_BIN}/libgtkspell-[0-9]*.dll
+    ${MINGW_BIN}/libharfbuzz-[0-9]*.dll
+    ${MINGW_BIN}/libhogweed-[0-9]*.dll
+    ${MINGW_BIN}/libiconv-[0-9]*.dll
+    ${MINGW_BIN}/libicudt[0-9]*.dll
+    ${MINGW_BIN}/libicuin[0-9]*.dll
+    ${MINGW_BIN}/libicuuc[0-9]*.dll
+    ${MINGW_BIN}/libidn-[0-9]*.dll
+    ${MINGW_BIN}/libintl-[0-9]*.dll
+    ${MINGW_BIN}/libjpeg-[0-9]*.dll
+    ${MINGW_BIN}/liblcms2-[0-9]*.dll
+    ${MINGW_BIN}/liblqr-1-[0-9]*.dll
+    ${MINGW_BIN}/libltdl-[0-9]*.dll
+    ${MINGW_BIN}/liblzma-[0-9]*.dll
+    ${MINGW_BIN}/libnettle-[0-9]*.dll
+    ${MINGW_BIN}/libnghttp2-[0-9]*.dll
+    ${MINGW_BIN}/libnspr[0-9]*.dll
     ${MINGW_BIN}/libopenblas.dll
-    ${MINGW_BIN}/libquadmath-0.dll
-    DESTINATION .)
+    ${MINGW_BIN}/libopenjp2-[0-9]*.dll
+    ${MINGW_BIN}/libp11-kit-[0-9]*.dll
+    ${MINGW_BIN}/libpango-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libpangocairo-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libpangoft2-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libpangomm-1.4-[0-9]*.dll
+    ${MINGW_BIN}/libpangowin32-1.0-[0-9]*.dll
+    ${MINGW_BIN}/libpcre-[0-9]*.dll
+    ${MINGW_BIN}/libpixman-1-[0-9]*.dll
+    ${MINGW_BIN}/libplc[0-9]*.dll
+    ${MINGW_BIN}/libplds[0-9]*.dll
+    ${MINGW_BIN}/libpng16-[0-9]*.dll
+    ${MINGW_BIN}/libpoppler-[0-9]*.dll
+    ${MINGW_BIN}/libpoppler-glib-[0-9]*.dll
+    ${MINGW_BIN}/libpopt-[0-9]*.dll
+    ${MINGW_BIN}/libpotrace-[0-9]*.dll
+    ${MINGW_BIN}/libquadmath-[0-9]*.dll
+    ${MINGW_BIN}/libreadline[0-9]*.dll
+    ${MINGW_BIN}/librevenge-0.[0-9]*.dll
+    ${MINGW_BIN}/librevenge-stream-0.[0-9]*.dll
+    ${MINGW_BIN}/librsvg-2-[0-9]*.dll
+    ${MINGW_BIN}/librtmp-[0-9]*.dll
+    ${MINGW_BIN}/libsigc-2.0-[0-9]*.dll
+    ${MINGW_BIN}/libsqlite3-[0-9]*.dll
+    ${MINGW_BIN}/libssh2-[0-9]*.dll
+    ${MINGW_BIN}/libstdc++-[0-9]*.dll
+    ${MINGW_BIN}/libtasn1-[0-9]*.dll
+    ${MINGW_BIN}/libtermcap-[0-9]*.dll
+    ${MINGW_BIN}/libtiff-[0-9]*.dll
+    ${MINGW_BIN}/libunistring-[0-9]*.dll
+    ${MINGW_BIN}/libvisio-0.[0-9]*.dll
+    ${MINGW_BIN}/libwebp-[0-9]*.dll
+    ${MINGW_BIN}/libwebpmux-[0-9]*.dll
+    ${MINGW_BIN}/libwinpthread-[0-9]*.dll
+    ${MINGW_BIN}/libwpd-0.[0-9]*.dll
+    ${MINGW_BIN}/libwpg-0.[0-9]*.dll
+    ${MINGW_BIN}/libxml2-[0-9]*.dll
+    ${MINGW_BIN}/libxslt-[0-9]*.dll
+    ${MINGW_BIN}/nss[0-9]*.dll
+    ${MINGW_BIN}/nssutil[0-9]*.dll
+    ${MINGW_BIN}/smime[0-9]*.dll
+    ${MINGW_BIN}/tcl[0-9]*.dll
+    ${MINGW_BIN}/tk[0-9]*.dll
+    ${MINGW_BIN}/zlib1.dll)
+  INSTALL(FILES ${MINGW_LIBS} DESTINATION .)
   # There are differences for 64-Bit and 32-Bit build environments.
   if(HAVE_MINGW64)
     install(FILES