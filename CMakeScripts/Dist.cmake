7718952a3f19dfcfffe2d5b124ded9cfb0938c30
Bryce Harrington <bryce@osg.samsung.com>
2017-07-23 21:13:31 -0700

    cmake: Include inkscape source in dist, not just docs/

--- CMakeScripts/Dist.cmake
+++ CMakeScripts/Dist.cmake
@@ -13,7 +13,7 @@ include(CMakeScripts/inkscape-version.cmake)
 
 add_custom_target(dist
     COMMAND sed -i "s/unknown/${INKSCAPE_REVISION}/" CMakeScripts/inkscape-version.cmake
-         && tar cfj ${CMAKE_BINARY_DIR}/${INKSCAPE_DIST_PREFIX}.tar.bz2 ${CMAKE_SOURCE_DIR}/doc --exclude=".*" --exclude-vcs-ignores
+         && tar cfj ${CMAKE_BINARY_DIR}/${INKSCAPE_DIST_PREFIX}.tar.bz2 ${CMAKE_SOURCE_DIR} --exclude=".*" --exclude-vcs-ignores
          || git checkout ${CMAKE_SOURCE_DIR}/CMakeScripts/inkscape-version.cmake
     COMMAND git checkout ${CMAKE_SOURCE_DIR}/CMakeScripts/inkscape-version.cmake # duplicate to make sure we actually revert in case of error
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
