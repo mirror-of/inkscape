e5eb28dbaf77ca3cbb6a455e2ece48f3946ea795
Alexander Valavanis <valavanisalex@gmail.com>
2018-03-29 19:16:19 +0100

    SelectToolbar: Migrate xywh controls

--- src/widgets/select-toolbar.h
+++ src/widgets/select-toolbar.h
@@ -17,26 +17,50 @@
 class SPDesktop;
 
 namespace Gtk {
-class Toolbar;
+class ToggleToolButton;
 }
 
-//typedef struct _GtkActionGroup GtkActionGroup;
-//typedef struct _GObject GObject;
-
-// void sp_select_toolbox_prep(SPDesktop *desktop, GtkActionGroup* mainActions, GObject* holder);
-
 namespace Inkscape {
+class Selection;
 class Verb;
 
 namespace UI {
 namespace Widget {
+class UnitTracker;
+
 class SelectToolbar : public Gtk::Toolbar {
 private:
     SPDesktop *_desktop;
     Gtk::ToolButton * create_toolbutton_for_verb(unsigned int  verb_code);
 
+    bool _update_flag;
+
+    Glib::RefPtr<Gtk::Adjustment>  _x_pos_adj;
+    Glib::RefPtr<Gtk::Adjustment>  _y_pos_adj;
+    Glib::RefPtr<Gtk::Adjustment>  _width_adj;
+    Glib::RefPtr<Gtk::Adjustment>  _height_adj;
+    Gtk::ToggleToolButton         *_lock_button;
+
+    std::vector<Gtk::ToolItem *> _context_items;
+
+    UnitTracker *_tracker;
+
+    enum LastChanged { CHANGED_NONE, CHANGED_X_Y, CHANGED_W, CHANGED_H };
+
+    LastChanged _last_changed;
+
+    void on_inkscape_selection_changed(Inkscape::Selection *selection);
+    void on_inkscape_selection_modified(Inkscape::Selection *selection, guint flags);
+    void layout_widget_update(Inkscape::Selection *selection);
+    void on_x_y_adj_value_changed();
+    void on_w_adj_value_changed();
+    void on_h_adj_value_changed();
+    void on_any_layout_adj_value_changed();
+    void on_lock_button_toggled();
+
 public:
     SelectToolbar(SPDesktop *desktop);
+    ~SelectToolbar();
 
     static GtkWidget *create(SPDesktop *desktop);
 };
