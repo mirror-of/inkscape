746044581d3d2d8c56331ab74d7a1b0b7c092dae
James Collier <james.collier412@gmail.com>
2019-05-11 22:33:08 +0200

    Ensure arena is released when the WMF failes to save
    (backported from b38c955a)

--- src/extension/internal/wmf-inout.cpp
+++ src/extension/internal/wmf-inout.cpp
@@ -115,6 +115,9 @@ Wmf::print_document_to_file(SPDocument *doc, const gchar *filename)
     /* Print document */
     if (mod->begin(doc)) {
         g_free(oldoutput);
+        mod->base->invoke_hide(mod->dkey);
+        mod->base = NULL;
+        mod->root = NULL;
         throw Inkscape::Extension::Output::save_failed();
     }
     mod->base->invoke_print(&context);
