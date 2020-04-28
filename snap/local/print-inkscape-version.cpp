d48b33a1cd0ab67926c86fb0f3f743d920a86ed1
Ted Gould <ted@gould.cx>
2020-04-28 14:49:09 +0000

    Switch version of snap to grab from the CPP generated string to match about screen exactly

---
+++ snap/local/print-inkscape-version.cpp
@@ -0,0 +1,38 @@
+#include "inkscape-version.h"
+
+#include <iostream>
+#include <cctype>
+
+int main (int argc, char ** argv) {
+	std::string outstr;
+
+	for (const auto& c : std::string{Inkscape::version_string}) {
+		if (outstr.length() == 32) {
+			break;
+		}
+
+		if (std::isalpha(c) || std::isdigit(c)) {
+			outstr.append(1, c);
+			continue;
+		}
+
+		if (c == '(' || c == ')' || c == ',') {
+			continue;
+		}
+
+		if (c == '.' || c == '-') {
+			outstr.append(1, c);
+			continue;
+		}
+
+		outstr.append(1, '-');
+	}
+
+	while (outstr.length() > 0 && !std::isalpha(outstr.back()) && !std::isdigit(outstr.back())) {
+		outstr.pop_back();
+	}
+
+	std::cout << outstr << std::endl;
+
+	return 0;
+}
