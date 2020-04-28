9273feed02c42e651fd9aebce5b073ccacbeeccf
Tavmjong Bah <tavmjong@free.fr>
2020-04-28 09:56:02 +0200

    Fix while loop.

--- src/object/sp-text.cpp
+++ src/object/sp-text.cpp
@@ -1026,7 +1026,7 @@ void SPText::newline_to_sodipodi() {
 
     // New lines can come anywhere, we must search character-by-character.
     auto it = layout.begin();
-    while (true) {
+    while (it != layout.end()) {
         if (layout.characterAt(it) == '\n') {
 
             // Delete newline ('\n').
@@ -1042,10 +1042,6 @@ void SPText::newline_to_sodipodi() {
 
         it.nextCharacter();
         layout.validateIterator(&it);
-
-        if (it == layout.end()) {
-            break;
-        }
     }
 }
 
