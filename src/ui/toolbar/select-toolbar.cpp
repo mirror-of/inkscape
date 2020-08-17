nullmastermind/inkscape-for-trace

commit de5feedee4a93cb7037a6eb1207fe45c53d41e33

Gobject data migration

src/ui/toolbar/select-toolbar.cpp
@@ -152,7 +152,6 @@ SelectToolbar::SelectToolbar(SPDesktop *desktop) :
     _lock_btn->set_tooltip_text(_("When locked, change both width and height by the same proportion"));
     _lock_btn->set_icon_name(INKSCAPE_ICON("object-unlocked"));
     _lock_btn->signal_toggled().connect(sigc::mem_fun(*this, &SelectToolbar::toggle_lock));
-    set_data("lock", _lock_btn->gobj());
     add(*_lock_btn);
 
     // height-value control

src/ui/contextmenu.cpp
@@ -158,7 +158,7 @@ ContextMenu::ContextMenu(SPDesktop *desktop, SPItem *item) :
     if ( group && group != _desktop->currentLayer() ) {
         /* TRANSLATORS: #%1 is the id of the group e.g. <g id="#g7">, not a number. */
         MIGroup.set_label (Glib::ustring::compose(_("Enter group #%1"), group->getId()));
-        MIGroup.set_data("group", group);
+        _MIGroup_group = group;
         MIGroup.signal_activate().connect(sigc::bind(sigc::mem_fun(*this, &ContextMenu::EnterGroup),&MIGroup));
         MIGroup.show();
         append(MIGroup);
@@ -194,7 +194,7 @@ Gtk::SeparatorMenuItem* ContextMenu::AddSeparator()
 
 void ContextMenu::EnterGroup(Gtk::MenuItem* mi)
 {
-    _desktop->setCurrentLayer(reinterpret_cast<SPObject *>(mi->get_data("group")));
+    _desktop->setCurrentLayer(_MIGroup_group);
     _desktop->selection->clear();
 }
 

src/ui/contextmenu.h
@@ -69,8 +69,9 @@ class ContextMenu : public Gtk::Menu
         SPDesktop *_desktop; //pointer to the desktop the user was currently working on at the time the ContextMenu is created
         
         int positionOfLastDialog;
-        
+
         Gtk::MenuItem MIGroup; //menu entry to enter a group
+        SPObject *_MIGroup_group; // Group to enter when MIGroup is activated
         Gtk::MenuItem MIParent; //menu entry to leave a group
         
         /**

src/ui/dialog-events.cpp
@@ -84,13 +84,6 @@ sp_dialog_defocus_on_enter (GtkWidget *w)
 gboolean
 sp_dialog_event_handler (GtkWindow *win, GdkEvent *event, gpointer data)
 {
-
-// if the focus is inside the Text and Font textview, do nothing
-    GObject *dlg = G_OBJECT(data);
-    if (g_object_get_data (dlg, "eatkeys")) {
-        return FALSE;
-    }
-
     gboolean ret = FALSE;
 
     switch (event->type) {

src/ui/dialog/clonetiler.cpp
@@ -59,6 +59,27 @@ using Inkscape::Util::unit_table;
 
 namespace Inkscape {
 namespace UI {
+
+namespace Widget {
+/**
+ * Simple extension of Gtk::CheckButton, which adds a flag
+ * to indicate whether the box should be unticked when reset
+ */
+class CheckButtonInternal : public Gtk::CheckButton {
+  private:
+    bool _uncheckable = false;
+  public:
+    CheckButtonInternal() = default;
+
+    CheckButtonInternal(const Glib::ustring &label)
+        : Gtk::CheckButton(label)
+    {}
+
+    void set_uncheckable(const bool val = true) { _uncheckable = val; }
+    bool get_uncheckable() const { return _uncheckable; }
+};
+}
+
 namespace Dialog {
 
 #define SB_MARGIN 1
@@ -779,8 +800,8 @@ CloneTiler::CloneTiler () :
             gtk_box_set_homogeneous(GTK_BOX(hb), FALSE);
             gtk_box_pack_start (GTK_BOX (vb), hb, FALSE, FALSE, 0);
 
-            _b = Gtk::manage(new Gtk::CheckButton(_("Trace the drawing under the clones/sprayed items")));
-            _b->set_data("uncheckable", GINT_TO_POINTER(TRUE));
+            _b = Gtk::manage(new UI::Widget::CheckButtonInternal(_("Trace the drawing under the clones/sprayed items")));
+            _b->set_uncheckable();
             bool old = prefs->getBool(prefs_path + "dotrace");
             _b->set_active(old);
             _b->set_tooltip_text(_("For each clone/sprayed item, pick a value from the drawing in its location and apply it"));
@@ -1095,7 +1116,7 @@ CloneTiler::CloneTiler () :
             auto hb = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_HORIZONTAL, VB_MARGIN));
             gtk_box_pack_start (GTK_BOX (mainbox), GTK_WIDGET(hb->gobj()), FALSE, FALSE, 0);
 
-            _cb_keep_bbox = Gtk::manage(new Gtk::CheckButton(_("Use saved size and position of the tile")));
+            _cb_keep_bbox = Gtk::manage(new UI::Widget::CheckButtonInternal(_("Use saved size and position of the tile")));
             auto keepbbox = prefs->getBool(prefs_path + "keepbbox", true);
             _cb_keep_bbox->set_active(keepbbox);
             _cb_keep_bbox->set_tooltip_text(_("Pretend that the size and position of the tile are the same "
@@ -2522,7 +2543,7 @@ Gtk::Widget * CloneTiler::checkbox(const char          *tip,
                                    const Glib::ustring &attr)
 {
     auto hb = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_HORIZONTAL, VB_MARGIN));
-    auto b  = Gtk::manage(new Gtk::CheckButton());
+    auto b  = Gtk::manage(new UI::Widget::CheckButtonInternal());
     b->set_tooltip_text(tip);
 
     auto const prefs = Inkscape::Preferences::get();
@@ -2532,7 +2553,7 @@ Gtk::Widget * CloneTiler::checkbox(const char          *tip,
     hb->pack_start(*b, false, true);
     b->signal_clicked().connect(sigc::bind(sigc::mem_fun(*this, &CloneTiler::checkbox_toggled), b, attr));
 
-    b->set_data("uncheckable", GINT_TO_POINTER(true));
+    b->set_uncheckable();
 
     return hb;
 }
@@ -2581,9 +2602,9 @@ Gtk::Widget * CloneTiler::spinbox(const char          *tip,
         a->signal_value_changed().connect(sigc::bind(sigc::mem_fun(*this, &CloneTiler::value_changed), a, attr));
 
         if (exponent) {
-            sb->set_data ("oneable", GINT_TO_POINTER(TRUE));
+            sb->set_oneable();
         } else {
-            sb->set_data ("zeroable", GINT_TO_POINTER(TRUE));
+            sb->set_zeroable();
         }
     }
 
@@ -2622,42 +2643,44 @@ void CloneTiler::pick_to(Gtk::ToggleButton *tb, Glib::ustring const &pref)
 }
 
 
-void CloneTiler::reset_recursive(GtkWidget *w)
+void CloneTiler::reset_recursive(Gtk::Widget *w)
 {
-    if (w && G_IS_OBJECT(w)) {
+    if (w) {
+        auto sb = dynamic_cast<Inkscape::UI::Widget::SpinButton *>(w);
+        auto tb = dynamic_cast<Inkscape::UI::Widget::CheckButtonInternal *>(w);
+
         {
-            int r = GPOINTER_TO_INT (g_object_get_data(G_OBJECT(w), "zeroable"));
-            if (r && GTK_IS_SPIN_BUTTON(w)) { // spinbutton
-                GtkAdjustment *a = gtk_spin_button_get_adjustment (GTK_SPIN_BUTTON(w));
-                gtk_adjustment_set_value (a, 0);
+            if (sb && sb->get_zeroable()) { // spinbutton
+                auto a = sb->get_adjustment();
+                a->set_value(0);
             }
         }
         {
-            int r = GPOINTER_TO_INT (g_object_get_data(G_OBJECT(w), "oneable"));
-            if (r && GTK_IS_SPIN_BUTTON(w)) { // spinbutton
-                GtkAdjustment *a = gtk_spin_button_get_adjustment (GTK_SPIN_BUTTON(w));
-                gtk_adjustment_set_value (a, 1);
+            if (sb && sb->get_oneable()) { // spinbutton
+                auto a = sb->get_adjustment();
+                a->set_value(1);
             }
         }
         {
-            int r = GPOINTER_TO_INT (g_object_get_data(G_OBJECT(w), "uncheckable"));
-            if (r && GTK_IS_TOGGLE_BUTTON(w)) { // checkbox
-                gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(w), FALSE);
+            if (tb && tb->get_uncheckable()) { // checkbox
+                tb->set_active(false);
             }
         }
     }
 
-    if (GTK_IS_CONTAINER(w)) {
-        std::vector<Gtk::Widget*> c = Glib::wrap(GTK_CONTAINER(w))->get_children();
-        for ( auto i : c ) {
-            reset_recursive(i->gobj());
+    auto container = dynamic_cast<Gtk::Container *>(w);
+
+    if (container) {
+        auto c = container->get_children();
+        for (auto i : c) {
+            reset_recursive(i);
         }
     }
 }
 
 void CloneTiler::reset()
 {
-    reset_recursive(GTK_WIDGET(this->gobj()));
+    reset_recursive(this);
 }
 
 void CloneTiler::table_attach(GtkWidget *table, Gtk::Widget *widget, float align, int row, int col)

src/ui/dialog/clonetiler.h
@@ -16,7 +16,6 @@
 #include "ui/widget/color-picker.h"
 
 namespace Gtk {
-    class CheckButton;
     class ComboBox;
     class ToggleButton;
 }
@@ -33,6 +32,7 @@ namespace Inkscape {
 namespace UI {
 
 namespace Widget {
+    class CheckButtonInternal;
     class UnitMenu;
 }
 
@@ -96,7 +96,7 @@ protected:
                  Glib::ustring const &pref);
     void remove(bool do_undo = true);
     void reset();
-    void reset_recursive(GtkWidget *w);
+    void reset_recursive(Gtk::Widget *w);
     void switch_to_create();
     void switch_to_fill();
     void unclump();
@@ -146,8 +146,8 @@ private:
     CloneTiler(CloneTiler const &d) = delete;
     CloneTiler& operator=(CloneTiler const &d) = delete;
 
-    Gtk::CheckButton *_b;
-    Gtk::CheckButton *_cb_keep_bbox;
+    UI::Widget::CheckButtonInternal *_b;
+    UI::Widget::CheckButtonInternal *_cb_keep_bbox;
     GtkWidget *nb;
     Inkscape::UI::Widget::ColorPicker *color_picker;
     GtkSizeGroup* table_row_labels;

src/ui/dialog/export.cpp
@@ -107,6 +107,31 @@ namespace Inkscape {
 namespace UI {
 namespace Dialog {
 
+class ExportProgressDialog : public Gtk::Dialog {
+  private:
+      Gtk::ProgressBar *_progress = nullptr;
+      Export *_export_panel = nullptr;
+      int _current = 0;
+      int _total = 0;
+
+  public:
+      ExportProgressDialog(const Glib::ustring &title, bool modal = false)
+          : Gtk::Dialog(title, modal)
+      {}
+
+      inline void set_export_panel(const decltype(_export_panel) export_panel) { _export_panel = export_panel; }
+      inline decltype(_export_panel) get_export_panel() const { return _export_panel; }
+
+      inline void set_progress(const decltype(_progress) progress) { _progress = progress; }
+      inline decltype(_progress) get_progress() const { return _progress; }
+
+      inline void set_current(const int current) { _current = current; }
+      inline int get_current() const { return _current; }
+
+      inline void set_total(const int total) { _total = total; }
+      inline int get_total() const { return _total; }
+};
+
 static std::string create_filepath_from_id(Glib::ustring, const Glib::ustring &);
 
 /** A list of strings that is used both in the preferences, and in the
@@ -846,14 +871,14 @@ void Export::onProgressCancel ()
 /// Called for every progress iteration
 unsigned int Export::onProgressCallback(float value, void *dlg)
 {
-    Gtk::Dialog *dlg2 = reinterpret_cast<Gtk::Dialog*>(dlg);
+    auto dlg2 = reinterpret_cast<ExportProgressDialog*>(dlg);
 
-    Export *self = reinterpret_cast<Export *>(dlg2->get_data("exportPanel"));
+    auto self = dlg2->get_export_panel();
     if (self->interrupted)
         return FALSE;
     
-    gint current = GPOINTER_TO_INT(dlg2->get_data("current"));
-    gint total = GPOINTER_TO_INT(dlg2->get_data("total"));
+    auto current = dlg2->get_current();
+    auto total = dlg2->get_total();
     if (total > 0) {
         double completed = current;
         completed /= static_cast<double>(total);
@@ -861,7 +886,7 @@ unsigned int Export::onProgressCallback(float value, void *dlg)
         value = completed + (value / static_cast<double>(total));
     }
 
-    Gtk::ProgressBar *prg = reinterpret_cast<Gtk::ProgressBar *>(dlg2->get_data("progress"));
+    auto prg = dlg2->get_progress();
     prg->set_fraction(value);
 
     if (self) {
@@ -895,13 +920,15 @@ void Export::setExporting(bool exporting, Glib::ustring const &text)
     }
 }
 
-Gtk::Dialog * Export::create_progress_dialog (Glib::ustring progress_text) {
-    Gtk::Dialog *dlg = new Gtk::Dialog(_("Export in progress"), TRUE);
+ExportProgressDialog *
+Export::create_progress_dialog(Glib::ustring progress_text)
+{
+    auto dlg = new ExportProgressDialog(_("Export in progress"), true);
     dlg->set_transient_for( *(INKSCAPE.active_desktop()->getToplevel()) );
 
     Gtk::ProgressBar *prg = new Gtk::ProgressBar ();
     prg->set_text(progress_text);
-    dlg->set_data ("progress", prg);
+    dlg->set_progress(prg);
     auto CA = dlg->get_content_area();
     CA->pack_start(*prg, FALSE, FALSE, 4);
 
@@ -988,7 +1015,7 @@ void Export::onExport ()
         }
 
         prog_dlg = create_progress_dialog(Glib::ustring::compose(_("Exporting %1 files"), num));
-        prog_dlg->set_data("exportPanel", this);
+        prog_dlg->set_export_panel(this);
         setExporting(true, Glib::ustring::compose(_("Exporting %1 files"), num));
 
         gint export_count = 0;
@@ -997,8 +1024,8 @@ void Export::onExport ()
         for(auto i = itemlist.begin();i!=itemlist.end() && !interrupted ;++i){
             SPItem *item = *i;
 
-            prog_dlg->set_data("current", GINT_TO_POINTER(n));
-            prog_dlg->set_data("total", GINT_TO_POINTER(num));
+            prog_dlg->set_current(n);
+            prog_dlg->set_total(num);
             onProgressCallback(0.0, prog_dlg);
 
             // retrieve export filename hint
@@ -1120,11 +1147,11 @@ void Export::onExport ()
 
         /* TRANSLATORS: %1 will be the filename, %2 the width, and %3 the height of the image */
         prog_dlg = create_progress_dialog (Glib::ustring::compose(_("Exporting %1 (%2 x %3)"), fn, width, height));
-        prog_dlg->set_data("exportPanel", this);
+        prog_dlg->set_export_panel(this);
         setExporting(true, Glib::ustring::compose(_("Exporting %1 (%2 x %3)"), fn, width, height));
 
-        prog_dlg->set_data("current", GINT_TO_POINTER(0));
-        prog_dlg->set_data("total", GINT_TO_POINTER(0));
+        prog_dlg->set_current(0);
+        prog_dlg->set_total(0);
 
         auto area = Geom::Rect(Geom::Point(x0, y0), Geom::Point(x1, y1)) * desktop->dt2doc();
 
@@ -1539,9 +1566,7 @@ void Export::areaYChange(Glib::RefPtr<Gtk::Adjustment>& adj)
     height = floor ((y1 - y0) * ydpi / DPI_BASE + 0.5);
 
     if (height < SP_EXPORT_MIN_SIZE) {
-        //const gchar *key;
         height = SP_EXPORT_MIN_SIZE;
-        //key = (const gchar *)g_object_get_data(G_OBJECT (adj), "key");
         if (adj == y1_adj) {
             //if (!strcmp (key, "y0")) {
             y1 = y0 + height * DPI_BASE / ydpi;

src/ui/dialog/export.h
@@ -23,13 +23,11 @@
 
 #include "ui/widget/panel.h"
 
-namespace Gtk {
-class Dialog;
-}
-
 namespace Inkscape {
 namespace UI {
 namespace Dialog {
+
+class ExportProgressDialog;
 
 /** What type of button is being pressed */
 enum selection_type {
@@ -216,7 +214,7 @@ private:
      *
      * @param progress_text Text to be shown in the progress bar
      */
-    Gtk::Dialog * create_progress_dialog (Glib::ustring progress_text);
+    ExportProgressDialog * create_progress_dialog(Glib::ustring progress_text);
 
     /**
      * Callback to be used in for loop to update the progress bar.
@@ -336,7 +334,7 @@ private:
 
     Gtk::ProgressBar _prog;
 
-    Gtk::Dialog *prog_dlg;
+    ExportProgressDialog *prog_dlg;
     bool interrupted; // indicates whether export needs to be interrupted (read: user pressed cancel in the progress dialog)
 
     Inkscape::Preferences *prefs;

mirror-of/inkscape

commit 13b2006583fa6bc7f9793344d7774adb7e3a6196

Restore proportion lock toggle's behaviour
This fixes a regression introduced in de5feedee4a93cb7037a6eb1207fe45c53d41e33
where the select toolbar's lock button, supposed to lock the proportion
of objects as they are scaled with the mouse, instead has no effect.

src/ui/toolbar/select-toolbar.cpp
@@ -152,6 +152,7 @@ SelectToolbar::SelectToolbar(SPDesktop *desktop) :
     _lock_btn->set_tooltip_text(_("When locked, change both width and height by the same proportion"));
     _lock_btn->set_icon_name(INKSCAPE_ICON("object-unlocked"));
     _lock_btn->signal_toggled().connect(sigc::mem_fun(*this, &SelectToolbar::toggle_lock));
+    set_data("lock", _lock_btn->gobj());
     add(*_lock_btn);
 
     // height-value control