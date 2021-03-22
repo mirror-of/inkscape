nullmastermind/inkscape-for-trace

commit 1e31c41a0ef4b0f383d4f9d2941f643ae67ca646

Fix opacity of rasterized filters in PDF export
Opacity was applied thrice:
1. when rendering the filter to a pixbuf
   (i.e. transparency was baked into the bitmap)
2. when rendering this pixbuf onto a cairo surface
3. when actually rendering the (now doubly opaque) item

Avoid 1. and 2. and only apply opacity once.

Fixed bug:
  - https://bugs.launchpad.net/inkscape/+bug/381677

src/extension/internal/cairo-renderer.cpp
@@ -523,7 +523,9 @@ static void sp_asbitmap_render(SPItem *item, CairoRenderContext *ctx)
     if (pb) {
         //TEST(gdk_pixbuf_save( pb, "bitmap.png", "png", NULL, NULL ));
 
+        ctx->pushState();
         ctx->renderImage(pb.get(), t, item->style);
+        ctx->popState();
     }
 }
 

src/helper/pixbuf-ops.cpp
@@ -131,6 +131,9 @@ Inkscape::Pixbuf *sp_generate_internal_bitmap(SPDocument *doc, gchar const */*fi
     // because that would not work if the shown item references something in defs
     if (item_only) {
         hide_other_items_recursively(doc->getRoot(), item_only, dkey);
+        // TODO: The following line forces 100% opacity as required by sp_asbitmap_render() in cairo-renderer.cpp
+        //       Make it contitional if 'item_only' is ever used by other callers which need to retain opacity 
+        item_only->get_arenaitem(dkey)->setOpacity(1.0);
     }
 
     Geom::IntRect final_bbox = Geom::IntRect::from_xywh(0, 0, width, height);

mirror-of/inkscape

commit e2d5e006f80e7354d760999b0e136578ee0c8d8b

Do not rasterize clipPaths with filters during PDF export
clipPaths should ignore filters and this even caused crashes after
https://gitlab.com/inkscape/inkscape/-/commit/1e31c41a0ef4b0f383d4f9d2941f643ae67ca646

Fixes https://gitlab.com/inkscape/inkscape/-/issues/203

src/extension/internal/cairo-renderer.cpp
@@ -587,13 +587,19 @@ static void sp_item_invoke_render(SPItem *item, CairoRenderContext *ctx)
     if (item->isHidden()) {
         return;
     }
-
-    if(ctx->getFilterToBitmap() && (!item->style || item->style->filter.set != 0)) {
-        // This is not necesary but for cleanup, the filter hide the item
+    if (item->style && item->style->filter.set) {
+        // cleanup only; this is not necessary but the filter hides the item anyway
         SPFilter *filt = item->style->getFilter();
         if (filt && g_strcmp0(filt->getId(), "selectable_hidder_filter") == 0) {
             return;
         }
+    }
+
+    // rasterize filtered items as per user setting
+    // however, clipPaths ignore any filters, so do *not* rasterize
+    // TODO: might apply to some degree to masks with filtered elements as well;
+    //       we need to figure out where in the stack it would be safe to rasterize
+    if (ctx->getFilterToBitmap() && item->style->filter.set && !item->isInClipPath()) {
         return sp_asbitmap_render(item, ctx);
     }
 

src/helper/pixbuf-ops.cpp
@@ -102,8 +102,12 @@ Inkscape::Pixbuf *sp_generate_internal_bitmap(SPDocument *doc, gchar const */*fi
     if (item_only) {
         hide_other_items_recursively(doc->getRoot(), item_only, dkey);
         // TODO: The following line forces 100% opacity as required by sp_asbitmap_render() in cairo-renderer.cpp
-        //       Make it conditional if 'item_only' is ever used by other callers which need to retain opacity 
-        item_only->get_arenaitem(dkey)->setOpacity(1.0);
+        //       Make it conditional if 'item_only' is ever used by other callers which need to retain opacity
+        if (item_only->get_arenaitem(dkey)) {
+            item_only->get_arenaitem(dkey)->setOpacity(1.0);
+        } else {
+            g_warning("sp_generate_internal_bitmap: trying to set opacity of non-existing arenaitem");
+        }
     }
 
     Geom::IntRect final_bbox = Geom::IntRect::from_xywh(0, 0, width, height);