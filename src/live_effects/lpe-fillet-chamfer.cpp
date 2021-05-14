277f49e332cca98d02f76ea05457e307f0b664ae
Jabier Arraiza <jabier.arraiza@marker.es>
2021-05-14 16:51:48 +0000

    Fix a rendering helper paths brong on Corners LPE when yaxis down is false

--- src/live_effects/lpe-fillet-chamfer.cpp
+++ src/live_effects/lpe-fillet-chamfer.cpp
@@ -631,7 +631,7 @@ LPEFilletChamfer::doEffect_path(Geom::PathVector const &path_in)
                         path_chamfer.start(tmp_path.finalPoint());
                         if (eliptical) {
                             ccw_toggle = ccw_toggle ? false : true;
-                            path_chamfer.appendNew<Geom::EllipticalArc>(rx, ry, arc_angle, 0, ccw_toggle, end_arc_point);
+                            path_chamfer.appendNew<Geom::EllipticalArc>(rx, ry, arc_angle, false, ccw_toggle, end_arc_point);
                         } else {
                             path_chamfer.appendNew<Geom::CubicBezier>(handle_1, handle_2, end_arc_point);
                         }
@@ -643,7 +643,7 @@ LPEFilletChamfer::doEffect_path(Geom::PathVector const &path_in)
                         Geom::Path path_chamfer;
                         path_chamfer.start(tmp_path.finalPoint());
                         if (eliptical) {
-                            path_chamfer.appendNew<Geom::EllipticalArc>(rx, ry, arc_angle, 0, ccw_toggle, end_arc_point);
+                            path_chamfer.appendNew<Geom::EllipticalArc>(rx, ry, arc_angle, false, ccw_toggle, end_arc_point);
                         } else {
                             path_chamfer.appendNew<Geom::CubicBezier>(inverse_handle_1, inverse_handle_2, end_arc_point);
                         }
@@ -653,7 +653,12 @@ LPEFilletChamfer::doEffect_path(Geom::PathVector const &path_in)
                 case INVERSE_FILLET:
                     {
                         if (eliptical) {
-                            tmp_path.appendNew<Geom::EllipticalArc>(rx, ry, arc_angle, 0, ccw_toggle, end_arc_point);
+                            bool side = false;
+                            if (helperpath && !getSPDoc()->is_yaxisdown()) {
+                                side = true;
+                                ccw_toggle = ccw_toggle ? false : true;
+                            }
+                            tmp_path.appendNew<Geom::EllipticalArc>(rx, ry, arc_angle, side, ccw_toggle, end_arc_point);
                         } else {
                             tmp_path.appendNew<Geom::CubicBezier>(inverse_handle_1, inverse_handle_2, end_arc_point);
                         }
@@ -662,8 +667,13 @@ LPEFilletChamfer::doEffect_path(Geom::PathVector const &path_in)
                 default: //fillet
                     {
                         if (eliptical) {
-                            ccw_toggle = ccw_toggle ? false : true;
-                            tmp_path.appendNew<Geom::EllipticalArc>(rx, ry, arc_angle, 0, ccw_toggle, end_arc_point);
+                            bool side = false;
+                            if (helperpath && !getSPDoc()->is_yaxisdown()) {
+                                side = true;
+                            } else {
+                                ccw_toggle = ccw_toggle ? false : true;
+                            }
+                            tmp_path.appendNew<Geom::EllipticalArc>(rx, ry, arc_angle, side, ccw_toggle, end_arc_point);
                         } else {
                             tmp_path.appendNew<Geom::CubicBezier>(handle_1, handle_2, end_arc_point);
                         }
