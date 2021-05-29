235f462ff8fd9a2d0f3e50f2313736d0ad0e9485
KavyaJaiswal <jaiswalkavya2000@gmail.com>
2021-05-29 13:06:05 +0000

    Fixes unwanted warning 'Canvas::on_idle: clean region is empty!'

--- src/ui/widget/canvas.cpp
+++ src/ui/widget/canvas.cpp
@@ -975,11 +975,6 @@ Canvas::on_idle()
     bool done = do_update();
     int n_rects = _clean_region->get_num_rectangles();
 
-    // If we've drawn everything then we should have just one clean rectangle, covering the entire canvas.
-    if (n_rects == 0) {
-        std::cerr << "Canvas::on_idle: clean region is empty!" << std::endl;
-    }
-
     if (n_rects > 1) {
         done = false;
     }
