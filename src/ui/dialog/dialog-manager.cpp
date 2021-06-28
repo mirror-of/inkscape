e093358f1d170a5dc8901f504933b6d4700cde93
Mike Kowalski <michal_kowalski@hotmail.com>
2021-06-28 14:30:17 +0000

    Fix Mac build broken by using missing filesystem - use boost instead

--- src/ui/dialog/dialog-manager.cpp
+++ src/ui/dialog/dialog-manager.cpp
@@ -4,7 +4,9 @@
 
 #include <gdkmm/monitor.h>
 #include <limits>
-#include <filesystem>
+#include <boost/filesystem.hpp>
+namespace filesystem = boost::filesystem;
+// #include <filesystem> - waiting for compiler on MacOS to catch up to C++x17
 
 #include "io/resource.h"
 #include "inkscape-application.h"
@@ -235,7 +237,7 @@ void DialogManager::restore_dialogs_state(DialogContainer *docking_container, bo
     try {
         auto keyfile = std::make_unique<Glib::KeyFile>();
         std::string filename = Glib::build_filename(Inkscape::IO::Resource::profile_path(), dialogs_state);
-        if (std::filesystem::exists(filename) && keyfile->load_from_file(filename)) {
+        if (filesystem::exists(filename) && keyfile->load_from_file(filename)) {
             // restore visible dialogs first; that state is up-to-date
             docking_container->load_container_state(keyfile.get(), include_floating);
 
