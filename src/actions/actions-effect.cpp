3e0d83b24034e06d4cb9d053de5f13e9d2275750
sushant.co19 <sushant.co19@gmail.com>
2021-06-28 12:01:39 +0530

    Menubar Filters + Extensions : Conversion to Actions
    Approach:
    
    * Add Filters + Extensions to xml or ui file from effect.cpp
    
    *  Connect those items to actions in menubar.cpp
    
    Bug:
    
    * Items except Filters + Extensions are not connect to actions ( Connect it at the end when all the menu are present in one ui file )

--- src/actions/actions-effect.cpp
+++ src/actions/actions-effect.cpp
@@ -58,9 +58,9 @@ last_effect_pref(InkscapeApplication *app)
 std::vector<std::vector<Glib::ustring>> raw_data_effect =
 {
     // clang-format off
-    {"app.edit-remove-filter",      N_("Remove Filters"),                       "Filter",           N_("Remove any filters from selected objects")},
-    {"app.last-effect",             N_("Previous Extension"),                   "Extenssion",       N_("Repeat the last extension with the same settings")},
-    {"app.last-effect-pref",        N_("Previous Extension Settings"),          "Extenssion",       N_("Repeat the last extension with new settings")}
+    {"app.edit-remove-filter",      N_("Remove Filters"),                   "Filter",           N_("Remove any filters from selected objects")},
+    {"app.last-effect",             N_("Previous Extension"),               "Extenssion",       N_("Repeat the last extension with the same settings")},
+    {"app.last-effect-pref",        N_("Previous Extension Settings"),      "Extenssion",       N_("Repeat the last extension with new settings")}
     // clang-format on
 };
 
