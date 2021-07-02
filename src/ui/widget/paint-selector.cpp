mirror-of/inkscape

commit 19627310d9b5e85b7651946e8f48ceffa1cf07ee

Fix sentinel errors for builds against musl libc
This change fixes failing builds on systems with musl libc, where one
can experience lots of errors similar to following one:

```
FAILED: src/libnrtype/CMakeFiles/nrtype_LIB.dir/font-lister.cpp.o
/usr/bin/c++ ... -c ../src/libnrtype/font-lister.cpp
../src/libnrtype/font-lister.cpp: In function ‘void font_lister_cell_data_func2(GtkCellLayout*, GtkCellRenderer*, GtkTreeModel*, GtkTreeIter*, gpointer)’:
../src/libnrtype/font-lister.cpp:1236:64: error: missing sentinel in function call [-Werror=format=]
 1236 |     g_object_set(G_OBJECT(cell), "markup", markup.c_str(), NULL);
      |                                                                ^
```

Those errors are triggered by the compiler, because musl does not use
GCC's or Clang's __null extension for NULL definition, because it seems
non-conforming to the standard's requirements [1]. However, it is
possible to trigger this error with -Wstrict-null-sentinel GCC flag on
systems with glibc.

The change was originally made by dl2m, I applied the patch to the
version 1.0 release commit and rebased it on top of master branch.

[1]: https://git.musl-libc.org/cgit/musl/commit/?id=c8a9c22173f485c8c053709e1dfa0a617cb6be1a

Issue: https://gitlab.com/inkscape/inkscape/-/issues/1880
Based-on: https://gitlab.com/inkscape/inkscape/uploads/428412117341eb9601cb8ee01f2aae4b/0001-Fix-sentinal-errors.patch

src/ui/widget/paint-selector.cpp
@@ -1226,7 +1226,7 @@ void PaintSelector::set_mode_pattern(PaintSelector::Mode mode)
             GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
             gtk_cell_renderer_set_padding(renderer, 2, 0);
             gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(_patternmenu), renderer, TRUE);
-            gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(_patternmenu), renderer, "text", COMBO_COL_LABEL, NULL);
+            gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(_patternmenu), renderer, "text", COMBO_COL_LABEL, nullptr);
 
             ink_pattern_menu(_patternmenu);
             g_signal_connect(G_OBJECT(_patternmenu), "changed", G_CALLBACK(PaintSelector::pattern_change), this);
@@ -1322,7 +1322,7 @@ SPPattern *PaintSelector::getPattern()
         gchar *paturn;
 
         if (stockid) {
-            paturn = g_strconcat("urn:inkscape:pattern:", patid, NULL);
+            paturn = g_strconcat("urn:inkscape:pattern:", patid, nullptr);
         } else {
             paturn = g_strdup(patid);
         }

src/display/nr-filter-image.cpp
@@ -158,7 +158,7 @@ void FilterImage::render_cairo(FilterSlot &slot)
         if ( !g_file_test( fullname, G_FILE_TEST_EXISTS ) ) {
             // Try to load from relative position combined with document base
             if( document ) {
-                fullname = g_build_filename( document->getDocumentBase(), feImageHref, NULL );
+                fullname = g_build_filename( document->getDocumentBase(), feImageHref, nullptr );
             }
         }
         if ( !g_file_test( fullname, G_FILE_TEST_EXISTS ) ) {

src/extension/dependency.cpp
@@ -186,7 +186,7 @@ bool Dependency::check ()
                         break;
                     }
                     /* Look for deprecated locations next */
-                    auto deprloc = g_build_filename("inkex", "deprecated-simple", location.c_str(), NULL);
+                    auto deprloc = g_build_filename("inkex", "deprecated-simple", location.c_str(), nullptr);
                     std::string tempdepr =
                         Inkscape::IO::Resource::get_filename_string(Inkscape::IO::Resource::EXTENSIONS, deprloc, false, true);
                     g_free(deprloc);

src/inkscape.cpp
@@ -404,7 +404,7 @@ Application::crash_handler (int /*signum*/)
             FILE *file = nullptr;
             for(auto & location : locations) {
                 if (!location) continue; // It seems to be okay, but just in case
-                gchar * filename = g_build_filename(location, c, NULL);
+                gchar * filename = g_build_filename(location, c, nullptr);
                 Inkscape::IO::dump_fopen_call(filename, "E");
                 file = Inkscape::IO::fopen_utf8name(filename, "w");
                 if (file) {

src/io/resource.cpp
@@ -452,7 +452,7 @@ char const *profile_path()
         }
 #endif
         if (!prefdir) {
-            prefdir = g_build_filename(g_get_user_config_dir(), INKSCAPE_PROFILE_DIR, NULL);
+            prefdir = g_build_filename(g_get_user_config_dir(), INKSCAPE_PROFILE_DIR, nullptr);
             // In case the XDG user config dir of the moment does not yet exist...
             int mode = S_IRWXU;
 #ifdef S_IRGRP
@@ -471,7 +471,7 @@ char const *profile_path()
                 gchar const *userDirs[] = { "keys", "templates", "icons", "extensions", "ui",
                                             "symbols", "paint", "themes", "palettes", nullptr };
                 for (gchar const** name = userDirs; *name; ++name) {
-                    gchar *dir = g_build_filename(prefdir, *name, NULL);
+                    gchar *dir = g_build_filename(prefdir, *name, nullptr);
                     g_mkdir_with_parents(dir, mode);
                     g_free(dir);
                 }
@@ -495,7 +495,7 @@ char *homedir_path(const char *filename)
     static const gchar *homedir = nullptr;
     homedir = g_get_home_dir();
 
-    return g_build_filename(homedir, filename, NULL);
+    return g_build_filename(homedir, filename, nullptr);
 }
 
 }

src/libnrtype/font-lister.cpp
@@ -1233,7 +1233,7 @@ void font_lister_cell_data_func2(GtkCellLayout * /*cell_layout*/,
         g_free(sample_escaped);
     }
 
-    g_object_set(G_OBJECT(cell), "markup", markup.c_str(), NULL);
+    g_object_set(G_OBJECT(cell), "markup", markup.c_str(), nullptr);
     g_free(family);
     g_free(family_escaped);
 }

src/object/color-profile.cpp
@@ -695,7 +695,7 @@ std::set<ColorProfile::FilePlusHome> ColorProfile::getBaseProfileDirs() {
     std::set<ColorProfile::FilePlusHome> sources;
 
     // first try user's local dir
-    gchar* path = g_build_filename(g_get_user_data_dir(), "color", "icc", NULL);
+    gchar* path = g_build_filename(g_get_user_data_dir(), "color", "icc", nullptr);
     sources.insert(FilePlusHome(path, true));
     g_free(path);
 
@@ -703,11 +703,11 @@ std::set<ColorProfile::FilePlusHome> ColorProfile::getBaseProfileDirs() {
     // (see https://github.com/hughsie/colord/blob/fe10f76536bb27614ced04e0ff944dc6fb4625c0/lib/colord/cd-icc-store.c#L590)
 
     // user store
-    path = g_build_filename(g_get_user_data_dir(), "icc", NULL);
+    path = g_build_filename(g_get_user_data_dir(), "icc", nullptr);
     sources.insert(FilePlusHome(path, true));
     g_free(path);
 
-    path = g_build_filename(g_get_home_dir(), ".color", "icc", NULL);
+    path = g_build_filename(g_get_home_dir(), ".color", "icc", nullptr);
     sources.insert(FilePlusHome(path, true));
     g_free(path);
 
@@ -717,7 +717,7 @@ std::set<ColorProfile::FilePlusHome> ColorProfile::getBaseProfileDirs() {
 
     const gchar* const * dataDirs = g_get_system_data_dirs();
     for ( int i = 0; dataDirs[i]; i++ ) {
-        gchar* path = g_build_filename(dataDirs[i], "color", "icc", NULL);
+        gchar* path = g_build_filename(dataDirs[i], "color", "icc", nullptr);
         sources.insert(FilePlusHome(path, false));
         g_free(path);
     }
@@ -727,7 +727,7 @@ std::set<ColorProfile::FilePlusHome> ColorProfile::getBaseProfileDirs() {
         sources.insert(FilePlusHome("/System/Library/ColorSync/Profiles", false));
         sources.insert(FilePlusHome("/Library/ColorSync/Profiles", false));
 
-        gchar *path = g_build_filename(g_get_home_dir(), "Library", "ColorSync", "Profiles", NULL);
+        gchar *path = g_build_filename(g_get_home_dir(), "Library", "ColorSync", "Profiles", nullptr);
         sources.insert(FilePlusHome(path, true));
         g_free(path);
     }

src/object/sp-guide.cpp
@@ -512,7 +512,7 @@ char* SPGuide::description(bool const verbose) const
         
         if (verbose) {
             gchar *oldDescr = descr;
-            descr = g_strconcat(oldDescr, shortcuts, NULL);
+            descr = g_strconcat(oldDescr, shortcuts, nullptr);
             g_free(oldDescr);
         }
 

commit 72a0d5585d4fd592e398c943f4dd290ff621c7f5

Fix two skipped sentinel errors for builds against musl libc
Two sentinel errors were skipped in my previous commit due to an
undefined WITH_MESH.

Fixes: 19627310d9b5 ("Fix sentinel errors for builds against musl libc")

src/ui/widget/paint-selector.cpp
@@ -902,7 +902,7 @@ void PaintSelector::set_mode_mesh(PaintSelector::Mode mode)
             GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
             gtk_cell_renderer_set_padding(renderer, 2, 0);
             gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(combo), renderer, TRUE);
-            gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combo), renderer, "text", COMBO_COL_LABEL, NULL);
+            gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combo), renderer, "text", COMBO_COL_LABEL, nullptr);
 
             ink_mesh_menu(combo);
             g_signal_connect(G_OBJECT(combo), "changed", G_CALLBACK(PaintSelector::mesh_change), this);
@@ -971,7 +971,7 @@ SPMeshGradient *PaintSelector::getMeshGradient()
 
         gchar *mesh_name;
         if (stockid) {
-            mesh_name = g_strconcat("urn:inkscape:mesh:", meshid, NULL);
+            mesh_name = g_strconcat("urn:inkscape:mesh:", meshid, nullptr);
         } else {
             mesh_name = g_strdup(meshid);
         }