mirror-of/inkscape

commit 3345becf20c0b0604e66146ba4e284cd937e5fbe

Fix find and replace if text has description
Text in <description> or <title> should be ignored when
find and replacing text.

Fix https://gitlab.com/inkscape/inkscape/-/issues/2838

src/text-editing.cpp
@@ -910,7 +910,7 @@ static void sp_te_get_ustring_multiline(SPObject const *root, Glib::ustring *str
     for (auto& child: root->children) {
         if (SP_IS_STRING(&child)) {
             *string += SP_STRING(&child)->string;
-        } else {
+        } else if (is_part_of_text_subtree(&child)) {
             sp_te_get_ustring_multiline(&child, string, pending_line_break);
         }
     }
@@ -2131,7 +2131,7 @@ void sp_te_apply_style(SPItem *text, Inkscape::Text::Layout::iterator const &sta
     text->requestDisplayUpdate(SP_OBJECT_MODIFIED_FLAG | SP_OBJECT_STYLE_MODIFIED_FLAG);
 }
 
-bool is_part_of_text_subtree (SPObject *obj)
+bool is_part_of_text_subtree (SPObject const *obj)
 {
     return (SP_IS_TSPAN(obj) 
             || SP_IS_TEXT(obj) 
@@ -2143,13 +2143,13 @@ bool is_part_of_text_subtree (SPObject *obj)
             || SP_IS_FLOWREGIONBREAK(obj));
 }
 
-bool is_top_level_text_object (SPObject *obj)
+bool is_top_level_text_object (SPObject const *obj)
 {
     return (SP_IS_TEXT(obj) 
             || SP_IS_FLOWTEXT(obj));
 }
 
-bool has_visible_text(SPObject *obj)
+bool has_visible_text(SPObject const *obj)
 {
     bool hasVisible = false;
 
@@ -2157,7 +2157,7 @@ bool has_visible_text(SPObject *obj)
         hasVisible = true; // maybe we should also check that it's not all whitespace?
     } else {
         for (auto& child: obj->children) {
-            if (has_visible_text(const_cast<SPObject *>(&child))) {
+            if (is_part_of_text_subtree(&child) && has_visible_text(&child)) {
                 hasVisible = true;
                 break;
             }

src/text-editing.h
@@ -78,8 +78,8 @@ void sp_te_adjust_tspan_letterspacing_screen(SPItem *text, Inkscape::Text::Layou
 void sp_te_adjust_linespacing_screen(SPItem *text, Inkscape::Text::Layout::iterator const &start, Inkscape::Text::Layout::iterator const &end, SPDesktop *desktop, double by);
 void sp_te_apply_style(SPItem *text, Inkscape::Text::Layout::iterator const &start, Inkscape::Text::Layout::iterator const &end, SPCSSAttr const *css);
 
-bool is_part_of_text_subtree (SPObject *obj);
-bool is_top_level_text_object (SPObject *obj);
-bool has_visible_text (SPObject *obj);
+bool is_part_of_text_subtree (SPObject const *obj);
+bool is_top_level_text_object (SPObject const *obj);
+bool has_visible_text (SPObject const *obj);
 
 #endif // SEEN_SP_TEXT_EDITING_H

commit 47f979e664a21e63740b4d5e3b93bc5c1e20fee8

Fix regression so font substitution dialog works
Introduced in 3345becf20c0b0604e66146ba4e284cd937e5fbe

src/text-editing.cpp
@@ -2162,9 +2162,9 @@ bool has_visible_text(SPObject const *obj)
 
     if (SP_IS_STRING(obj) && !SP_STRING(obj)->string.empty()) {
         hasVisible = true; // maybe we should also check that it's not all whitespace?
-    } else {
+    } else if (is_part_of_text_subtree(obj)) {
         for (auto& child: obj->children) {
-            if (is_part_of_text_subtree(&child) && has_visible_text(&child)) {
+            if (has_visible_text(&child)) {
                 hasVisible = true;
                 break;
             }