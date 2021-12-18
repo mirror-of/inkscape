9b249ff6792993c6faa6509188e48e965c369fa4
Luca Bacci <luca.bacci982@gmail.com>
2021-12-18 21:01:41 +0100

    file-export-cmd: use C++17 std::filesystem::u8path on Windows
    Works around encoding issues, as boost::filesystem::path
    uses the ANSI encoding when constructing from a narrow string.

--- src/io/file-export-cmd.cpp
+++ src/io/file-export-cmd.cpp
@@ -43,8 +43,13 @@
 // C++17 std::filesystem (with #include <filesystem> ) then we drop this dep
 // (Dev meeting, 2020-09-25)
 
+#ifdef G_OS_WIN32
+#include <filesystem>
+namespace filesystem = std::filesystem;
+#else
 #include <boost/filesystem.hpp>
 namespace filesystem = boost::filesystem;
+#endif
 
 InkFileExportCmd::InkFileExportCmd()
     : export_overwrite(false)
@@ -75,7 +80,11 @@ InkFileExportCmd::do_export(SPDocument* doc, std::string filename_in)
 
     // Get export type from filename supplied with --export-filename
     if (!export_filename.empty() && export_filename != "-") {
+#ifdef G_OS_WIN32
+        auto fn = filesystem::u8path(export_filename);
+#else
         auto fn = filesystem::path(export_filename);
+#endif
         if (!fn.has_extension()) {
             if (export_type.empty() && export_extension.empty()) {
                 std::cerr << "InkFileExportCmd::do_export: No export type specified. "
