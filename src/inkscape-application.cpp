mirror-of/inkscape

commit c1616499b18d8feb5341c6208e9d49430b97d36b

Restore Welcome screen quit logic for mac
Taken from ac28ab6c60d7b546b6fbf4da6e6a59f260bb6973
See https://gitlab.com/inkscape/inkscape/-/issues/2762

src/inkscape-application.cpp
@@ -1617,9 +1617,11 @@ InkscapeApplication::on_new()
 void
 InkscapeApplication::on_quit()
 {
-    // Ensure closing the gtk_app windows
     if (gtk_app()) {
-        if (!destroy_all()) return; // Quit aborted.
+        // For mac, ensure closing the gtk_app windows
+        for (auto window : gtk_app()->get_windows()) {
+            window->close();
+        }
     }
 
     gio_app()->quit();

commit eacf41b111bdfa7a1d0a9e730c8c7f0ec9037390

Fix quit when document unsaved
Pointed out by Mike Kowalski, regressed from
c1616499b18d8feb5341c6208e9d49430b97d36b

src/inkscape-application.cpp
@@ -1618,6 +1618,7 @@ void
 InkscapeApplication::on_quit()
 {
     if (gtk_app()) {
+        if (!destroy_all()) return; // Quit aborted.
         // For mac, ensure closing the gtk_app windows
         for (auto window : gtk_app()->get_windows()) {
             window->close();